912785913643282d3ab4d20390846da273d55458
Jonathan Pobst <monkey@jpobst.com>
2010-02-09 17:20:17 -0600

    Modify PDN headers to reflect where I moved their license file to.

--- Pinta.Core/Effects/RgbColor.cs
+++ Pinta.Core/Effects/RgbColor.cs
@@ -2,9 +2,7 @@
 // Paint.NET                                                                   //
 // Copyright (C) Rick Brewster, Tom Jackson, and past contributors.            //
 // Portions Copyright (C) Microsoft Corporation. All Rights Reserved.          //
-// See src/Resources/Files/License.txt for full licensing and attribution      //
-// details.                                                                    //
-// .                                                                           //
+// See license-pdn.txt for full licensing and attribution details.             //
 /////////////////////////////////////////////////////////////////////////////////
 
 using System;
