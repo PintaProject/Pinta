ff3156ee6ffec7009c95031fb329b98c426fdf33
Jonathan Pobst <monkey@jpobst.com>
2010-03-25 14:11:12 -0500

    Use the PDN 3.36 code for Motion Blur.

--- Pinta.Core/Extensions/CairoExtensions.cs
+++ Pinta.Core/Extensions/CairoExtensions.cs
@@ -795,5 +795,68 @@ namespace Pinta.Core
 		{
 			return r.ContainsCorrect (pt.X, pt.Y);
 		}
+
+		public static unsafe ColorBgra GetBilinearSample (this ImageSurface src, float x, float y)
+		{
+			return GetBilinearSample (src, (ColorBgra*)src.DataPtr, src.Width, src.Height, x, y);
+		}
+
+		public static unsafe ColorBgra GetBilinearSample (this ImageSurface src, ColorBgra* srcDataPtr, int srcWidth, int srcHeight, float x, float y)
+		{
+			if (!Utility.IsNumber (x) || !Utility.IsNumber (y)) {
+				return ColorBgra.Transparent;
+			}
+
+			float u = x;
+			float v = y;
+
+			if (u >= 0 && v >= 0 && u < srcWidth && v < srcHeight) {
+				unchecked {
+					int iu = (int)Math.Floor (u);
+					uint sxfrac = (uint)(256 * (u - (float)iu));
+					uint sxfracinv = 256 - sxfrac;
+
+					int iv = (int)Math.Floor (v);
+					uint syfrac = (uint)(256 * (v - (float)iv));
+					uint syfracinv = 256 - syfrac;
+
+					uint wul = (uint)(sxfracinv * syfracinv);
+					uint wur = (uint)(sxfrac * syfracinv);
+					uint wll = (uint)(sxfracinv * syfrac);
+					uint wlr = (uint)(sxfrac * syfrac);
+
+					int sx = iu;
+					int sy = iv;
+					int sleft = sx;
+					int sright;
+
+					if (sleft == (srcWidth - 1)) {
+						sright = sleft;
+					} else {
+						sright = sleft + 1;
+					}
+
+					int stop = sy;
+					int sbottom;
+
+					if (stop == (srcHeight - 1)) {
+						sbottom = stop;
+					} else {
+						sbottom = stop + 1;
+					}
+
+					ColorBgra* cul = src.GetPointAddressUnchecked (srcDataPtr, srcWidth, sleft, stop);
+					ColorBgra* cur = cul + (sright - sleft);
+					ColorBgra* cll = src.GetPointAddressUnchecked (srcDataPtr, srcWidth, sleft, sbottom);
+					ColorBgra* clr = cll + (sright - sleft);
+
+					ColorBgra c = ColorBgra.BlendColors4W16IP (*cul, wul, *cur, wur, *cll, wll, *clr, wlr);
+					return c;
+				}
+			} else {
+				return ColorBgra.FromUInt32 (0);
+			}
+		}
+
 	}
 }
