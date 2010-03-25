1aa20c4021cf60b7c8aea4b5d97652297da34feb
Jonathan Pobst <monkey@jpobst.com>
2010-03-25 14:24:35 -0500

    Format.

--- Pinta/ConfigurableEffects/FragmentEffect.cs
+++ Pinta/ConfigurableEffects/FragmentEffect.cs
@@ -45,12 +45,12 @@ namespace Pinta.Core
 		}
 
 		public FragmentData Data { get; private set; }
-		
+
 		public FragmentEffect ()
 		{
 			Data = new FragmentData ();
 		}
-		
+
 		public override bool LaunchConfiguration ()
 		{
 			SimpleEffectDialog dialog = new SimpleEffectDialog (Text, PintaCore.Resources.GetIcon (Icon), Data);
@@ -68,82 +68,72 @@ namespace Pinta.Core
 		}
 
 		#region Algorithm Code Ported From PDN
-		
-		private Gdk.Point[] RecalcPointOffsets(int fragments, double rotationAngle, int distance)
-        {
-            double pointStep = 2 * Math.PI / (double)fragments;
-            double rotationRadians = ((rotationAngle - 90.0) * Math.PI) / 180.0;
-            double offsetAngle = pointStep;
+		private Gdk.Point[] RecalcPointOffsets (int fragments, double rotationAngle, int distance)
+		{
+			double pointStep = 2 * Math.PI / (double)fragments;
+			double rotationRadians = ((rotationAngle - 90.0) * Math.PI) / 180.0;
+			double offsetAngle = pointStep;
 
-            Gdk.Point[] pointOffsets = new Gdk.Point[fragments];
+			Gdk.Point[] pointOffsets = new Gdk.Point[fragments];
 
-            for (int i = 0; i < fragments; i++)
-            {
-                double currentRadians = rotationRadians + (pointStep * i);
+			for (int i = 0; i < fragments; i++) {
+				double currentRadians = rotationRadians + (pointStep * i);
 
-                pointOffsets[i] = new Gdk.Point(
-                    (int)Math.Round(distance * -Math.Sin(currentRadians), MidpointRounding.AwayFromZero),
-                    (int)Math.Round(distance * -Math.Cos(currentRadians), MidpointRounding.AwayFromZero));
-            }
+				pointOffsets[i] = new Gdk.Point (
+				    (int)Math.Round (distance * -Math.Sin (currentRadians), MidpointRounding.AwayFromZero),
+				    (int)Math.Round (distance * -Math.Cos (currentRadians), MidpointRounding.AwayFromZero));
+			}
+			
 			return pointOffsets;
-        }
-		
+		}
+
 		public unsafe override void RenderEffect (ImageSurface src, ImageSurface dst, Gdk.Rectangle[] rois)
 		{
 			Gdk.Point[] pointOffsets = RecalcPointOffsets (Data.Fragments, Data.Rotation, Data.Distance);
-			
-			
+
 			int poLength = pointOffsets.Length;
-            Gdk.Point *pointOffsetsPtr = stackalloc Gdk.Point[poLength];
-            for (int i = 0; i < poLength; ++i)
-            {
-                pointOffsetsPtr[i] = pointOffsets[i];
-            }
+			Gdk.Point* pointOffsetsPtr = stackalloc Gdk.Point[poLength];
+			
+			for (int i = 0; i < poLength; ++i)
+				pointOffsetsPtr[i] = pointOffsets[i];
 
-            ColorBgra* samples = stackalloc ColorBgra[poLength];
+			ColorBgra* samples = stackalloc ColorBgra[poLength];
 
-            foreach (Gdk.Rectangle rect in rois)
-            {
-                for (int y = rect.Top; y < rect.Bottom; y++)
-                {
-                    ColorBgra* dstPtr = dst.GetPointAddressUnchecked(rect.Left, y);
+			foreach (Gdk.Rectangle rect in rois) {
+				for (int y = rect.Top; y < rect.Bottom; y++) {
+					ColorBgra* dstPtr = dst.GetPointAddressUnchecked (rect.Left, y);
 
-                    for (int x = rect.Left; x < rect.Right; x++)
-                    {
-                        int sampleCount = 0;
+					for (int x = rect.Left; x < rect.Right; x++) {
+						int sampleCount = 0;
 
-                        for (int i = 0; i < poLength; ++i)
-                        {
-                            int u = x - pointOffsetsPtr[i].X;
-                            int v = y - pointOffsetsPtr[i].Y;
+						for (int i = 0; i < poLength; ++i) {
+							int u = x - pointOffsetsPtr[i].X;
+							int v = y - pointOffsetsPtr[i].Y;
 
-                            if (u >= 0 && u < src.GetBounds().Width && v >= 0 && v < src.GetBounds().Height)
-                            {
-                                samples[sampleCount] = src.GetPointUnchecked(u, v);
-                                ++sampleCount;
-                            }
-                        }
+							if (u >= 0 && u < src.GetBounds ().Width && v >= 0 && v < src.GetBounds ().Height) {
+								samples[sampleCount] = src.GetPointUnchecked (u, v);
+								++sampleCount;
+							}
+						}
 
-                        *dstPtr = ColorBgra.Blend(samples, sampleCount);
-                        ++dstPtr;
-                    }
-                }
-            }
-            
+						*dstPtr = ColorBgra.Blend (samples, sampleCount);
+						++dstPtr;
+					}
+				}
+			}
 		}
 		#endregion
 
 		public class FragmentData
 		{
 			[MinimumValue (2), MaximumValue (50)]
-	        public int Fragments = 4;
-			
+			public int Fragments = 4;
+
 			[MinimumValue (0), MaximumValue (100)]
-	        public int Rotation = 8;
-			
+			public int Rotation = 8;
+
 			[MinimumValue (0), MaximumValue (360)]
-	        public int Distance = 0;
-			
+			public int Distance = 0;
 		}
 	}
 }
