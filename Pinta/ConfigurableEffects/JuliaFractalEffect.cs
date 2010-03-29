b5c689478bc897cfc3122058a9020be8790b4859
dufoli <dufoli@dufoli-laptop.(none)>
2010-03-29 14:18:32 +0200

    Forget to add file in previous commit

---
+++ Pinta/ConfigurableEffects/JuliaFractalEffect.cs
@@ -0,0 +1,165 @@
+// 
+// JuliaFractalEffect.cs
+//  
+// Author:
+//       dufoli <${AuthorEmail}>
+// 
+// Copyright (c) 2010 dufoli
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using Pinta.Gui.Widgets;
+using Cairo;
+
+namespace Pinta.Core
+{
+	public class JuliaFractalEffect : BaseEffect
+	{
+		public override string Icon {
+			get { return "Menu.Effects.Render.JuliaFractal.png"; }
+		}
+
+		public override string Text {
+			get { return Mono.Unix.Catalog.GetString ("Julia Fractal"); }
+		}
+
+		public override bool IsConfigurable {
+			get { return true; }
+		}
+
+		public JuliaFractalData Data { get; private set; }
+
+		public JuliaFractalEffect ()
+		{
+			Data = new JuliaFractalData ();
+		}
+
+		public override bool LaunchConfiguration ()
+		{
+			SimpleEffectDialog dialog = new SimpleEffectDialog (Text, PintaCore.Resources.GetIcon (Icon), Data);
+			
+			int response = dialog.Run ();
+			
+			if (response == (int)Gtk.ResponseType.Ok) {
+				dialog.Destroy ();
+				return true;
+			}
+			
+			dialog.Destroy ();
+			
+			return false;
+		}
+
+		#region Algorithm Code Ported From PDN
+		private static readonly double log2_10000 = Math.Log (10000);
+
+		private static double Julia (double x, double y, double r, double i)
+		{
+			double c = 0;
+			
+			while (c < 256 && x * x + y * y < 10000) {
+				double t = x;
+				x = x * x - y * y + r;
+				y = 2 * t * y + i;
+				++c;
+			}
+			
+			c -= 2 - 2 * log2_10000 / Math.Log (x * x + y * y);
+			
+			return c;
+		}
+
+		unsafe public override void RenderEffect (ImageSurface src, ImageSurface dst, Gdk.Rectangle[] rois)
+		{
+			const double jr = 0.3125;
+			const double ji = 0.03;
+			
+			int w = dst.Width;
+			int h = dst.Height;
+			double invH = 1.0 / h;
+			double invZoom = 1.0 / Data.Zoom;
+			double invQuality = 1.0 / Data.Quality;
+			double aspect = (double)h / (double)w;
+			int count = Data.Quality * Data.Quality + 1;
+			double invCount = 1.0 / (double)count;
+			double angleTheta = (Data.Angle * Math.PI * 2) / 360.0;
+			
+			ColorBgra* dst_dataptr = (ColorBgra*)dst.DataPtr;
+			int dst_width = dst.Width;
+			
+			foreach (Gdk.Rectangle rect in rois) {
+				for (int y = rect.Top; y < rect.Bottom; y++) {
+					ColorBgra* dstPtr = dst.GetPointAddressUnchecked (dst_dataptr, dst_width, rect.Left, y);
+					
+					for (int x = rect.Left; x < rect.Right; x++) {
+						int r = 0;
+						int g = 0;
+						int b = 0;
+						int a = 0;
+						
+						for (double i = 0; i < count; i++) {
+							double u = (2.0 * x - w + (i * invCount)) * invH;
+							double v = (2.0 * y - h + ((i * invQuality) % 1)) * invH;
+							
+							double radius = Math.Sqrt ((u * u) + (v * v));
+							double radiusP = radius;
+							double theta = Math.Atan2 (v, u);
+							double thetaP = theta + angleTheta;
+							
+							double uP = radiusP * Math.Cos (thetaP);
+							double vP = radiusP * Math.Sin (thetaP);
+							
+							double jX = (uP - vP * aspect) * invZoom;
+							double jY = (vP + uP * aspect) * invZoom;
+							
+							double j = Julia (jX, jY, jr, ji);
+							
+							double c = Data.Factor * j;
+							
+							b += Utility.ClampToByte (c - 768);
+							g += Utility.ClampToByte (c - 512);
+							r += Utility.ClampToByte (c - 256);
+							a += Utility.ClampToByte (c - 0);
+						}
+						
+						*dstPtr = ColorBgra.FromBgra (Utility.ClampToByte (b / count), Utility.ClampToByte (g / count), Utility.ClampToByte (r / count), Utility.ClampToByte (a / count));
+						
+						++dstPtr;
+					}
+				}
+			}
+		}
+		#endregion
+
+		public class JuliaFractalData
+		{
+			[MinimumValue(1), MaximumValue(10)]
+			public int Factor = 4;
+
+			[MinimumValue(1), MaximumValue(5)]
+			public int Quality = 2;
+
+			[MinimumValue(0), MaximumValue(50)]
+			public int Zoom = 1;
+
+			public double Angle = 0;
+		}
+	}
+}
