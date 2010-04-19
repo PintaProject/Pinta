1f5114426e760fb1555e46440fcd0664ec3ad041
Jonathan Pobst <monkey@jpobst.com>
2010-04-18 20:55:01 -0500

    Format.

--- Pinta/ConfigurableEffects/SurfaceBlurEffect.cs
+++ Pinta/ConfigurableEffects/SurfaceBlurEffect.cs
@@ -30,14 +30,13 @@ using Pinta.Gui.Widgets;
 
 namespace Pinta.Core
 {
-    public class SurfaceBlurEffect : LocalHistogramEffect
+	public class SurfaceBlurEffect : LocalHistogramEffect
 	{
+		private int radius;
+		private int threshold;
+		private int[] intensityFunction;
 
-        private int radius;
-        private int threshold;
-        private int[] intensityFunction;
-
-        public override string Icon {
+		public override string Icon {
 			get { return "Menu.Effects.Blurs.SurfaceBlur.png"; }
 		}
 
@@ -49,13 +48,13 @@ namespace Pinta.Core
 			get { return true; }
 		}
 
-        public SurfaceBlurData Data { get; private set; }
+		public SurfaceBlurData Data { get; private set; }
 
-        public SurfaceBlurEffect()
+		public SurfaceBlurEffect ()
 		{
-            Data = new SurfaceBlurData();
+			Data = new SurfaceBlurData ();
 		}
-		
+
 		public override bool LaunchConfiguration ()
 		{
 			SimpleEffectDialog dialog = new SimpleEffectDialog (Text, PintaCore.Resources.GetIcon (Icon), Data);
@@ -73,116 +72,98 @@ namespace Pinta.Core
 		}
 
 		#region Algorithm Code Ported From PDN
+		// rather than a fancy function such as a gaussian, 
+		// currently using a trigular function as this seems to be 'good-enough'
+		private static int[] PrecalculateIntensityFunction (int threshold)
+		{
+			int[] factors = new int[256];
 
-        // rather than a fancy function such as a gaussian, 
-        // currently using a trigular function as this seems to be 'good-enough'
-        private static int[] PrecalculateIntensityFunction(int threshold)
-        {
-            int[] factors = new int[256];
+			double slope = 96d / threshold;
 
-            double slope = 96d / threshold;
+			for (int i = 0; i < 256; i++) {
+				int factor = (int)Math.Round (255 - (i * slope), MidpointRounding.AwayFromZero);
 
-            for (int i = 0; i < 256; i++)
-            {
-                int factor = (int)Math.Round(255 - (i * slope), MidpointRounding.AwayFromZero);
+				if (factor < 0)
+					factor = 0;
 
-                if (factor < 0)
-                {
-                    factor = 0;
-                }
+				factors[i] = factor;
+			}
 
-                factors[i] = factor;
-            }
-
-            return factors;
-        }
+			return factors;
+		}
 
 		public unsafe override void RenderEffect (ImageSurface src, ImageSurface dest, Gdk.Rectangle[] rois)
 		{
- 
-            this.radius = Data.Radius;
-            this.threshold = Data.Threshold;
-            this.intensityFunction = PrecalculateIntensityFunction(this.threshold);
+			this.radius = Data.Radius;
+			this.threshold = Data.Threshold;
+			this.intensityFunction = PrecalculateIntensityFunction (this.threshold);
 
-            foreach (Gdk.Rectangle rect in rois)
-            {
-                RenderRect(this.radius, src, dest, rect);
-            }
+			foreach (Gdk.Rectangle rect in rois)
+				RenderRect (this.radius, src, dest, rect);
 
 		}
 
-        public override unsafe ColorBgra Apply(ColorBgra src, int area, int* hb, int* hg, int* hr, int* ha)
-        {
-            int resultB = BlurChannel(src.B, hb);
-            int resultG = BlurChannel(src.G, hg);
-            int resultR = BlurChannel(src.R, hr);
+		public override unsafe ColorBgra Apply (ColorBgra src, int area, int* hb, int* hg, int* hr, int* ha)
+		{
+			int resultB = BlurChannel (src.B, hb);
+			int resultG = BlurChannel (src.G, hg);
+			int resultR = BlurChannel (src.R, hr);
 
-            // there is no way we can deal with pre-multiplied alphas; the correlation 
-            // between channels no longer exists by this point in the algorithm... 
-            // so, just use the alpha from the source pixel.
+			// there is no way we can deal with pre-multiplied alphas; the correlation 
+			// between channels no longer exists by this point in the algorithm... 
+			// so, just use the alpha from the source pixel.
+			ColorBgra result = ColorBgra.FromBgra ((byte)resultB, (byte)resultG, (byte)resultR, src.A);
 
-            ColorBgra result = ColorBgra.FromBgra((byte)resultB, (byte)resultG, (byte)resultR, src.A);
-            return result;
-        }
+			return result;
+		}
 
-        private unsafe int BlurChannel(int current, int* histogram)
-        {
-            // note to self: pointers are passed by-value...
-            //               incrementing passed pointer - no effect outside current scope
-            int sum = 0;
-            int divisor = 0;
-            int result = current;
+		private unsafe int BlurChannel (int current, int* histogram)
+		{
+			// note to self: pointers are passed by-value...
+			//               incrementing passed pointer - no effect outside current scope
+			int sum = 0;
+			int divisor = 0;
+			int result = current;
 
-            for (int bin = 0; bin < 256; bin++)
-            {
-                if (*histogram > 0)
-                {
-                    int diff;
+			for (int bin = 0; bin < 256; bin++) {
+				if (*histogram > 0) {
+					int diff;
 
-                    if (bin > current)
-                    {
-                        diff = bin - current;
-                    }
-                    else
-                    {
-                        diff = current - bin;
-                    }
+					if (bin > current)
+						diff = bin - current;
+					else
+						diff = current - bin;
 
-                    int intensity = this.intensityFunction[diff];
+					int intensity = this.intensityFunction[diff];
 
-                    if (intensity > 0)
-                    {
-                        int t = (*histogram) * intensity;
-                        sum += (t * bin);
-                        divisor += t;
-                    }
-                }
+					if (intensity > 0) {
+						int t = (*histogram) * intensity;
+						sum += (t * bin);
+						divisor += t;
+					}
+				}
 
-                ++histogram;
-            }
+				++histogram;
+			}
 
-            if (divisor > 0)
-            {
-                // 1/2 LSB for integer rounding
-                int roundingTerm = divisor >> 1;
-                result = (sum + roundingTerm) / divisor;
-            }
+			if (divisor > 0) {
+				// 1/2 LSB for integer rounding
+				int roundingTerm = divisor >> 1;
+				result = (sum + roundingTerm) / divisor;
+			}
 
-            return result;
-        }
-
-
+			return result;
+		}
 		#endregion
 
-        public class SurfaceBlurData
+		public class SurfaceBlurData
 		{
+			[MinimumValue (1), MaximumValue (100)]
+			public int Radius = 6;
 
-            [MinimumValue(1), MaximumValue(100)]
-            public int Radius = 6;
+			[MinimumValue (1), MaximumValue (100)]
+			public int Threshold = 15;
 
-            [MinimumValue(1), MaximumValue(100)]
-            public int Threshold = 15;
-			
 			[Skip]
 			public bool IsEmpty { get { return Radius == 0; } }
 		}
