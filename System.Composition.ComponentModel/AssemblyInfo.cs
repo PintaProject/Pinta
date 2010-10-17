7154c6cc823e6bdd29bf6cfc9fae9ed8c21f6bc7
Jonathan Pobst <monkey@jpobst.com>
2010-10-16 22:34:59 -0500

    Add a source copy of Microsoft's System.ComponentModel.Composition.dll (MEF), which is MS-PL licensed.
    http://mef.codeplex.com/

---
+++ System.Composition.ComponentModel/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+[assembly: System.CLSCompliant(true)]
+[assembly: System.Reflection.AssemblyTitle("System.ComponentModel.Composition")]
+[assembly: System.Reflection.AssemblyCopyright("(c) Microsoft Corporation. All rights reserved.")]
+[assembly: System.Reflection.AssemblyVersion("2010.02.11")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("System.ComponentModel.Composition.UnitTests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("System.ComponentModel.Composition.UnitTestFramework")]
