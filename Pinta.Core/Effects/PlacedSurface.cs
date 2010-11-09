682046b2c8d1113d54c07b2fcf95c715c9c6c909
Jonathan Pobst <monkey@jpobst.com>
2010-11-08 23:46:05 -0600

    Reformat some contributed code before I modify it.

--- Pinta.Core/Effects/PlacedSurface.cs
+++ Pinta.Core/Effects/PlacedSurface.cs
@@ -10,186 +10,151 @@ using Cairo;
 
 namespace Pinta.Core
 {
-    /// <summary>
-    /// Encapsulates a surface ("what") along with a pixel offset ("where") which
-    /// defines where the surface would be drawn on to another surface.
-    /// Instances of this object are immutable -- once you create it, you can not
-    /// change it.
-    /// </summary>
-    [Serializable]
-    public sealed class PlacedSurface
-        : ISurfaceDraw,
-          ICloneable
-    {
-        Gdk.Point where;
-        ImageSurface what;
+	/// <summary>
+	/// Encapsulates a surface ("what") along with a pixel offset ("where") which
+	/// defines where the surface would be drawn on to another surface.
+	/// Instances of this object are immutable -- once you create it, you can not
+	/// change it.
+	/// </summary>
+	[Serializable]
+	public sealed class PlacedSurface : ISurfaceDraw, ICloneable
+	{
+		Gdk.Point where;
+		ImageSurface what;
 
-        public Gdk.Point Where
-        {
-            get
-            {
-                if (disposed)
-                {
-                    throw new ObjectDisposedException("PlacedSurface");
-                }
+		#region Constructors
+		private PlacedSurface (PlacedSurface ps)
+		{
+			where = ps.Where;
+			what = ps.What.Clone ();
+		}
 
-                return where;
-            }
-        }
+		private PlacedSurface ()
+		{
+		}
 
-        public ImageSurface What
-        {
-            get
-            {
-                if (disposed)
-                {
-                    throw new ObjectDisposedException("PlacedSurface");
-                }
+		public PlacedSurface (ImageSurface source, Gdk.Rectangle roi)
+		{
+			where = roi.Location;
+			what = new ImageSurface (Format.Argb32, roi.Width, roi.Height);
 
-                return what;
-            }
-        }
-
-        public Gdk.Size Size
-        {
-            get
-            {
-                if (disposed)
-                {
-                    throw new ObjectDisposedException("PlacedSurface");
-                }
-
-                return new Gdk.Size(what.Width, what.Height);
-            }
-        }
-
-        public Gdk.Rectangle Bounds
-        {
-            get
-            {
-                if (disposed)
-                {
-                    throw new ObjectDisposedException("PlacedSurface");
-                }
-
-                return new Gdk.Rectangle(Where, Size);
-            }
-        }
-
-        public void Draw(ImageSurface dst)
-        {
-            if (disposed)
-            {
-                throw new ObjectDisposedException("PlacedSurface");
-            }
-
-            using (Cairo.Context g = new Cairo.Context(dst))
-                g.DrawPixbuf(what.ToPixbuf(), new Cairo.Point(where.X, where.Y));
-        }
-
-        public void Draw(ImageSurface dst, PixelOp pixelOp)
-        {
-            if (disposed)
-            {
-                throw new ObjectDisposedException("PlacedSurface");
-            }
-
-            Gdk.Rectangle dstRect = Bounds;
-            Gdk.Rectangle dstClip = Gdk.Rectangle.Intersect(dstRect, dst.GetBounds());
-
-            if (dstClip.Width > 0 && dstClip.Height > 0)
-            {
-                int dtX = dstClip.X - where.X;
-                int dtY = dstClip.Y - where.Y;
-
-                pixelOp.Apply(dst, dstClip.Location, what, new Gdk.Point(dtX, dtY), dstClip.Size);
-            }
-        }
-
-        public void Draw(ImageSurface dst, int tX, int tY)
-        {
-            if (disposed)
-            {
-                throw new ObjectDisposedException("PlacedSurface");
-            }
-
-            Gdk.Point oldWhere = where;
-
-            try
-            {
-                where.X += tX;
-                where.Y += tY;
-                Draw(dst);
-            }
-
-            finally
-            {
-                where = oldWhere;
-            }
-        }
-
-        public void Draw(ImageSurface dst, int tX, int tY, PixelOp pixelOp)
-        {
-            if (disposed)
-            {
-                throw new ObjectDisposedException("PlacedSurface");
-            }
-
-            Gdk.Point oldWhere = where;
-
-            try
-            {
-                where.X += tX;
-                where.Y += tY;
-                Draw(dst, pixelOp);
-            }
-
-            finally
-            {
-                where = oldWhere;
-            }
-        }
-
-        public PlacedSurface(ImageSurface source, Gdk.Rectangle roi)
-        {
-            where = roi.Location;
-			what = new ImageSurface(Format.Argb32, roi.Width, roi.Height);
-			
 			using (Context g = new Context (what)) {
 				g.SetSourceSurface (source, -roi.X, -roi.Y);
 				g.Paint ();
 			}
-        }
+		}
+		#endregion
 
-        private PlacedSurface(PlacedSurface ps)
-        {
-            where = ps.Where;
-            what = ps.What.Clone();
-        }
+		#region Public Properties
+		public Gdk.Point Where {
+			get {
+				if (disposed)
+					throw new ObjectDisposedException ("PlacedSurface");
 
-        private PlacedSurface()
-        {
-        }
+				return where;
+			}
+		}
 
-        #region IDisposable Members
-        private bool disposed = false;
+		public ImageSurface What {
+			get {
+				if (disposed)
+					throw new ObjectDisposedException ("PlacedSurface");
 
-        #endregion
+				return what;
+			}
+		}
 
+		public Gdk.Size Size {
+			get {
+				if (disposed)
+					throw new ObjectDisposedException ("PlacedSurface");
 
-        #region ICloneable Members
+				return new Gdk.Size (what.Width, what.Height);
+			}
+		}
 
-        public object Clone()
-        {
-            if (disposed)
-            {
-                throw new ObjectDisposedException("PlacedSurface");
-            }
+		public Gdk.Rectangle Bounds {
+			get {
+				if (disposed)
+					throw new ObjectDisposedException ("PlacedSurface");
 
-            return new PlacedSurface(this);
-        }
+				return new Gdk.Rectangle (Where, Size);
+			}
+		}
+		#endregion
 
-        #endregion
-    }
+		#region Public Methods
+		public void Draw (ImageSurface dst)
+		{
+			if (disposed)
+				throw new ObjectDisposedException ("PlacedSurface");
+
+			using (Cairo.Context g = new Cairo.Context (dst))
+				g.DrawPixbuf (what.ToPixbuf (), new Cairo.Point (where.X, where.Y));
+		}
+
+		public void Draw (ImageSurface dst, PixelOp pixelOp)
+		{
+			if (disposed)
+				throw new ObjectDisposedException ("PlacedSurface");
+
+			Gdk.Rectangle dstRect = Bounds;
+			Gdk.Rectangle dstClip = Gdk.Rectangle.Intersect (dstRect, dst.GetBounds ());
+
+			if (dstClip.Width > 0 && dstClip.Height > 0) {
+				int dtX = dstClip.X - where.X;
+				int dtY = dstClip.Y - where.Y;
+
+				pixelOp.Apply (dst, dstClip.Location, what, new Gdk.Point (dtX, dtY), dstClip.Size);
+			}
+		}
+
+		public void Draw (ImageSurface dst, int tX, int tY)
+		{
+			if (disposed)
+				throw new ObjectDisposedException ("PlacedSurface");
+
+			Gdk.Point oldWhere = where;
+
+			try {
+				where.X += tX;
+				where.Y += tY;
+				Draw (dst);
+			} finally {
+				where = oldWhere;
+			}
+		}
+
+		public void Draw (ImageSurface dst, int tX, int tY, PixelOp pixelOp)
+		{
+			if (disposed)
+				throw new ObjectDisposedException ("PlacedSurface");
+
+			Gdk.Point oldWhere = where;
+
+			try {
+				where.X += tX;
+				where.Y += tY;
+				Draw (dst, pixelOp);
+			} finally {
+				where = oldWhere;
+			}
+		}
+		#endregion
+
+		#region IDisposable Members
+		private bool disposed = false;
+		#endregion
+
+		#region ICloneable Members
+		public object Clone ()
+		{
+			if (disposed)
+				throw new ObjectDisposedException ("PlacedSurface");
+
+			return new PlacedSurface (this);
+		}
+		#endregion
+	}
 }
 
