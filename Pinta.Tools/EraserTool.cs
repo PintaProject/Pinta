06ebf2f26d80c8e702270b8fdd0c714b5316ef7e
Jonathan Pobst <monkey@jpobst.com>
2010-11-09 23:33:56 -0600

    Make the eraser respect the antialias setting.
    [Fixes bug #671508]

--- Pinta.Tools/EraserTool.cs
+++ Pinta.Tools/EraserTool.cs
@@ -77,7 +77,7 @@ namespace Pinta.Tools
 				g.FillRule = FillRule.EvenOdd;
 				g.Clip ();
 
-				g.Antialias = Antialias.Subpixel;
+				g.Antialias = UseAntialiasing ? Antialias.Subpixel : Antialias.None;
 				
 				g.MoveTo (last_point.X, last_point.Y);
 				g.LineTo (x, y);
