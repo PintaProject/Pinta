d32a7013ad8e0b06e995d2cc2faa239382f87173
Jonathan Pobst <monkey@jpobst.com>
2011-03-27 14:40:46 -0500

    Remove MEF.

--- System.ComponentModel.Composition/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-//------------------------------------------------------------
-[assembly: System.CLSCompliant(true)]
-[assembly: System.Reflection.AssemblyTitle("System.ComponentModel.Composition")]
-[assembly: System.Reflection.AssemblyCopyright("(c) Microsoft Corporation. All rights reserved.")]
-[assembly: System.Reflection.AssemblyVersion("2010.02.11")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("System.ComponentModel.Composition.UnitTests")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("System.ComponentModel.Composition.UnitTestFramework")]
