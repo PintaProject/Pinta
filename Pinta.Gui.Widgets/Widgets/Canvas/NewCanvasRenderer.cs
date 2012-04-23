5845e76de3a59c39e7318c2085c1dd2debe0c696
Jonathan Pobst <monkey@jpobst.com>
2012-04-22 22:42:01 -0500

    [WIP] Add support for zoomed [in|out] canvases.

--- Pinta.Gui.Widgets/Widgets/Canvas/NewCanvasRenderer.cs
+++ Pinta.Gui.Widgets/Widgets/Canvas/NewCanvasRenderer.cs
@@ -50,10 +50,10 @@ namespace Pinta.Gui.Widgets
 		
 			if (scale_factor.Ratio == 1)
 				RenderOneToOne (layers, dst, offset, checker);
-			//else if (scale_factor.Ratio < 1)
-			//        RenderZoomIn (src, dst, offset, checker);
-			//else
-			//        RenderZoomOut (src, dst, offset, destination_size, checker);
+			else if (scale_factor.Ratio < 1)
+				RenderZoomIn (layers, dst, offset, checker);
+			else
+				RenderZoomOut (layers, dst, offset, destination_size, checker);
 			
 			dst.MarkDirty ();
 		}
@@ -122,130 +122,154 @@ namespace Pinta.Gui.Widgets
 			}
 		}
 
-		private unsafe void RenderZoomIn (Cairo.ImageSurface src, Cairo.ImageSurface dst, Gdk.Point offset, bool checker)
+		private unsafe void RenderZoomIn (IEnumerable<Layer> layers, Cairo.ImageSurface dst, Gdk.Point offset, bool checker)
 		{
-			ColorBgra* src_ptr = (ColorBgra*)src.DataPtr;
-			ColorBgra* dst_ptr = (ColorBgra*)dst.DataPtr;
+			checker = true;
 
-			int src_width = src.Width;
-			int dst_width = dst.Width;
-			int dst_height = dst.Height;
+			foreach (var layer in layers) {
+				var src = layer.Surface;
 
-			if (!generated) {
-				d2sLookupX = CreateLookupX (src_width, destination_size.Width, scale_factor);
-				d2sLookupY = CreateLookupY (src.Height, destination_size.Height, scale_factor);
-				s2dLookupX = CreateS2DLookupX (src_width, destination_size.Width, scale_factor);
-				s2dLookupY = CreateS2DLookupY (src.Height, destination_size.Height, scale_factor);
+				// Get the blend mode for this layer and opacity
+				var blend_op = UserBlendOps.GetBlendOp (layer.BlendMode, layer.Opacity);
+				
+				ColorBgra* src_ptr = (ColorBgra*)src.DataPtr;
+				ColorBgra* dst_ptr = (ColorBgra*)dst.DataPtr;
 
-				generated = true;
-			}
+				int src_width = src.Width;
+				int dst_width = dst.Width;
+				int dst_height = dst.Height;
 
-			for (int dstRow = 0; dstRow < dst_height; ++dstRow) {
-				int nnY = dstRow + offset.Y;
-				int srcY = d2sLookupY[nnY];
+				if (!generated) {
+					d2sLookupX = CreateLookupX (src_width, destination_size.Width, scale_factor);
+					d2sLookupY = CreateLookupY (src.Height, destination_size.Height, scale_factor);
+					s2dLookupX = CreateS2DLookupX (src_width, destination_size.Width, scale_factor);
+					s2dLookupY = CreateS2DLookupY (src.Height, destination_size.Height, scale_factor);
 
-				ColorBgra* dstPtr = dst.GetRowAddressUnchecked (dst_ptr, dst_width, dstRow);
-				ColorBgra* srcRow = src.GetRowAddressUnchecked (src_ptr, src_width, srcY);
-
-				for (int dstCol = 0; dstCol < dst_width; ++dstCol) {
-					int nnX = dstCol + offset.X;
-					int srcX = d2sLookupX[nnX];
-					
-					if (checker) {
-						ColorBgra src2 = *(srcRow + srcX);
-						int b = src2.B;
-						int g = src2.G;
-						int r = src2.R;
-						int a = src2.A;
-
-						// Blend it over the checkerboard background
-						int v = (((dstCol + offset.X) ^ (dstRow + offset.Y)) & 8) * 8 + 191;
-						a = a + (a >> 7);
-						int vmia = v * (256 - a);
-
-						r = ((r * a) + vmia) >> 8;
-						g = ((g * a) + vmia) >> 8;
-						b = ((b * a) + vmia) >> 8;
-
-						dstPtr->Bgra = (uint)b + ((uint)g << 8) + ((uint)r << 16) + ((uint)255 << 24);
-					} else {
-						*dstPtr = *(srcRow + srcX);
-					}
-
-					++dstPtr;
+					generated = true;
 				}
+
+				for (int dstRow = 0; dstRow < dst_height; ++dstRow) {
+					int nnY = dstRow + offset.Y;
+					int srcY = d2sLookupY[nnY];
+
+					ColorBgra* dstPtr = dst.GetRowAddressUnchecked (dst_ptr, dst_width, dstRow);
+					ColorBgra* srcRow = src.GetRowAddressUnchecked (src_ptr, src_width, srcY);
+
+					for (int dstCol = 0; dstCol < dst_width; ++dstCol) {
+						int nnX = dstCol + offset.X;
+						int srcX = d2sLookupX[nnX];
+					
+						if (checker) {
+							ColorBgra src2 = *(srcRow + srcX);
+							int b = src2.B;
+							int g = src2.G;
+							int r = src2.R;
+							int a = src2.A;
+
+							// Blend it over the checkerboard background
+							int v = (((dstCol + offset.X) ^ (dstRow + offset.Y)) & 8) * 8 + 191;
+							a = a + (a >> 7);
+							int vmia = v * (256 - a);
+
+							r = ((r * a) + vmia) >> 8;
+							g = ((g * a) + vmia) >> 8;
+							b = ((b * a) + vmia) >> 8;
+
+							dstPtr->Bgra = (uint)b + ((uint)g << 8) + ((uint)r << 16) + ((uint)255 << 24);
+						} else {
+							*dstPtr = blend_op.Apply (*dstPtr, *(srcRow + srcX));
+						}
+
+						++dstPtr;
+					}
+				}
+
+				// Only checker the first layer
+				checker = false;
 			}
 		}
 
-		private unsafe void RenderZoomOut (Cairo.ImageSurface src, Cairo.ImageSurface dst, Gdk.Point offset, Gdk.Size destinationSize, bool checker)
+		private unsafe void RenderZoomOut (IEnumerable<Layer> layers, Cairo.ImageSurface dst, Gdk.Point offset, Gdk.Size destinationSize, bool checker)
 		{
-			const int fpShift = 12;
-			const int fpFactor = (1 << fpShift);
+			checker = true;
 
-			Gdk.Size sourceSize = src.GetBounds ().Size;
-			long fDstLeftLong = ((long)offset.X * fpFactor * (long)sourceSize.Width) / (long)destinationSize.Width;
-			long fDstTopLong = ((long)offset.Y * fpFactor * (long)sourceSize.Height) / (long)destinationSize.Height;
-			long fDstRightLong = ((long)(offset.X + dst.Width) * fpFactor * (long)sourceSize.Width) / (long)destinationSize.Width;
-			long fDstBottomLong = ((long)(offset.Y + dst.Height) * fpFactor * (long)sourceSize.Height) / (long)destinationSize.Height;
-			int fDstLeft = (int)fDstLeftLong;
-			int fDstTop = (int)fDstTopLong;
-			int fDstRight = (int)fDstRightLong;
-			int fDstBottom = (int)fDstBottomLong;
-			int dx = (fDstRight - fDstLeft) / dst.Width;
-			int dy = (fDstBottom - fDstTop) / dst.Height;
+			foreach (var layer in layers) {
+				var src = layer.Surface;
 
-			ColorBgra* src_ptr = (ColorBgra*)src.DataPtr;
-			ColorBgra* dst_ptr = (ColorBgra*)dst.DataPtr;
-			int src_width = src.Width;
-			int dst_width = dst.Width;
+				// Get the blend mode for this layer and opacity
+				var blend_op = UserBlendOps.GetBlendOp (layer.BlendMode, layer.Opacity);
+				
+				const int fpShift = 12;
+				const int fpFactor = (1 << fpShift);
+
+				Gdk.Size sourceSize = src.GetBounds ().Size;
+				long fDstLeftLong = ((long)offset.X * fpFactor * (long)sourceSize.Width) / (long)destinationSize.Width;
+				long fDstTopLong = ((long)offset.Y * fpFactor * (long)sourceSize.Height) / (long)destinationSize.Height;
+				long fDstRightLong = ((long)(offset.X + dst.Width) * fpFactor * (long)sourceSize.Width) / (long)destinationSize.Width;
+				long fDstBottomLong = ((long)(offset.Y + dst.Height) * fpFactor * (long)sourceSize.Height) / (long)destinationSize.Height;
+				int fDstLeft = (int)fDstLeftLong;
+				int fDstTop = (int)fDstTopLong;
+				int fDstRight = (int)fDstRightLong;
+				int fDstBottom = (int)fDstBottomLong;
+				int dx = (fDstRight - fDstLeft) / dst.Width;
+				int dy = (fDstBottom - fDstTop) / dst.Height;
+
+				ColorBgra* src_ptr = (ColorBgra*)src.DataPtr;
+				ColorBgra* dst_ptr = (ColorBgra*)dst.DataPtr;
+				int src_width = src.Width;
+				int dst_width = dst.Width;
 			
-			for (int dstRow = 0, fDstY = fDstTop; dstRow < dst.Height && fDstY < fDstBottom; ++dstRow, fDstY += dy) {
-				int srcY1 = fDstY >> fpShift;                            // y
-				int srcY2 = (fDstY + (dy >> 2)) >> fpShift;              // y + 0.25
-				int srcY3 = (fDstY + (dy >> 1)) >> fpShift;              // y + 0.50
-				int srcY4 = (fDstY + (dy >> 1) + (dy >> 2)) >> fpShift;  // y + 0.75
+				for (int dstRow = 0, fDstY = fDstTop; dstRow < dst.Height && fDstY < fDstBottom; ++dstRow, fDstY += dy) {
+					int srcY1 = fDstY >> fpShift;                            // y
+					int srcY2 = (fDstY + (dy >> 2)) >> fpShift;              // y + 0.25
+					int srcY3 = (fDstY + (dy >> 1)) >> fpShift;              // y + 0.50
+					int srcY4 = (fDstY + (dy >> 1) + (dy >> 2)) >> fpShift;  // y + 0.75
 
-				ColorBgra* src1 = src.GetRowAddressUnchecked (src_ptr, src_width, srcY1);
-				ColorBgra* src2 = src.GetRowAddressUnchecked (src_ptr, src_width, srcY2);
-				ColorBgra* src3 = src.GetRowAddressUnchecked (src_ptr, src_width, srcY3);
-				ColorBgra* src4 = src.GetRowAddressUnchecked (src_ptr, src_width, srcY4);
-				ColorBgra* dstPtr = dst.GetRowAddressUnchecked (dst_ptr, dst_width, dstRow);
-				int checkerY = dstRow + offset.Y;
-				int checkerX = offset.X;
-				int maxCheckerX = checkerX + dst.Width;
+					ColorBgra* src1 = src.GetRowAddressUnchecked (src_ptr, src_width, srcY1);
+					ColorBgra* src2 = src.GetRowAddressUnchecked (src_ptr, src_width, srcY2);
+					ColorBgra* src3 = src.GetRowAddressUnchecked (src_ptr, src_width, srcY3);
+					ColorBgra* src4 = src.GetRowAddressUnchecked (src_ptr, src_width, srcY4);
+					ColorBgra* dstPtr = dst.GetRowAddressUnchecked (dst_ptr, dst_width, dstRow);
+					int checkerY = dstRow + offset.Y;
+					int checkerX = offset.X;
+					int maxCheckerX = checkerX + dst.Width;
 
-				for (int fDstX = fDstLeft; checkerX < maxCheckerX && fDstX < fDstRight; ++checkerX, fDstX += dx) {
-					int srcX1 = (fDstX + (dx >> 2)) >> fpShift;             // x + 0.25
-					int srcX2 = (fDstX + (dx >> 1) + (dx >> 2)) >> fpShift; // x + 0.75
-					int srcX3 = fDstX >> fpShift;                           // x
-					int srcX4 = (fDstX + (dx >> 1)) >> fpShift;             // x + 0.50
-					ColorBgra* p1 = src1 + srcX1;
-					ColorBgra* p2 = src2 + srcX2;
-					ColorBgra* p3 = src3 + srcX3;
-					ColorBgra* p4 = src4 + srcX4;
+					for (int fDstX = fDstLeft; checkerX < maxCheckerX && fDstX < fDstRight; ++checkerX, fDstX += dx) {
+						int srcX1 = (fDstX + (dx >> 2)) >> fpShift;             // x + 0.25
+						int srcX2 = (fDstX + (dx >> 1) + (dx >> 2)) >> fpShift; // x + 0.75
+						int srcX3 = fDstX >> fpShift;                           // x
+						int srcX4 = (fDstX + (dx >> 1)) >> fpShift;             // x + 0.50
+						ColorBgra* p1 = src1 + srcX1;
+						ColorBgra* p2 = src2 + srcX2;
+						ColorBgra* p3 = src3 + srcX3;
+						ColorBgra* p4 = src4 + srcX4;
 
-					int r = (2 + p1->R + p2->R + p3->R + p4->R) >> 2;
-					int g = (2 + p1->G + p2->G + p3->G + p4->G) >> 2;
-					int b = (2 + p1->B + p2->B + p3->B + p4->B) >> 2;
-					int a = (2 + p1->A + p2->A + p3->A + p4->A) >> 2;
+						int r = (2 + p1->R + p2->R + p3->R + p4->R) >> 2;
+						int g = (2 + p1->G + p2->G + p3->G + p4->G) >> 2;
+						int b = (2 + p1->B + p2->B + p3->B + p4->B) >> 2;
+						int a = (2 + p1->A + p2->A + p3->A + p4->A) >> 2;
 
-					if (checker) {
-						// Blend it over the checkerboard background
-						int v = ((checkerX ^ checkerY) & 8) * 8 + 191;
-						a = a + (a >> 7);
-						int vmia = v * (256 - a);
+						if (checker) {
+							// Blend it over the checkerboard background
+							int v = ((checkerX ^ checkerY) & 8) * 8 + 191;
+							a = a + (a >> 7);
+							int vmia = v * (256 - a);
 
-						r = ((r * a) + vmia) >> 8;
-						g = ((g * a) + vmia) >> 8;
-						b = ((b * a) + vmia) >> 8;
+							r = ((r * a) + vmia) >> 8;
+							g = ((g * a) + vmia) >> 8;
+							b = ((b * a) + vmia) >> 8;
 
-						dstPtr->Bgra = (uint)b + ((uint)g << 8) + ((uint)r << 16) + 0xff000000;
-					} else {
-						dstPtr->Bgra = (uint)b + ((uint)g << 8) + ((uint)r << 16) + ((uint)a << 24);
-					}
+							dstPtr->Bgra = (uint)b + ((uint)g << 8) + ((uint)r << 16) + 0xff000000;
+						} else {
+							*dstPtr = blend_op.Apply (*dstPtr, ColorBgra.FromUInt32 ((uint)b + ((uint)g << 8) + ((uint)r << 16) + ((uint)a << 24)));
+						}
 					
-					++dstPtr;
+						++dstPtr;
+					}
 				}
+
+				// Only checker the first layer
+				checker = false;
 			}
 		}
 
