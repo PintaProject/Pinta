b0df4395a564c255a0250995b96cbb05ef3d8732
Cameron White <cameronwhite91@gmail.com>
2012-05-02 16:06:07 -0400

    Bump version numbers to 1.4.

--- Pinta.Effects/Properties/AssemblyInfo.cs
+++ Pinta.Effects/Properties/AssemblyInfo.cs
@@ -33,10 +33,10 @@ using Mono.Addins;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.3.0.0")]
-[assembly: AssemblyFileVersion ("1.3.0.0")]
+[assembly: AssemblyVersion ("1.4.0.0")]
+[assembly: AssemblyFileVersion ("1.4.0.0")]
 
-[assembly: Addin ("DefaultEffects", "1.3", Category = "Core")]
+[assembly: Addin ("DefaultEffects", "1.4", Category = "Core")]
 [assembly: AddinName ("Default Effects")]
 [assembly: AddinDescription ("The default adjustments and effects that ship with Pinta")]
-[assembly: AddinDependency ("Pinta", "1.3")]
+[assembly: AddinDependency ("Pinta", "1.4")]
--- Pinta/Dialogs/AboutPintaTabPage.cs
+++ Pinta/Dialogs/AboutPintaTabPage.cs
@@ -39,7 +39,7 @@ namespace Pinta
 			label.Markup = String.Format (
 			    "<b>{0}</b>\n    {1}",
 			    Catalog.GetString ("Version"),
-			    "1.3");
+			    "1.4");
 			    
 			HBox hBoxVersion = new HBox ();
 			hBoxVersion.PackStart (label, false, false, 5);
