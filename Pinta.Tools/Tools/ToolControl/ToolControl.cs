066440316f9e322d45525c25fb48bdc9baebe2a5
Robert Nordan <rpvn@robpvn.net>
2012-07-05 21:45:47 +0200

    Fix for #1021426, fix licence header problems

--- Pinta.Tools/Tools/ToolControl/ToolControl.cs
+++ Pinta.Tools/Tools/ToolControl/ToolControl.cs
@@ -4,7 +4,7 @@
 // Author:
 //       Olivier Dufour <olivier.duff@gmail.com>
 // 
-// Copyright (c) 2011 
+// Copyright (c) 2011 Olivier Dufour
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
--- Pinta/Pads/OpenImagesPad.cs
+++ Pinta/Pads/OpenImagesPad.cs
@@ -4,7 +4,7 @@
 // Author:
 //       Cameron White <cameronwhite91@gmail.com>
 // 
-// Copyright (c) 2011 2011
+// Copyright (c) 2011 Cameron White
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
