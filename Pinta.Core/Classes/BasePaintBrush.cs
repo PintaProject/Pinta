80764af0e5a0e7723156f85a54197d592ede501a
Cameron White <cameronwhite91@gmail.com>
2013-06-15 19:05:39 -0400

    Add some documentation for brush-related APIs.

--- Pinta.Core/Classes/BasePaintBrush.cs
+++ Pinta.Core/Classes/BasePaintBrush.cs
@@ -30,21 +30,39 @@ using Cairo;
 
 namespace Pinta.Core
 {
+	/// <summary>
+	/// The base class for all brushes.
+	/// </summary>
 	[TypeExtensionPoint]
 	public abstract class BasePaintBrush
 	{
 		private static Random random = new Random ();
 
+		/// <summary>
+		/// The name of the brush.
+		/// </summary>
 		public abstract string Name { get; }
 
+		/// <summary>
+		/// Priority value for ordering brushes. If the priority is zero, then
+		/// alphabetical ordering is used.
+		/// </summary>
 		public virtual int Priority {
 			get { return 0; }
 		}
 
+		/// <summary>
+		/// Random number generator. This can be used to implement brushes with
+		/// random effects.
+		/// </summary>
 		public Random Random {
 			get { return random; }
 		}
 
+		/// <summary>
+		/// Used to multiply the alpha value of the stroke color by a
+		/// constant factor.
+		/// </summary>
 		public virtual double StrokeAlphaMultiplier {
 			get { return 1; }
 		}
@@ -65,14 +83,31 @@ namespace Pinta.Core
 			return OnMouseMove (g, strokeColor, surface, x, y, lastX, lastY);
 		}
 
+		/// <summary>
+		/// Event handler called when the mouse is released.
+		/// </summary>
 		protected virtual void OnMouseUp ()
 		{
 		}
 
+		/// <summary>
+		/// Event handler called when the mouse is pressed down.
+		/// </summary>
 		protected virtual void OnMouseDown ()
 		{
 		}
 
+		/// <summary>
+		/// Event handler called when the mouse is moved. This method is where
+		/// the brush should perform its drawing.
+		/// </summary>
+		/// <param name="g">The current Cairo drawing context.</param>
+		/// <param name="strokeColor">The current stroke color.</param>
+		/// <param name="surface">Image surface to draw on.</param>
+		/// <param name="x">The current x coordinate of the mouse.</param>
+		/// <param name="y">The current y coordinate of the mouse.</param>
+		/// <param name="lastX">The previous x coordinate of the mouse.</param>
+		/// <param name="lastY">The previous y coordinate of the mouse.</param>
 		protected abstract Gdk.Rectangle OnMouseMove (Context g, Color strokeColor, ImageSurface surface,
 		                                              int x, int y, int lastX, int lastY);
 	}
