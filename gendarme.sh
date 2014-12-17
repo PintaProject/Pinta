eec3f18f20a1c90ec2b6ae78cc22e3630e47bd97
horkana <alanhorkan@gmail.com>
2014-12-17 22:36:06 +0000

    add gendarme to travis automatic build

---
+++ gendarme.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+# Gendarme - Rule-based code analysis for Mono C#
+gendarme --log log.txt --xml log.xml  --html log.html --severity critical --confidence total --quiet assemblies ./bin/*.exe ./bin/*/exe
