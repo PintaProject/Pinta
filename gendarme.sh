27320deb6c4a667c57ec931b50ce843a8ca468db
horkana <alanhorkan@gmail.com>
2014-12-18 12:43:42 +0000

    after build, test using gendarme (part of mono tools)

--- gendarme.sh
+++ gendarme.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 # Gendarme - Rule-based code analysis for Mono C#
-gendarme --log log.txt --xml log.xml  --html log.html --severity critical --confidence total --quiet assemblies ./bin/*.exe ./bin/*/exe
+gendarme --log log.txt --xml log.xml  --html log.html --severity critical --confidence total --quiet assemblies ./bin/Pinta*.dll ./bin/Pinta.exe
