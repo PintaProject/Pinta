04d642423e56b6f2a7e7e6bdb8aff84042722806
horkana <alanhorkan@gmail.com>
2014-12-18 15:43:39 +0000

    gendarme.sh was too quiet, can't see the logfile from Travis console

--- gendarme.sh
+++ gendarme.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 # Gendarme - Rule-based code analysis for Mono C#
-gendarme --log log.txt --xml log.xml  --html log.html --severity critical --confidence total --quiet ./bin/Pinta.*.dll ./bin/Pinta.exe
+gendarme --log log.txt --xml log.xml  --html log.html --severity critical --confidence total ./bin/Pinta.*.dll ./bin/Pinta.exe
