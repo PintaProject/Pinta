e0e687353be78316b46a58e6a7eb8a9b308c5a46
Cameron White <cameronwhite91@gmail.com>
2015-02-19 21:14:22 -0500

    Fix translation issues in the Posterize dialog.
    I also removed the generated code while I was in here.
    
    Bug: #1191928

--- Pinta.Effects/Dialogs/Effects.PosterizeDialog.cs
+++ Pinta.Effects/Dialogs/Effects.PosterizeDialog.cs
@@ -32,56 +32,101 @@ using Pinta.Gui.Widgets;
 
 namespace Pinta.Effects
 {
-	public partial class PosterizeDialog : Gtk.Dialog
+	public class PosterizeDialog : Gtk.Dialog
 	{
+		private HScaleSpinButtonWidget red_spinbox;
+		private HScaleSpinButtonWidget green_spinbox;
+		private HScaleSpinButtonWidget blue_spinbox;
+		private CheckButton link_button;
+
 		public int Red {
-			get { return hscalespinRed.ValueAsInt; }
+			get { return red_spinbox.ValueAsInt; }
 		}
 		
 		public int Green { 
-			get { return hscalespinGreen.ValueAsInt; }
+			get { return green_spinbox.ValueAsInt; }
 		}
 		
 		public int Blue {
-            		get { return hscalespinBlue.ValueAsInt; }
+            get { return blue_spinbox.ValueAsInt; }
 		}
+
+		public PosterizeData EffectData { get; set; }
 
 		public PosterizeDialog () : base (Catalog.GetString ("Posterize"),
 		                                  PintaCore.Chrome.MainWindow, DialogFlags.Modal)
 		{
 			Build ();
 			
-			hscalespinRed.ValueChanged += HandleValueChanged;
-			hscalespinGreen.ValueChanged += HandleValueChanged;
-			hscalespinBlue.ValueChanged += HandleValueChanged;
+			red_spinbox.ValueChanged += HandleValueChanged;
+			green_spinbox.ValueChanged += HandleValueChanged;
+			blue_spinbox.ValueChanged += HandleValueChanged;
 
 			AlternativeButtonOrder = new int[] { (int) Gtk.ResponseType.Ok, (int) Gtk.ResponseType.Cancel };
 			DefaultResponse = Gtk.ResponseType.Ok;
 		}
 		
-		public PosterizeData EffectData { get; set; }
-		
-		void HandleValueChanged (object sender, EventArgs e)
+		private void HandleValueChanged (object sender, EventArgs e)
 		{
 			var widget = sender as HScaleSpinButtonWidget;
 			
-			if (checkLinked.Active)
-				hscalespinGreen.Value = hscalespinBlue.Value = hscalespinRed.Value = widget.Value;
+			if (link_button.Active)
+				green_spinbox.Value = blue_spinbox.Value = red_spinbox.Value = widget.Value;
 			
 			UpdateEffectData ();
 		}
 		
-		void UpdateEffectData ()
+		private void UpdateEffectData ()
 		{
 			if (EffectData == null)
 				return;
 			
-			EffectData.Red = hscalespinRed.ValueAsInt;
-			EffectData.Green = hscalespinGreen.ValueAsInt;
-			EffectData.Blue = hscalespinBlue.ValueAsInt;
+			EffectData.Red = red_spinbox.ValueAsInt;
+			EffectData.Green = green_spinbox.ValueAsInt;
+			EffectData.Blue = blue_spinbox.ValueAsInt;
 			
 			// Only fire event once, even if all properties have changed.
 			EffectData.FirePropertyChanged ("_all_");
+		}
+
+		private void InitSpinBox (HScaleSpinButtonWidget spinbox)
+		{
+			spinbox.DefaultValue = 16;
+			spinbox.MaximumValue = 64;
+			spinbox.MinimumValue = 2;
+			VBox.Add (spinbox);
+		}
+
+		private void Build ()
+		{
+			Resizable = false;
+
+			VBox.WidthRequest = 400;
+			VBox.BorderWidth = 6;
+			VBox.Spacing = 6;
+
+			red_spinbox = new HScaleSpinButtonWidget ();
+			red_spinbox.Label = Catalog.GetString ("Red");
+			InitSpinBox (red_spinbox);
+
+			green_spinbox = new HScaleSpinButtonWidget ();
+			green_spinbox.Label = Catalog.GetString ("Green");
+			InitSpinBox (green_spinbox);
+
+			blue_spinbox = new HScaleSpinButtonWidget ();
+			blue_spinbox.Label = Catalog.GetString ("Blue");
+			InitSpinBox (blue_spinbox);
+
+			link_button = new CheckButton (Catalog.GetString ("Linked"));
+			link_button.Active = true;
+			VBox.Add (link_button);
+
+			AddButton (Stock.Cancel, ResponseType.Cancel);
+			AddButton (Stock.Ok, ResponseType.Ok);
+
+			DefaultWidth = 400;
+			DefaultHeight = 300;
+			ShowAll ();
 		}
 	}
 }
