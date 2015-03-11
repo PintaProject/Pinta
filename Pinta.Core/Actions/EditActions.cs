PintaProject/Pinta

commit 4291e4ba2d24e0072439ae8b518de0fd704f97a2

Lazily create the selection path.
This simplifies the code quite a bit, and saves some work when dealing
with selection modes.

Bug: #1428740

Pinta.Core/Actions/EditActions.cs
@@ -227,11 +227,8 @@ namespace Pinta.Core
 			SelectionHistoryItem hist = new SelectionHistoryItem ("Menu.Edit.Deselect.png", Catalog.GetString ("Deselect"));
 			hist.TakeSnapshot ();
 
-			doc.ResetSelectionPaths ();
-			doc.Selection.SelectionPolygons.Clear();
-            doc.PreviousSelection.SelectionPolygons.Clear ();
-			doc.Selection.Origin = new PointD();
-			doc.Selection.End = new PointD();
+            doc.Selection.Clear ();
+            doc.PreviousSelection.Clear ();
 
 			doc.History.PushNewItem (hist);
 			doc.Workspace.Invalidate ();

Pinta.Core/Classes/Document.cs
@@ -611,8 +611,9 @@ namespace Pinta.Core
 		
 		public void ResetSelectionPaths()
 		{
-			Selection.ResetSelection(selection_layer.Surface, ImageSize);
-			PreviousSelection.ResetSelection(selection_layer.Surface, ImageSize);
+		    var rect = new Cairo.Rectangle (0, 0, ImageSize.Width, ImageSize.Height);
+            Selection.CreateRectangleSelection (rect);
+            PreviousSelection.CreateRectangleSelection (rect);
 
 			ShowSelection = false;
 		}
@@ -844,8 +845,7 @@ namespace Pinta.Core
 			DocumentSelection old_selection = Selection.Clone();
 			bool old_show_selection = ShowSelection;
 
-			Selection.CreateRectangleSelection (SelectionLayer.Surface,
-			                                    new Cairo.Rectangle (x, y, cbImage.Width, cbImage.Height));
+			Selection.CreateRectangleSelection (new Cairo.Rectangle (x, y, cbImage.Width, cbImage.Height));
 			ShowSelection = true;
 
 			Workspace.Invalidate ();

Pinta.Core/Classes/DocumentSelection.cs
@@ -44,18 +44,29 @@ namespace Pinta.Core
 
 		public Path SelectionPath
 		{
-			get { return selection_path; }
-			set
-			{
-				if (selection_path == value)
-					return;
+		    get
+		    {
+		        if (selection_path == null)
+		        {
+		            using (var g = new Context (PintaCore.Layers.CurrentLayer.Surface))
+		                selection_path = g.CreatePolygonPath (ConvertToPolygonSet (SelectionPolygons));
+		        }
 
-                if (selection_path != null)
-                    selection_path.Dispose ();
-
-				selection_path = value;
-			}
+                return selection_path;
+		    }
 		}
+
+        /// <summary>
+        /// Indicate that the selection has changed.
+        /// </summary>
+        public void MarkDirty()
+        {
+            if (selection_path != null)
+            {
+                selection_path.Dispose ();
+                selection_path = null;
+            }
+        }
 
 		public void Clip (Context g)
 		{
@@ -94,20 +105,17 @@ namespace Pinta.Core
 		}
 
 		/// <summary>
-		/// Make a complete copy of the Selection.
+		/// Makes a copy of the Selection.
 		/// </summary>
-		/// <returns>A copy of this Selection (as a DocumentSelection object).</returns>
 		public DocumentSelection Clone()
 		{
-			DocumentSelection clonedSelection = new DocumentSelection();
-
-			clonedSelection.SelectionPolygons = SelectionPolygons.ToList();
-			clonedSelection.SelectionClipper = new Clipper();
-
-			clonedSelection.Origin = new PointD(Origin.X, Origin.Y);
-			clonedSelection.End = new PointD(End.X, End.Y);
-
-			return clonedSelection;
+		    return new DocumentSelection
+		    {
+		        SelectionPolygons = SelectionPolygons.ToList (),
+		        SelectionClipper = new Clipper (),
+		        Origin = new PointD (Origin.X, Origin.Y),
+		        End = new PointD (End.X, End.Y)
+		    };
 		}
 
 		/// <summary>
@@ -187,16 +195,9 @@ namespace Pinta.Core
 		/// <summary>
 		/// Create an elliptical Selection from a bounding Rectangle.
 		/// </summary>
-		/// <param name="selectionSurface">The selection surface to use for calculating the elliptical Path.</param>
 		/// <param name="r">The bounding Rectangle surrounding the ellipse.</param>
-		public void CreateEllipseSelection(Surface selectionSurface, Rectangle r)
+		public void CreateEllipseSelection(Rectangle r)
 		{
-			using (Context g = new Context(selectionSurface))
-			{
-				SelectionPath = g.CreateEllipsePath(r);
-			}
-
-
 			//These values were calculated in the static CreateEllipsePath method
 			//in Pinta.Core.CairoExtensions, so they were used here as well.
 			double rx = r.Width / 2; //1/2 of the bounding Rectangle Width.
@@ -257,6 +258,7 @@ namespace Pinta.Core
 
 			//Add the newly calculated elliptical Polygon.
 			SelectionPolygons.Add(newPolygon);
+            MarkDirty ();
 		}
 
 		/// <summary>
@@ -327,19 +329,12 @@ namespace Pinta.Core
 		/// <summary>
 		/// Create a rectangular Selection from a Rectangle.
 		/// </summary>
-		/// <param name="selectionSurface">The selection surface to use for calculating the rectangular Path.</param>
 		/// <param name="r">The Rectangle.</param>
-		public void CreateRectangleSelection(Surface selectionSurface, Rectangle r)
+		public void CreateRectangleSelection(Rectangle r)
 		{
-			using (Context g = new Context(selectionSurface))
-			{
-				SelectionPath = g.CreateRectanglePath(r);
-			}
-
-			//Clear the Selection Polygons collection to start from a clean slate.
 			SelectionPolygons.Clear();
-
 			SelectionPolygons.Add (CreateRectanglePolygon (r));
+            MarkDirty ();
 		}
 
 		/// <summary>
@@ -366,24 +361,7 @@ namespace Pinta.Core
 			SelectionClipper.Clear ();
 
 			SelectionPolygons = resultingPolygons;
-			using (Context g = new Context (surface)) {
-				SelectionPath = g.CreatePolygonPath (ConvertToPolygonSet (resultingPolygons));
-			}
-		}
-
-		/// <summary>
-		/// Reset (clear) the Selection.
-		/// </summary>
-		/// <param name="selectionSurface"></param>
-		/// <param name="imageSize"></param>
-		public void ResetSelection(Surface selectionSurface, Gdk.Size imageSize)
-		{
-			using (Cairo.Context g = new Cairo.Context(selectionSurface))
-			{
-				SelectionPath = g.CreateRectanglePath(new Rectangle(0, 0, imageSize.Width, imageSize.Height));
-			}
-
-			SelectionPolygons.Clear();
+            MarkDirty ();
 		}
 
 		private List<IntPoint> CreateRectanglePolygon (Rectangle r)
@@ -413,6 +391,17 @@ namespace Pinta.Core
         {
             if (selection_path != null)
                 selection_path.Dispose ();
+        }
+
+        /// <summary>
+        /// Resets the selection.
+        /// </summary>
+        public void Clear ()
+        {
+            SelectionPolygons.Clear ();
+            Origin = new PointD(0, 0);
+            End = new PointD(0, 0);
+            MarkDirty ();
         }
     }
 }

Pinta.Core/Classes/SelectionModeHandler.cs
@@ -120,7 +120,6 @@ namespace Pinta.Core
 
                     //Set the resulting selection path to the new selection path.
                     doc.Selection.SelectionPolygons = polygons;
-                    doc.Selection.SelectionPath = g.CreatePolygonPath (DocumentSelection.ConvertToPolygonSet(polygons));
                 }
                 else
                 {
@@ -160,8 +159,9 @@ namespace Pinta.Core
 
                     //Set the resulting selection path to the calculated ("clipped") selection path.
                     doc.Selection.SelectionPolygons = resultingPolygons;
-                    doc.Selection.SelectionPath = g.CreatePolygonPath (DocumentSelection.ConvertToPolygonSet (resultingPolygons));
                 }
+
+                doc.Selection.MarkDirty ();
             }
 
             PintaCore.Workspace.CallSelectionChanged (null, EventArgs.Empty);

Pinta.Tools/Tools/EllipseSelectTool.cs
@@ -49,7 +49,7 @@ namespace Pinta.Tools
 		{
 			Document doc = PintaCore.Workspace.ActiveDocument;
 
-			doc.Selection.CreateEllipseSelection(l.Surface, r);
+			doc.Selection.CreateEllipseSelection(r);
 			
 			return r;
 		}

Pinta.Tools/Tools/LassoSelectTool.cs
@@ -111,8 +111,6 @@ namespace Pinta.Tools
 				
 				g.FillRule = FillRule.EvenOdd;
 				g.ClosePath ();
-
-				doc.Selection.SelectionPath = g.CopyPath ();
 			}
 
 			doc.Selection.SelectionPolygons.Clear ();
@@ -136,8 +134,6 @@ namespace Pinta.Tools
 
 				g.FillRule = FillRule.EvenOdd;
 				g.ClosePath ();
-
-				doc.Selection.SelectionPath = g.CopyPath ();
 			}
 
 			doc.Selection.SelectionPolygons.Clear ();

Pinta.Tools/Tools/MoveSelectedTool.cs
@@ -70,8 +70,8 @@ namespace Pinta.Tools
 
 			// If there is no selection, select the whole image.
 			if (doc.Selection.SelectionPolygons.Count == 0) {
-				doc.Selection.CreateRectangleSelection (
-					doc.SelectionLayer.Surface, new Cairo.Rectangle (0, 0, doc.ImageSize.Width, doc.ImageSize.Height));
+                doc.Selection.CreateRectangleSelection (
+                    new Cairo.Rectangle (0, 0, doc.ImageSize.Width, doc.ImageSize.Height));
 			}
 
 			original_selection = new List<List<IntPoint>> (doc.Selection.SelectionPolygons);
@@ -115,11 +115,7 @@ namespace Pinta.Tools
 			Document doc = PintaCore.Workspace.ActiveDocument;
 			doc.Selection.SelectionClipper.Clear ();
 			doc.Selection.SelectionPolygons = newSelectionPolygons;
-			using (var g = new Cairo.Context (doc.CurrentUserLayer.Surface)) {
-				doc.Selection.SelectionPath = g.CreatePolygonPath (DocumentSelection.ConvertToPolygonSet (newSelectionPolygons));
-				g.FillRule = FillRule.EvenOdd;
-				g.AppendPath (doc.Selection.SelectionPath);
-			}
+            doc.Selection.MarkDirty ();
 
 			doc.ShowSelection = true;
 			doc.SelectionLayer.Transform.InitMatrix (original_transform);

Pinta.Tools/Tools/MoveSelectionTool.cs
@@ -81,11 +81,7 @@ namespace Pinta.Tools
 			Document doc = PintaCore.Workspace.ActiveDocument;
 			doc.Selection.SelectionClipper.Clear ();
 			doc.Selection.SelectionPolygons = newSelectionPolygons;
-			using (var g = new Cairo.Context (doc.CurrentUserLayer.Surface)) {
-				doc.Selection.SelectionPath = g.CreatePolygonPath (DocumentSelection.ConvertToPolygonSet (newSelectionPolygons));
-				g.FillRule = FillRule.EvenOdd;
-				g.AppendPath (doc.Selection.SelectionPath);
-			}
+            doc.Selection.MarkDirty ();
 
 			doc.ShowSelection = true;
 

commit b11b568623be07b4077d8a7bbf8e7ed5a85de1eb

Fix #1430200 - Fix the Deselect regression from 4291e4ba.

Pinta.Core/Actions/EditActions.cs
@@ -227,8 +227,7 @@ namespace Pinta.Core
 			SelectionHistoryItem hist = new SelectionHistoryItem ("Menu.Edit.Deselect.png", Catalog.GetString ("Deselect"));
 			hist.TakeSnapshot ();
 
-            doc.Selection.Clear ();
-            doc.PreviousSelection.Clear ();
+            doc.ResetSelectionPaths ();
 
 			doc.History.PushNewItem (hist);
 			doc.Workspace.Invalidate ();