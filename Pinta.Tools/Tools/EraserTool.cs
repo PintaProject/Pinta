1d5ed0a0e1cbacdd23ea8e9259c11a6c7abe5346
codeprof <mail4stefm@gmail.com>
2015-11-30 17:51:55 +0100

    Added smooth erase tool
    Added a combobox in the toolbar to change the type of the erase tool.

--- Pinta.Tools/Tools/EraserTool.cs
+++ Pinta.Tools/Tools/EraserTool.cs
@@ -34,16 +34,163 @@ namespace Pinta.Tools
 {
 	public class EraserTool : BaseBrushTool
 	{		
+        private enum EraserType
+        {
+            Normal = 0,
+            Smooth = 1,
+        }
+
 		private Point last_point = point_empty;
+        private EraserType eraser_type = EraserType.Normal; 
+
+        private const int LUT_Resolution = 256;
+        private byte[,] lut_factor = null;
+
+        private ToolBarLabel label_type = null;
+        private ToolBarComboBox comboBox_type = null;
 
 		public EraserTool ()
 		{
 		}
 
+        protected void initLookupTable()
+        {
+            if (lut_factor == null) {
+                lut_factor = new byte[LUT_Resolution + 1, LUT_Resolution + 1];
+
+                for (int dy = 0; dy < LUT_Resolution+1; dy++) {
+                    for (int dx = 0; dx < LUT_Resolution+1; dx++) {
+                        double d = Math.Sqrt (dx * dx + dy * dy) / LUT_Resolution;
+                        if (d > 1.0)
+                            lut_factor [dx, dy] = 255;
+                        else
+                            lut_factor [dx, dy] = (byte)(255.0 - Math.Cos (Math.Sqrt (d) * Math.PI / 2.0) * 255.0);
+                    }
+                }
+            }
+        }
+
+        protected ImageSurface copySurfacePart(ImageSurface surf, Gdk.Rectangle dest_rect)
+        {
+            ImageSurface tmp_surface = new ImageSurface (Format.Argb32, dest_rect.Width, dest_rect.Height);
+
+            using (Context g = new Context (tmp_surface)) {
+                g.Operator = Operator.Source;
+                g.SetSourceSurface (surf, -dest_rect.Left, -dest_rect.Top);
+                g.Rectangle (new Rectangle (0, 0, dest_rect.Width, dest_rect.Height));
+                g.Fill ();
+            }
+            //Flush to make sure all drawing operations are finished
+            tmp_surface.Flush ();
+            return tmp_surface;
+        }
+
+        protected void pasteSurfacePart(Context g, ImageSurface tmp_surface, Gdk.Rectangle dest_rect)
+        {
+            g.Operator = Operator.Source;
+            g.SetSourceSurface (tmp_surface, dest_rect.Left, dest_rect.Top);
+            g.Rectangle (new Rectangle (dest_rect.Left, dest_rect.Top, dest_rect.Width, dest_rect.Height));
+            g.Fill ();
+        }
+
+        protected void eraseNormal(Context g, Document doc, int x, int y)
+        {
+            g.AppendPath (doc.Selection.SelectionPath);
+            g.FillRule = FillRule.EvenOdd;
+            g.Clip ();
+
+            g.Antialias = UseAntialiasing ? Antialias.Subpixel : Antialias.None;
+
+            // Adding 0.5 forces cairo into the correct square:
+            // See https://bugs.launchpad.net/bugs/672232
+            g.MoveTo (last_point.X + 0.5, last_point.Y + 0.5);
+            g.LineTo (x + 0.5, y + 0.5);
+
+            // Right-click is erase to background color, left-click is transparent
+            if (mouse_button == 3) {
+                g.Operator = Operator.Source;
+                g.SetSourceColor (PintaCore.Palette.SecondaryColor);
+            }
+            else
+                g.Operator = Operator.Clear;
+
+            g.LineWidth = BrushWidth;
+            g.LineJoin = LineJoin.Round;
+            g.LineCap = LineCap.Round;
+
+            g.Stroke ();
+        }
+
+        protected unsafe void eraseSmooth(Context g, ImageSurface surf, int x, int y)
+        {
+            int rad = (int)(BrushWidth / 2.0) + 1;
+            Gdk.Rectangle surface_rect = new Gdk.Rectangle (0, 0, surf.Width, surf.Height);
+            Gdk.Rectangle brush_rect = new Gdk.Rectangle (x - rad, y - rad, 2 * rad, 2 * rad);
+            Gdk.Rectangle dest_rect = Gdk.Rectangle.Intersect (surface_rect, brush_rect);
+
+            if ((dest_rect.Width > 0) && (dest_rect.Height > 0)) {
+                //Initialize lookup table when first used (to prevent slower startup of the application)
+                initLookupTable ();
+
+                //Allow Clipping through a temporary surface
+                ImageSurface tmp_surface = copySurfacePart (surf, dest_rect); 
+
+                //Premultiply with alpha value
+                byte bk_col_a = (byte)(PintaCore.Palette.SecondaryColor.A * 255.0);
+                byte bk_col_r = (byte)(PintaCore.Palette.SecondaryColor.R * bk_col_a);
+                byte bk_col_g = (byte)(PintaCore.Palette.SecondaryColor.G * bk_col_a);
+                byte bk_col_b = (byte)(PintaCore.Palette.SecondaryColor.B * bk_col_a);
+
+                for (int iy = dest_rect.Top; iy < dest_rect.Bottom; iy++) {
+                    ColorBgra* srcRowPtr = tmp_surface.GetRowAddressUnchecked (iy - dest_rect.Top);
+                    for (int ix = dest_rect.Left; ix < dest_rect.Right; ix++) {
+                        ColorBgra col = *srcRowPtr;
+                        int dx = ((ix - x) * LUT_Resolution) / rad;
+                        if (dx < 0)
+                            dx = -dx;
+                        int dy = ((iy - y) * LUT_Resolution) / rad;
+                        if (dy < 0)
+                            dy = -dy;
+
+                        int force = lut_factor[dx,dy]; 
+                        //Note: premultiplied alpha is used!
+                        if (mouse_button == 3) {
+                            col.A = (byte)((col.A * force + bk_col_a * (255 - force)) / 255);         
+                            col.R = (byte)((col.R * force + bk_col_r * (255 - force)) / 255);
+                            col.G = (byte)((col.G * force + bk_col_g * (255 - force)) / 255);
+                            col.B = (byte)((col.B * force + bk_col_b * (255 - force)) / 255);
+                        }
+                        else {
+                            col.A = (byte)(col.A * force / 255);
+                            col.R = (byte)(col.R * force / 255);
+                            col.G = (byte)(col.G * force / 255);
+                            col.B = (byte)(col.B * force / 255);
+                        }
+                        *srcRowPtr = col;
+                        srcRowPtr++;
+                    }
+                }
+                //Draw the final result on the surface
+                pasteSurfacePart (g, tmp_surface, dest_rect);
+            }
+        }
+
 		protected override void OnBuildToolBar(Toolbar tb)
 		{
 			base.OnBuildToolBar(tb);
 
+            if (label_type == null)
+                label_type = new ToolBarLabel (string.Format (" {0}: ", Catalog.GetString ("Type")));
+            if (comboBox_type == null) {
+                comboBox_type = new ToolBarComboBox (100, 0, false, Catalog.GetString ("Normal"), Catalog.GetString ("Smooth"));
+                                                  
+                comboBox_type.ComboBox.Changed += (o, e) =>
+                {
+                    eraser_type = (EraserType)comboBox_type.ComboBox.Active;
+                };
+            }
+            tb.AppendItem (label_type);
+            tb.AppendItem (comboBox_type);
 			// Change the cursor when the BrushWidth is changed.
 			brush_width.ComboBox.Changed += (sender, e) => SetCursor (DefaultCursor);
 		}
@@ -69,7 +216,7 @@ namespace Pinta.Tools
 		#endregion
 
 		#region Mouse Handlers
-		protected override void OnMouseMove (object o, Gtk.MotionNotifyEventArgs args, Cairo.PointD point)
+        protected override void OnMouseMove (object o, Gtk.MotionNotifyEventArgs args, Cairo.PointD point)
 		{
 			Document doc = PintaCore.Workspace.ActiveDocument;
 
@@ -88,31 +235,15 @@ namespace Pinta.Tools
 				surface_modified = true;
 
 			ImageSurface surf = doc.CurrentUserLayer.Surface;
-			
-			using (Context g = new Context (surf)) {
-				g.AppendPath (doc.Selection.SelectionPath);
-				g.FillRule = FillRule.EvenOdd;
-				g.Clip ();
+            using (Context g = new Context (surf)) {
 
-				g.Antialias = UseAntialiasing ? Antialias.Subpixel : Antialias.None;
-
-				// Adding 0.5 forces cairo into the correct square:
-				// See https://bugs.launchpad.net/bugs/672232
-				g.MoveTo (last_point.X + 0.5, last_point.Y + 0.5);
-				g.LineTo (x + 0.5, y + 0.5);
-
-				// Right-click is erase to background color, left-click is transparent
-				if (mouse_button == 3)
-					g.SetSourceColor (PintaCore.Palette.SecondaryColor);
-				else
-					g.Operator = Operator.Clear;
-
-				g.LineWidth = BrushWidth;
-				g.LineJoin = LineJoin.Round;
-				g.LineCap = LineCap.Round;
-				
-				g.Stroke ();
-			}
+                if (eraser_type == EraserType.Normal) {
+                    eraseNormal (g, doc, x, y);
+                }
+                else if (eraser_type == EraserType.Smooth) {
+                    eraseSmooth(g, surf, x, y);
+                }
+		    }
 			
 			Gdk.Rectangle r = GetRectangleFromPoints (last_point, new Point (x, y));
 
@@ -123,6 +254,7 @@ namespace Pinta.Tools
 			}
 			
 			last_point = new Point (x, y);
+            
 		}
 		#endregion
 	}
