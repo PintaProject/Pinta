b01c0fe74361d034b4f5dc78c8c5b793367306e2
codeprof <mail4stefm@gmail.com>
2015-12-01 12:30:41 +0100

    improved smooth erase tool
    respect current selection and erase lines beginning from last_point for fast mouse movements.

--- Pinta.Tools/Tools/EraserTool.cs
+++ Pinta.Tools/Tools/EraserTool.cs
@@ -32,26 +32,27 @@ using Mono.Unix;
 
 namespace Pinta.Tools
 {
-	public class EraserTool : BaseBrushTool
-	{		
+    public class EraserTool : BaseBrushTool
+    {       
         private enum EraserType
         {
             Normal = 0,
             Smooth = 1,
         }
 
-		private Point last_point = point_empty;
+        private Point last_point = point_empty;
         private EraserType eraser_type = EraserType.Normal; 
 
         private const int LUT_Resolution = 256;
         private byte[,] lut_factor = null;
+        private ImageSurface tmp_surface = null;
 
         private ToolBarLabel label_type = null;
         private ToolBarComboBox comboBox_type = null;
 
-		public EraserTool ()
-		{
-		}
+        public EraserTool ()
+        {
+        }
 
         protected void initLookupTable()
         {
@@ -70,9 +71,17 @@ namespace Pinta.Tools
             }
         }
 
-        protected ImageSurface copySurfacePart(ImageSurface surf, Gdk.Rectangle dest_rect)
+        protected void copySurfacePart(ImageSurface surf, Gdk.Rectangle dest_rect)
         {
-            ImageSurface tmp_surface = new ImageSurface (Format.Argb32, dest_rect.Width, dest_rect.Height);
+            bool create = false;
+            if (tmp_surface == null)
+                create = true;
+            else
+                if ((dest_rect.Width != tmp_surface.Width) ||  (dest_rect.Height != tmp_surface.Height))
+                    create = true;
+
+            if (create)
+                tmp_surface = new ImageSurface (Format.Argb32, dest_rect.Width, dest_rect.Height);
 
             using (Context g = new Context (tmp_surface)) {
                 g.Operator = Operator.Source;
@@ -82,29 +91,24 @@ namespace Pinta.Tools
             }
             //Flush to make sure all drawing operations are finished
             tmp_surface.Flush ();
-            return tmp_surface;
         }
 
-        protected void pasteSurfacePart(Context g, ImageSurface tmp_surface, Gdk.Rectangle dest_rect)
-        {
+        protected void pasteSurfacePart(Context g, Gdk.Rectangle dest_rect)
+        {       
             g.Operator = Operator.Source;
             g.SetSourceSurface (tmp_surface, dest_rect.Left, dest_rect.Top);
             g.Rectangle (new Rectangle (dest_rect.Left, dest_rect.Top, dest_rect.Width, dest_rect.Height));
             g.Fill ();
         }
 
-        protected void eraseNormal(Context g, Document doc, int x, int y)
+        protected void eraseNormal(Context g, PointD start, PointD end)
         {
-            g.AppendPath (doc.Selection.SelectionPath);
-            g.FillRule = FillRule.EvenOdd;
-            g.Clip ();
-
             g.Antialias = UseAntialiasing ? Antialias.Subpixel : Antialias.None;
 
             // Adding 0.5 forces cairo into the correct square:
             // See https://bugs.launchpad.net/bugs/672232
-            g.MoveTo (last_point.X + 0.5, last_point.Y + 0.5);
-            g.LineTo (x + 0.5, y + 0.5);
+            g.MoveTo (start.X + 0.5, start.Y + 0.5);
+            g.LineTo (end.X + 0.5, end.Y + 0.5);
 
             // Right-click is erase to background color, left-click is transparent
             if (mouse_button == 3) {
@@ -121,69 +125,74 @@ namespace Pinta.Tools
             g.Stroke ();
         }
 
-        protected unsafe void eraseSmooth(Context g, ImageSurface surf, int x, int y)
+        protected unsafe void eraseSmooth(Context g, PointD start, PointD end)
         {
             int rad = (int)(BrushWidth / 2.0) + 1;
-            Gdk.Rectangle surface_rect = new Gdk.Rectangle (0, 0, surf.Width, surf.Height);
-            Gdk.Rectangle brush_rect = new Gdk.Rectangle (x - rad, y - rad, 2 * rad, 2 * rad);
-            Gdk.Rectangle dest_rect = Gdk.Rectangle.Intersect (surface_rect, brush_rect);
+            ImageSurface surf = (ImageSurface)g.GetTarget();
+            //Premultiply with alpha value
+            byte bk_col_a = (byte)(PintaCore.Palette.SecondaryColor.A * 255.0);
+            byte bk_col_r = (byte)(PintaCore.Palette.SecondaryColor.R * bk_col_a);
+            byte bk_col_g = (byte)(PintaCore.Palette.SecondaryColor.G * bk_col_a);
+            byte bk_col_b = (byte)(PintaCore.Palette.SecondaryColor.B * bk_col_a);
+            int num_steps = (int)start.Distance(end) / rad + 1; 
+            //Initialize lookup table when first used (to prevent slower startup of the application)
+            initLookupTable ();
 
-            if ((dest_rect.Width > 0) && (dest_rect.Height > 0)) {
-                //Initialize lookup table when first used (to prevent slower startup of the application)
-                initLookupTable ();
+            for (int step = 0; step < num_steps; step++) {
+                PointD pt = Utility.Lerp(start, end, (float)step / num_steps);
+                int x = (int)pt.X, y = (int)pt.Y;
 
-                //Allow Clipping through a temporary surface
-                ImageSurface tmp_surface = copySurfacePart (surf, dest_rect); 
+                Gdk.Rectangle surface_rect = new Gdk.Rectangle (0, 0, surf.Width, surf.Height);
+                Gdk.Rectangle brush_rect = new Gdk.Rectangle (x - rad, y - rad, 2 * rad, 2 * rad);
+                Gdk.Rectangle dest_rect = Gdk.Rectangle.Intersect (surface_rect, brush_rect);
 
-                //Premultiply with alpha value
-                byte bk_col_a = (byte)(PintaCore.Palette.SecondaryColor.A * 255.0);
-                byte bk_col_r = (byte)(PintaCore.Palette.SecondaryColor.R * bk_col_a);
-                byte bk_col_g = (byte)(PintaCore.Palette.SecondaryColor.G * bk_col_a);
-                byte bk_col_b = (byte)(PintaCore.Palette.SecondaryColor.B * bk_col_a);
+                if ((dest_rect.Width > 0) && (dest_rect.Height > 0)) {
+                    //Allow Clipping through a temporary surface
+                    copySurfacePart (surf, dest_rect); 
 
-                for (int iy = dest_rect.Top; iy < dest_rect.Bottom; iy++) {
-                    ColorBgra* srcRowPtr = tmp_surface.GetRowAddressUnchecked (iy - dest_rect.Top);
-                    for (int ix = dest_rect.Left; ix < dest_rect.Right; ix++) {
-                        ColorBgra col = *srcRowPtr;
-                        int dx = ((ix - x) * LUT_Resolution) / rad;
-                        if (dx < 0)
-                            dx = -dx;
-                        int dy = ((iy - y) * LUT_Resolution) / rad;
-                        if (dy < 0)
-                            dy = -dy;
+                    for (int iy = dest_rect.Top; iy < dest_rect.Bottom; iy++) {
+                        ColorBgra* srcRowPtr = tmp_surface.GetRowAddressUnchecked (iy - dest_rect.Top);
+                        for (int ix = dest_rect.Left; ix < dest_rect.Right; ix++) {
+                            ColorBgra col = *srcRowPtr;
+                            int dx = ((ix - x) * LUT_Resolution) / rad;
+                            if (dx < 0)
+                                dx = -dx;
+                            int dy = ((iy - y) * LUT_Resolution) / rad;
+                            if (dy < 0)
+                                dy = -dy;
 
-                        int force = lut_factor[dx,dy]; 
-                        //Note: premultiplied alpha is used!
-                        if (mouse_button == 3) {
-                            col.A = (byte)((col.A * force + bk_col_a * (255 - force)) / 255);         
-                            col.R = (byte)((col.R * force + bk_col_r * (255 - force)) / 255);
-                            col.G = (byte)((col.G * force + bk_col_g * (255 - force)) / 255);
-                            col.B = (byte)((col.B * force + bk_col_b * (255 - force)) / 255);
+                            int force = lut_factor [dx, dy]; 
+                            //Note: premultiplied alpha is used!
+                            if (mouse_button == 3) {
+                                col.A = (byte)((col.A * force + bk_col_a * (255 - force)) / 255);         
+                                col.R = (byte)((col.R * force + bk_col_r * (255 - force)) / 255);
+                                col.G = (byte)((col.G * force + bk_col_g * (255 - force)) / 255);
+                                col.B = (byte)((col.B * force + bk_col_b * (255 - force)) / 255);
+                            } else {
+                                col.A = (byte)(col.A * force / 255);
+                                col.R = (byte)(col.R * force / 255);
+                                col.G = (byte)(col.G * force / 255);
+                                col.B = (byte)(col.B * force / 255);
+                            }
+                            *srcRowPtr = col;
+                            srcRowPtr++;
                         }
-                        else {
-                            col.A = (byte)(col.A * force / 255);
-                            col.R = (byte)(col.R * force / 255);
-                            col.G = (byte)(col.G * force / 255);
-                            col.B = (byte)(col.B * force / 255);
-                        }
-                        *srcRowPtr = col;
-                        srcRowPtr++;
                     }
+                    //Draw the final result on the surface
+                    pasteSurfacePart (g, dest_rect);
                 }
-                //Draw the final result on the surface
-                pasteSurfacePart (g, tmp_surface, dest_rect);
             }
         }
 
-		protected override void OnBuildToolBar(Toolbar tb)
-		{
-			base.OnBuildToolBar(tb);
+        protected override void OnBuildToolBar(Toolbar tb)
+        {
+            base.OnBuildToolBar(tb);
 
             if (label_type == null)
                 label_type = new ToolBarLabel (string.Format (" {0}: ", Catalog.GetString ("Type")));
             if (comboBox_type == null) {
                 comboBox_type = new ToolBarComboBox (100, 0, false, Catalog.GetString ("Normal"), Catalog.GetString ("Smooth"));
-                                                  
+
                 comboBox_type.ComboBox.Changed += (o, e) =>
                 {
                     eraser_type = (EraserType)comboBox_type.ComboBox.Active;
@@ -191,71 +200,73 @@ namespace Pinta.Tools
             }
             tb.AppendItem (label_type);
             tb.AppendItem (comboBox_type);
-			// Change the cursor when the BrushWidth is changed.
-			brush_width.ComboBox.Changed += (sender, e) => SetCursor (DefaultCursor);
-		}
+            // Change the cursor when the BrushWidth is changed.
+            brush_width.ComboBox.Changed += (sender, e) => SetCursor (DefaultCursor);
+        }
 
-		#region Properties
-		public override string Name { get { return Catalog.GetString ("Eraser"); } }
-		public override string Icon { get { return "Tools.Eraser.png"; } }
-		public override string StatusBarText { get { return Catalog.GetString ("Left click to erase to transparent, right click to erase to secondary color. "); } }
+        #region Properties
+        public override string Name { get { return Catalog.GetString ("Eraser"); } }
+        public override string Icon { get { return "Tools.Eraser.png"; } }
+        public override string StatusBarText { get { return Catalog.GetString ("Left click to erase to transparent, right click to erase to secondary color. "); } }
 
-		public override Gdk.Cursor DefaultCursor {
-			get {
-				int iconOffsetX, iconOffsetY;
-				var icon = CreateIconWithShape ("Cursor.Eraser.png",
-				                                CursorShape.Ellipse, BrushWidth, 8, 22,
-				                                out iconOffsetX, out iconOffsetY);
+        public override Gdk.Cursor DefaultCursor {
+            get {
+                int iconOffsetX, iconOffsetY;
+                var icon = CreateIconWithShape ("Cursor.Eraser.png",
+                                                CursorShape.Ellipse, BrushWidth, 8, 22,
+                                                out iconOffsetX, out iconOffsetY);
                 return new Gdk.Cursor (Gdk.Display.Default, icon, iconOffsetX, iconOffsetY);
-			}
-		}
-		public override bool CursorChangesOnZoom { get { return true; } }
+            }
+        }
+        public override bool CursorChangesOnZoom { get { return true; } }
 
-		public override Gdk.Key ShortcutKey { get { return Gdk.Key.E; } }
-		public override int Priority { get { return 27; } }
-		#endregion
+        public override Gdk.Key ShortcutKey { get { return Gdk.Key.E; } }
+        public override int Priority { get { return 27; } }
+        #endregion
 
-		#region Mouse Handlers
-        protected override void OnMouseMove (object o, Gtk.MotionNotifyEventArgs args, Cairo.PointD point)
-		{
-			Document doc = PintaCore.Workspace.ActiveDocument;
+        #region Mouse Handlers
+        protected override void OnMouseMove (object o, Gtk.MotionNotifyEventArgs args, Cairo.PointD new_pointd)
+        {
+            Point new_point = new Point ((int)new_pointd.X, (int)new_pointd.Y);
 
-			if (mouse_button <= 0) {
-				last_point = point_empty;
-				return;
-			}
+            Document doc = PintaCore.Workspace.ActiveDocument;
 
-			int x = (int)point.X;
-			int y = (int)point.Y;
-			
-			if (last_point.Equals (point_empty))
-				last_point = new Point (x, y);
+            if (mouse_button <= 0) {
+                last_point = point_empty;
+                return;
+            }
 
-			if (doc.Workspace.PointInCanvas (point))
-				surface_modified = true;
+            if (last_point.Equals (point_empty))
+                last_point = new_point;
 
-			ImageSurface surf = doc.CurrentUserLayer.Surface;
-            using (Context g = new Context (surf)) {
+            if (doc.Workspace.PointInCanvas (new_pointd))
+                surface_modified = true;
+
+            using (Context g = new Context (doc.CurrentUserLayer.Surface)) {
+
+                g.AppendPath (doc.Selection.SelectionPath);
+                g.FillRule = FillRule.EvenOdd;
+                g.Clip ();
+                PointD last_pointd = new PointD (last_point.X, last_point.Y);                
 
                 if (eraser_type == EraserType.Normal) {
-                    eraseNormal (g, doc, x, y);
+                    eraseNormal (g, last_pointd, new_pointd);
                 }
                 else if (eraser_type == EraserType.Smooth) {
-                    eraseSmooth(g, surf, x, y);
+                    eraseSmooth(g, last_pointd, new_pointd);
                 }
-		    }
-			
-			Gdk.Rectangle r = GetRectangleFromPoints (last_point, new Point (x, y));
+            }
 
-			if (doc.Workspace.IsPartiallyOffscreen (r)) {
-				doc.Workspace.Invalidate ();
-			} else {
-				doc.Workspace.Invalidate (doc.ClampToImageSize (r));
-			}
-			
-			last_point = new Point (x, y);
-            
-		}
-		#endregion
-	}
+            Gdk.Rectangle r = GetRectangleFromPoints (last_point, new_point);
+
+            if (doc.Workspace.IsPartiallyOffscreen (r)) {
+                doc.Workspace.Invalidate ();
+            } else {
+                doc.Workspace.Invalidate (doc.ClampToImageSize (r));
+            }
+
+            last_point = new_point;
+        }
+        #endregion
+    }
 }
