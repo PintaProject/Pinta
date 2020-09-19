dc4fa2854af922a920a93f28f38e0ece98e43736
Cameron White <cameronwhite91@gmail.com>
2020-09-19 18:38:47 -0400

    Enable building an OSX installer for the GTK3 build.
    - This uses the self-contained publish mode, which avoids the need to separately install the dotnet runtime.
    - Don't publish the unit test project.

--- osx/build_installer.sh
+++ osx/build_installer.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -x
 
+msbuild ../Pinta.Install.proj -target:CompileTranslations
+dotnet publish ../Pinta.sln -r osx-x64
+
 MAC_APP_DIR=Pinta.app
 MAC_APP_BIN_DIR="${MAC_APP_DIR}/Contents/MacOS/"
-BIN_DIR=../bin
+BIN_DIR=../bin/osx-x64/publish
 
 mkdir -p ${MAC_APP_DIR}/Contents/{MacOS,Resources}
-cp -r ${BIN_DIR}/ ./pinta ${MAC_APP_BIN_DIR}
-chmod +x ${MAC_APP_BIN_DIR}/pinta
+cp -r ${BIN_DIR}/ ../bin/locale ${MAC_APP_BIN_DIR}
 cp Info.plist ${MAC_APP_DIR}/Contents
 cp pinta.icns ${MAC_APP_DIR}/Contents/Resources
 touch ${MAC_APP_DIR}
