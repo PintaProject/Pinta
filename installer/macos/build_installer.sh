33d8cb0dcc47fe681457fdeb344f929bf315837a
Cameron White <cameronwhite91@gmail.com>
2020-12-22 17:25:44 -0500

    Related to 9a4cd491, move the mac installer into the 'installer' folder along with Windows

--- installer/macos/build_installer.sh
+++ installer/macos/build_installer.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -x
 
-msbuild ../Pinta.Install.proj -target:CompileTranslations
-dotnet publish ../Pinta.sln --configuration Release -r osx-x64
+msbuild ../../Pinta.Install.proj -target:CompileTranslations
+dotnet publish ../../Pinta.sln --configuration Release -r osx-x64
 
 MAC_APP_DIR=Pinta.app
 MAC_APP_BIN_DIR="${MAC_APP_DIR}/Contents/MacOS/"
-BIN_DIR=../bin/osx-x64/publish
+BIN_DIR=../../bin/osx-x64/publish
 
 mkdir -p ${MAC_APP_DIR}/Contents/{MacOS,Resources}
-cp -r ${BIN_DIR}/ ../bin/locale ${MAC_APP_BIN_DIR}
+cp -r ${BIN_DIR}/ ../../bin/locale ${MAC_APP_BIN_DIR}
 cp Info.plist ${MAC_APP_DIR}/Contents
 cp pinta.icns ${MAC_APP_DIR}/Contents/Resources
 touch ${MAC_APP_DIR}
