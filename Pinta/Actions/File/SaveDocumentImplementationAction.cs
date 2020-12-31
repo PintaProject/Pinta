PintaProject/Pinta

commit d58ab5b21ebe77612a7e23eb259e45002a3236f1

Fix issues where the document's dirty state was not updated properly.
The history stack should track where the last clean item was, so that it is marked as modified when switching away from that item, and unmodified when returning to that item.

This also avoids the incorrect assumption that returning to the first history item clears the modified state, which is wrong if the document was saved at a later history item.

Fixes: #1905165

Pinta/Actions/File/SaveDocumentImplementationAction.cs
@@ -232,7 +232,7 @@ namespace Pinta.Actions
 			}
 
 			document.Filename = Path.GetFileName (file);
-			document.IsDirty = false;
+			document.Workspace.History.SetClean();
 
 			PintaCore.Tools.CurrentTool.DoAfterSave();
 

Pinta.Core/Classes/DocumentWorkspaceHistory.cs
@@ -35,6 +35,7 @@ namespace Pinta.Core
 		private Document document;
 		List<BaseHistoryItem> history = new List<BaseHistoryItem> ();
 		int historyPointer = -1;
+		int cleanPointer = -1;
 
 		internal DocumentWorkspaceHistory (Document document)
 		{
@@ -80,7 +81,6 @@ namespace Pinta.Core
 			newItem.Id = ListStore.AppendValues (newItem);
 			history.Add (newItem);
 			historyPointer = history.Count - 1;
-			
 			if (newItem.CausesDirty)
 				document.IsDirty = true;
 				
@@ -102,13 +102,18 @@ namespace Pinta.Core
 				BaseHistoryItem item = history[historyPointer];
 				item.Undo ();
 				item.State = HistoryItemState.Redo;
+				if (item.CausesDirty)
+					document.IsDirty = true;
+
 				ListStore.SetValue (item.Id, 0, item);
 				history[historyPointer] = item;
 				historyPointer--;
-			}	
-			
-			if (historyPointer == 0) {
+			}
+
+			if (historyPointer == cleanPointer)
 				document.IsDirty = false;
+
+			if (historyPointer == 0) {
 				PintaCore.Actions.Edit.Undo.Sensitive = false;
 				CanUndo = false;
 			}
@@ -134,8 +139,10 @@ namespace Pinta.Core
 				PintaCore.Actions.Edit.Redo.Sensitive = false;
 				CanRedo = false;
 			}
-				
-			if (item.CausesDirty)
+
+			if (historyPointer == cleanPointer)
+				document.IsDirty = false;
+			else if (item.CausesDirty)
 				document.IsDirty = true;
 
 			if (history.Count > 1) {
@@ -145,6 +152,17 @@ namespace Pinta.Core
 
 			PintaCore.History.OnActionRedone ();
 		}
+
+		/// <summary>
+        /// Mark the document as being clean at the current point in the history stack.
+        /// This might be used after e.g. saving the document to disk.
+        /// The document's IsDirty property is also set to false.
+        /// </summary>
+		public void SetClean()
+        {
+			cleanPointer = historyPointer;
+			document.IsDirty = false;
+        }
 		
 		public void Clear ()
 		{
@@ -152,6 +170,7 @@ namespace Pinta.Core
 			history.Clear();	
 			ListStore.Clear ();	
 			historyPointer = -1;
+			cleanPointer = -1;
 
 			document.IsDirty = false;
 			PintaCore.Actions.Edit.Redo.Sensitive = false;

Pinta.Core/Managers/WorkspaceManager.cs
@@ -164,7 +164,7 @@ namespace Pinta.Core
 			}
 
 			doc.Workspace.History.PushNewItem (new BaseHistoryItem (Stock.New, Catalog.GetString ("New Image")));
-			doc.IsDirty = false;
+			doc.Workspace.History.SetClean();
 
 			// This ensures these are called after the window is done being created and sized.
 			// Without it, we sometimes try to zoom when the window has a size of (0, 0).
@@ -193,7 +193,7 @@ namespace Pinta.Core
 
 				PintaCore.Workspace.ActiveDocument.PathAndFileName = file;
 				PintaCore.Workspace.ActiveWorkspace.History.PushNewItem (new BaseHistoryItem (Stock.Open, Catalog.GetString ("Open Image")));
-				PintaCore.Workspace.ActiveDocument.IsDirty = false;
+				PintaCore.Workspace.ActiveDocument.History.SetClean();
 				PintaCore.Workspace.ActiveDocument.HasFile = true;
 
                 // This ensures these are called after the window is done being created and sized.

commit 96304cbb8a6081141e936a5d093cc795aeef03f1

Fix a regression from d58ab5b2.
The re-editable tools can adjust the history after saving (e.g. undoing a "Finalize" history item that was done when committing the changes), so we need to carefully ensure that the document isn't stuck in a dirty state immediately after saving.

Fixes: 1909752

Pinta/Actions/File/SaveDocumentImplementationAction.cs
@@ -222,9 +222,12 @@ namespace Pinta.Actions
 			}
 
 			document.Filename = Path.GetFileName (file);
-			document.Workspace.History.SetClean();
 
 			PintaCore.Tools.CurrentTool.DoAfterSave();
+
+			// Mark the document as clean following the tool's after-save handler, which might
+			// adjust history (e.g. undo changes that were committed before saving).
+			document.Workspace.History.SetClean ();
 
 			//Now the Document has been saved to the file it's associated with in this session.
 			document.HasBeenSavedInSession = true;