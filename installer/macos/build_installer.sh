dd90a686222b6918ba7aaf9f12d83cc10088a2f8
Cameron White <cameronwhite91@gmail.com>
2021-11-19 21:31:03 -0500

    Fix warnings from not specifying the --self-contained argument explicitly.

--- installer/macos/build_installer.sh
+++ installer/macos/build_installer.sh
@@ -6,7 +6,7 @@ MAC_APP_DIR=Pinta.app
 MAC_APP_BIN_DIR="${MAC_APP_DIR}/Contents/MacOS/"
 
 mkdir -p ${MAC_APP_DIR}/Contents/{MacOS,Resources}
-dotnet publish ../../Pinta.sln -p:BuildTranslations=true --configuration Release -r osx-x64 -o ${MAC_APP_BIN_DIR}
+dotnet publish ../../Pinta.sln -p:BuildTranslations=true --configuration Release -r osx-x64 --self-contained true -o ${MAC_APP_BIN_DIR}
 cp Info.plist ${MAC_APP_DIR}/Contents
 cp pinta.icns ${MAC_APP_DIR}/Contents/Resources
 ./bundle_gtk.py --install_dir ${MAC_APP_BIN_DIR}
