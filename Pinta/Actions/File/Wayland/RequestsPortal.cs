5f65906e107b7aaa80953dbcb14734daf26d4242
James Carroll <eloquism@gmail.com>
2022-02-10 11:42:58 +0000

    Support screenshots on Wayland via XDG Desktop Portals

---
+++ Pinta/Actions/File/Wayland/RequestsPortal.cs
@@ -0,0 +1,21 @@
+/// This file is generated automatically and manually adjusted.
+/// dotnet tool install -g Tmds.DBus.Tool
+/// dotnet dbus codegen Requests.xml
+/// https://github.com/flatpak/xdg-desktop-portal/blob/main/data/org.freedesktop.portal.Request.xml
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Tmds.DBus;
+
+[assembly: InternalsVisibleTo(Tmds.DBus.Connection.DynamicAssemblyName)]
+namespace Requests.DBus
+{
+    [DBusInterface("org.freedesktop.portal.Request")]
+    interface IRequest : IDBusObject
+    {
+        Task CloseAsync();
+        Task<IDisposable> WatchResponseAsync(Action<(uint response, IDictionary<string, object> results)> handler, Action<Exception>? onError = null);
+    }
+}
---
+++ Pinta/Actions/File/Wayland/ScreenshotPortal.cs
@@ -0,0 +1,48 @@
+/// This file is generated automatically and manually adjusted.
+/// dotnet tool install -g Tmds.DBus.Tool
+/// dotnet dbus codegen --bus session --service org.freedesktop.portal.Desktop --interface org.freedesktop.portal.Screenshot
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Tmds.DBus;
+using Requests.DBus;
+
+[assembly: InternalsVisibleTo(Tmds.DBus.Connection.DynamicAssemblyName)]
+namespace ScreenshotPortal.DBus
+{
+    [DBusInterface("org.freedesktop.portal.Screenshot")]
+    interface IScreenshot : IDBusObject
+    {
+        Task<IRequest> ScreenshotAsync(string ParentWindow, IDictionary<string, object> Options);
+        Task<IRequest> PickColorAsync(string ParentWindow, IDictionary<string, object> Options);
+        Task<T> GetAsync<T>(string prop);
+        Task<ScreenshotProperties> GetAllAsync();
+        Task SetAsync(string prop, object val);
+        Task<IDisposable> WatchPropertiesAsync(Action<PropertyChanges> handler);
+    }
+
+    [Dictionary]
+    class ScreenshotProperties
+    {
+        private uint _version = default(uint);
+        public uint Version
+        {
+            get
+            {
+                return _version;
+            }
+
+            set
+            {
+                _version = (value);
+            }
+        }
+    }
+
+    static class ScreenshotExtensions
+    {
+        public static Task<uint> GetVersionAsync(this IScreenshot o) => o.GetAsync<uint>("version");
+    }
+}
