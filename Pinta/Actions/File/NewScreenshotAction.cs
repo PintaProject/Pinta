bcbaa952655cef1eea6c3cb96f7af14e28413dd3
James Carroll <eloquism@gmail.com>
2022-03-12 03:58:43 +0000

    Support screenshots via XDG Desktop Portals (#218)
    * Support screenshots on Wayland via XDG Desktop Portals
    
    * Avoid legacy screenshot logic on Wayland, but allow fallback on X11

--- Pinta/Actions/File/NewScreenshotAction.cs
+++ Pinta/Actions/File/NewScreenshotAction.cs
@@ -25,8 +25,11 @@
 // THE SOFTWARE.
 
 using System;
+using System.Collections.Generic;
 using Gdk;
 using Pinta.Core;
+using ScreenshotPortal.DBus;
+using Tmds.DBus;
 
 namespace Pinta.Actions
 {
@@ -44,8 +47,59 @@ namespace Pinta.Actions
 		}
 		#endregion
 
-		private void Activated (object sender, EventArgs e)
+		async private void Activated (object sender, EventArgs e)
 		{
+			// Use XDG Desktop Portal Screenshot API's to allow capturing screenshots on Wayland environments
+			// Check for presence of the $WAYLAND_DISPLAY variable to indicate Wayland is being used.
+			// Ideally GTK would give us this information instead, but the necessary functions aren't wrapped.
+			if (SystemManager.GetOperatingSystem () == OS.X11) {
+				try {
+					// It's important that the portal interactions are synchronised with the main thread
+					// Otherwise the use of the portals will cause massive instability and crash Pinta
+					var systemConnection = new Connection (new ClientConnectionOptions (Address.Session) { AutoConnect = true, SynchronizationContext = GLib.GLibSynchronizationContext.Current });
+
+					var portal = systemConnection.CreateProxy<IScreenshot> (
+						"org.freedesktop.portal.Desktop",
+						"/org/freedesktop/portal/desktop");
+
+					// The rootWindowID should be set to allow proper parenting of the screenshot dialog.
+					// However, the necessary functions are not correctly wrapped.
+					// The empty string means that the compositor may unfortunately place the dialog wherever it pleases.
+					// https://flatpak.github.io/xdg-desktop-portal/#parent_window
+					var rootWindowID = "";
+					var portalOptions = new Dictionary<string, object> {
+						{ "modal", true },
+						{ "interactive", true }}; // Enables options such as delay, specific windows, etc.
+
+					var handle = await portal.ScreenshotAsync (rootWindowID, portalOptions);
+					await handle.WatchResponseAsync (
+						reply => {
+							// response 0 == success, 1 == undefined error, 2 == user cancelled (not an error)
+							// However the response 1 can occur when the user presses "Cancel" on the second stage of the UI
+							// As a result, it's not reliable to throw error messages when the retval == 1
+							if (reply.response == 0) {
+								if (PintaCore.Workspace.OpenFile (GLib.FileFactory.NewForUri (reply.results["uri"].ToString ()))) {
+									// Mark as not having a file, so that the user doesn't unintentionally
+									// save using the temp file.
+									PintaCore.Workspace.ActiveDocument.ClearFileReference ();
+								}
+							}
+						}
+					);
+					return; // Prevent falling back to default screenshot behaviour
+
+				} catch (Tmds.DBus.DBusException) {
+					if (Environment.GetEnvironmentVariable ("WAYLAND_DISPLAY") != null) {
+						PintaCore.Chrome.ShowErrorDialog (PintaCore.Chrome.MainWindow,
+						Translations.GetString ("Failed to take screenshot"),
+						Translations.GetString ("Failed to access XDG Desktop Portals"));
+						return; // Don't bother trying to use legacy screenshot logic on Wayland, it won't work
+					}
+				}
+			}
+
+			// Pinta's standard screenshotting logic for non-Wayland systems
+
 			int delay = PintaCore.Settings.GetSetting<int> ("screenshot-delay", 0);
 
 			using var dialog = new SpinButtonEntryDialog (Translations.GetString ("Take Screenshot"),
