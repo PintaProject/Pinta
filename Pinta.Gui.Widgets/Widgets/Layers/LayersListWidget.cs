89003434167dcd346dab188ca8a1911188a56fb1
Zekiah-A <zekiahamoako@outlook.com>
2022-04-04 20:16:03 +0100

    Undid accidental code changes (oops)

--- Pinta.Gui.Widgets/Widgets/Layers/LayersListWidget.cs
+++ Pinta.Gui.Widgets/Widgets/Layers/LayersListWidget.cs
@@ -1,22 +1,22 @@
-//
+// 
 // LayersListWidget.cs
-//
+//  
 // Author:
 //       Jonathan Pobst <monkey@jpobst.com>
 //       Greg Lowe <greg@vis.net.nz>
-//
+// 
 // Copyright (c) 2010 Jonathan Pobst
-//
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -68,7 +68,6 @@ namespace Pinta.Gui.Widgets
 			PintaCore.Workspace.ActiveDocumentChanged += Workspace_ActiveDocumentChanged;
 
 			tree.CursorChanged += HandleLayerSelected;
-			tree.Model.RowsReordered += HandleLayerOrderChanged;
 		}
 
 		private UserLayer? GetSelectedLayerInTreeView () => tree.GetSelectedValueAt<UserLayer> (store_index_layer);
@@ -87,7 +86,7 @@ namespace Pinta.Gui.Widgets
 			tree = new TreeView {
 				HeadersVisible = false,
 				FixedHeightMode = true,
-				Reorderable = true,
+				Reorderable = false,
 				EnableGridLines = TreeViewGridLines.None,
 				EnableTreeLines = false,
 				ShowExpanders = false,
@@ -162,14 +161,6 @@ namespace Pinta.Gui.Widgets
 				doc.Layers.SetCurrentUserLayer (layer);
 
 			updating_model = false;
-		}
-
-		private void HandleLayerOrderChanged (object? o, RowsReorderedArgs e)
-		{
-			Console.WriteLine("!");
-
-			//var doc = PintaCore.Workspace.ActiveDocument;
-			//doc.Layers.MoveCurrentLayerUp ();
 		}
 
 		private void LayerVisibilityToggled (object? o, ToggledArgs args)
@@ -307,3 +298,4 @@ namespace Pinta.Gui.Widgets
 		}
 	}
 }
+
