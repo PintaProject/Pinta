5f083d0c75926bd45bcf96b1b4540001ef00929d
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2022-05-24 01:51:17 +0100

    Update SelectTool.cs

--- Pinta.Tools/Tools/SelectTool.cs
+++ Pinta.Tools/Tools/SelectTool.cs
@@ -124,8 +124,8 @@ namespace Pinta.Tools
 				return;
 			}
 
-			var x = Math.Round (Utility.Clamp (e.PointDouble.X, 0, document.ImageSize.Width - 1));
-			var y = Math.Round (Utility.Clamp (e.PointDouble.Y, 0, document.ImageSize.Height - 1));
+			var x = Math.Round (Utility.Clamp (e.PointDouble.X, 0, document.ImageSize.Width));
+			var y = Math.Round (Utility.Clamp (e.PointDouble.Y, 0, document.ImageSize.Height));
 
 			// Should always be true, set in OnMouseDown
 			if (active_handle.HasValue)
