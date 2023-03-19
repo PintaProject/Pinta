2d02d47f658fef30160a7d40418b96bc8488ae43
Cameron White <cameronwhite91@gmail.com>
2023-03-18 21:50:00 -0400

    Support drawing thumbnails in the GTK4 layers list view

--- Pinta.Gui.Widgets/Widgets/Layers/LayersListViewItemWidget.cs
+++ Pinta.Gui.Widgets/Widgets/Layers/LayersListViewItemWidget.cs
@@ -22,6 +22,7 @@
 // THE SOFTWARE.
 
 using System;
+using System.Collections.Generic;
 using Gtk;
 using Pinta.Core;
 
@@ -30,6 +31,8 @@ namespace Pinta.Gui.Widgets
 	// GObject subclass for use with Gio.ListStore
 	public class LayersListViewItem : GObject.Object
 	{
+		CanvasRenderer? canvas_renderer;
+
 		private Document doc;
 		private UserLayer layer;
 
@@ -41,6 +44,26 @@ namespace Pinta.Gui.Widgets
 
 		public string Label => layer.Name;
 		public bool Visible => !layer.Hidden;
+
+		public Cairo.ImageSurface BuildThumbnail (int width_request, int height_request)
+		{
+			// If this is the currently selected layer, we may need to draw the
+			// selection layer over it, like when dragging a selection.
+			if (layer == doc.Layers.CurrentUserLayer && doc.Layers.ShowSelectionLayer) {
+				var surface = CairoExtensions.CreateImageSurface (Cairo.Format.Argb32, width_request, height_request);
+
+				canvas_renderer ??= new CanvasRenderer (false, false);
+				canvas_renderer.Initialize (doc.ImageSize, new Size (width_request, height_request));
+
+				var layers = new List<Layer> { layer, doc.Layers.SelectionLayer };
+				canvas_renderer.Render (layers, surface, PointI.Zero);
+
+				return surface;
+			}
+
+			// Otherwise just directly use the layer's surface.
+			return layer.Surface;
+		}
 
 		public void HandleVisibilityToggled (bool visible)
 		{
@@ -66,7 +89,12 @@ namespace Pinta.Gui.Widgets
 
 	public class LayersListViewItemWidget : Box
 	{
+		private static readonly Cairo.Pattern transparent_pattern = CairoExtensions.CreateTransparentBackgroundPattern (8);
+
 		private LayersListViewItem? item;
+		private Cairo.ImageSurface? thumbnail_surface;
+
+		private Gtk.DrawingArea thumbnail;
 		private Gtk.Label label;
 		private Gtk.CheckButton visible_button;
 
@@ -76,7 +104,11 @@ namespace Pinta.Gui.Widgets
 			this.SetAllMargins (2);
 			SetOrientation (Orientation.Horizontal);
 
-			// TODO-GTK4 - add preview image for layer
+			thumbnail = Gtk.DrawingArea.New ();
+			thumbnail.SetDrawFunc ((area, context, width, height) => DrawThumbnail (context, width, height));
+			thumbnail.WidthRequest = 60;
+			thumbnail.HeightRequest = 40;
+			Append (thumbnail);
 
 			label = Gtk.Label.New (string.Empty);
 			label.Halign = Align.Start;
@@ -98,8 +130,54 @@ namespace Pinta.Gui.Widgets
 		public void Update (LayersListViewItem item)
 		{
 			this.item = item;
+
 			label.SetText (item.Label);
 			visible_button.SetActive (item.Visible);
+
+			thumbnail_surface = null;
+			thumbnail.QueueDraw ();
+		}
+
+		private void DrawThumbnail (Cairo.Context g, int width, int height)
+		{
+			ArgumentNullException.ThrowIfNull (item);
+
+			thumbnail_surface ??= item.BuildThumbnail (width, height);
+
+			double scale;
+			var draw_width = width;
+			var draw_height = height;
+
+			// The image is more constrained by height than width
+			if ((double) width / (double) thumbnail_surface.Width >= (double) height / (double) thumbnail_surface.Height) {
+				scale = (double) height / (double) (thumbnail_surface.Height);
+				draw_width = (int) (thumbnail_surface.Width * height / thumbnail_surface.Height);
+			} else {
+				scale = (double) width / (double) (thumbnail_surface.Width);
+				draw_height = (int) (thumbnail_surface.Height * width / thumbnail_surface.Width);
+			}
+
+			var offset_x = (int) ((width - draw_width) / 2f);
+			var offset_y = (int) ((height - draw_height) / 2f);
+
+			g.Save ();
+			g.Rectangle (offset_x, offset_y, draw_width, draw_height);
+			g.Clip ();
+
+			g.SetSource (transparent_pattern);
+			g.Paint ();
+
+			g.Scale (scale, scale);
+			g.SetSourceSurface (thumbnail_surface, (int) (offset_x / scale), (int) (offset_y / scale));
+			g.Paint ();
+
+			g.Restore ();
+
+			// TODO: scale this box correctly to match layer aspect ratio
+			g.SetSourceColor (new Cairo.Color (0.5, 0.5, 0.5));
+			g.Rectangle (offset_x + 0.5, offset_y + 0.5, draw_width, draw_height);
+			g.LineWidth = 1;
+			g.Stroke ();
 		}
 	}
 }
