f8a9bccedf8e9d998efb9582b271f81b6401495d
Cameron White <cameronwhite91@gmail.com>
2023-04-15 15:06:55 -0400

    Set the compatibility version for the Pinta.Core dependency
    This ensures an error is produced when attempting to install a v1.7 addin, which will not work due to many compatibility changes.
    In general, addins compiled against other Pinta versions have a good chance of some compatibility issue, so we should probably be recompiling add-ins each version

--- Pinta.Core/Classes/IExtension.cs
+++ Pinta.Core/Classes/IExtension.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Pinta.Core;
 
-[assembly: Mono.Addins.AddinRoot ("Pinta", PintaCore.ApplicationVersion)]
+[assembly: Mono.Addins.AddinRoot ("Pinta", PintaCore.ApplicationVersion, CompatVersion = PintaCore.ApplicationVersion)]
 
 namespace Pinta.Core
 {
