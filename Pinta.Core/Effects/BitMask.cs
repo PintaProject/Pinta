0a067ac5fe3fb05f80d34da9022cd9e04a23de0f
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2023-08-07 18:21:51 +0200

    Added basic tests for `BitMask` (#278)
    * Sealed `BitMask` class and changed existing comments to documentation comments
    
    * Added checks to BitMask width and height arguments. I was undecided if 0 was or not a valid value, but I decided it is based on how Cairo does things.
    
    See: https://cgit.freedesktop.org/cairo/tree/src/cairo-image-surface.c
    
    in function _cairo_image_surface_is_size_valid
    
    * Added basic tests to `BitMask`
    
    ---------
    
    Co-authored-by: Lehonti Ramos <john@doe>

--- Pinta.Core/Effects/BitMask.cs
+++ Pinta.Core/Effects/BitMask.cs
@@ -1,69 +1,73 @@
+using System;
 using System.Collections;
 
-namespace Pinta.Core
+namespace Pinta.Core;
+
+/// <summary>
+/// Represents a two-dimensional matrix of bits used to store a
+/// true/false value for each pixel in an image.
+/// </summary>
+public sealed class BitMask
 {
-	// Represent a two dimensional matrix of bits used to store a
-	// true/false value for each pixel in an image.
-	public class BitMask
+	private readonly BitArray array;
+
+	public BitMask (int width, int height)
 	{
-		private readonly BitArray array;
+		if (width < 0)
+			throw new ArgumentOutOfRangeException (nameof (width));
+		if (height < 0)
+			throw new ArgumentOutOfRangeException (nameof (height));
 
-		public BitMask (int width, int height)
-		{
-			Width = width;
-			Height = height;
+		Width = width;
+		Height = height;
 
-			array = new BitArray (width * height);
-		}
-
-		public bool this[PointI pt] {
-			get => this[pt.X, pt.Y];
-			set => this[pt.X, pt.Y] = value;
-		}
-
-		public bool this[int x, int y] {
-			get => Get (x, y);
-			set => Set (x, y, value);
-		}
-
-		public int Width { get; }
-
-		public int Height { get; }
-
-		public bool IsEmpty => array.Length == 0;
-
-		public void Clear (bool newValue) => array.SetAll (newValue);
-
-		public bool Get (int x, int y) => array.Get (GetIndex (x, y));
-
-		public void Invert (int x, int y)
-		{
-			var index = GetIndex (x, y);
-
-			array[index] = !array[index];
-		}
-
-		public void Invert (Scanline scan)
-		{
-			var x = scan.X;
-
-			while (x < scan.X + scan.Length) {
-				Invert (x, scan.Y);
-				++x;
-			}
-		}
-
-		public void Set (int x, int y, bool newValue) => array[GetIndex (x, y)] = newValue;
-
-		public void Set (RectangleI rect, bool newValue)
-		{
-			for (var y = rect.Y; y <= rect.Bottom; ++y) {
-				for (var x = rect.X; x <= rect.Right; ++x) {
-					Set (x, y, newValue);
-				}
-			}
-		}
-
-		private int GetIndex (int x, int y) => (y * Width) + x;
+		array = new BitArray (width * height);
 	}
+
+	public bool this[PointI pt] {
+		get => this[pt.X, pt.Y];
+		set => this[pt.X, pt.Y] = value;
+	}
+
+	public bool this[int x, int y] {
+		get => Get (x, y);
+		set => Set (x, y, value);
+	}
+
+	public int Width { get; }
+
+	public int Height { get; }
+
+	public bool IsEmpty => array.Length == 0;
+
+	public void Clear (bool newValue) => array.SetAll (newValue);
+
+	public bool Get (int x, int y) => array.Get (GetIndex (x, y));
+
+	public void Invert (int x, int y)
+	{
+		var index = GetIndex (x, y);
+
+		array[index] = !array[index];
+	}
+
+	public void Invert (Scanline scan)
+	{
+		var x = scan.X;
+		while (x < scan.X + scan.Length) {
+			Invert (x, scan.Y);
+			++x;
+		}
+	}
+
+	public void Set (int x, int y, bool newValue) => array[GetIndex (x, y)] = newValue;
+
+	public void Set (RectangleI rect, bool newValue)
+	{
+		for (var y = rect.Y; y <= rect.Bottom; ++y)
+			for (var x = rect.X; x <= rect.Right; ++x)
+				Set (x, y, newValue);
+	}
+
+	private int GetIndex (int x, int y) => (y * Width) + x;
 }
