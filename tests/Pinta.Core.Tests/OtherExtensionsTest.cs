406f9f86d5e0f35187e50554e4e5cc8d39058b9a
Lehonti Ramos <john@doe>
2023-08-07 23:36:14 +0200

    Created a empty test case for `OtherExtensions.CreatePolygonSet`

--- tests/Pinta.Core.Tests/OtherExtensionsTest.cs
+++ tests/Pinta.Core.Tests/OtherExtensionsTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
@@ -14,4 +15,16 @@ internal sealed class OtherExtensionsTest
 		var materialized2 = materialized1.ToReadOnlyCollection ();
 		Assert.AreSame (materialized1, materialized2);
 	}
+
+	[TestCaseSource (nameof (create_polygon_set_arguments_for_empty))]
+	public void EmptyStencilReturnsEmptyPolygonSet (RectangleD bounds, int translateX, int translateY)
+	{
+		var bitmask = new BitMask (0, 0);
+		var polygonSet = bitmask.CreatePolygonSet (bounds, translateX, translateY);
+		Assert.Zero (polygonSet.Count);
+	}
+
+	private static readonly IReadOnlyList<TestCaseData> create_polygon_set_arguments_for_empty = new TestCaseData[] {
+		new (new RectangleD(0, 0, 1, 1), 1, 1),
+	};
 }
