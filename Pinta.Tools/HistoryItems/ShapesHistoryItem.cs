bf7066880028a53c6f2432f199f34aad88fcbd75
Lehonti Ramos <john@doe>
2023-08-10 18:50:13 +0200

    File-scoped namespaces for history items in `Pinta.Tools`

--- Pinta.Tools/HistoryItems/ShapesHistoryItem.cs
+++ Pinta.Tools/HistoryItems/ShapesHistoryItem.cs
@@ -27,115 +27,114 @@
 using Cairo;
 using Pinta.Core;
 
-namespace Pinta.Tools
+namespace Pinta.Tools;
+
+public sealed class ShapesHistoryItem : BaseHistoryItem
 {
-	public class ShapesHistoryItem : BaseHistoryItem
+	private readonly BaseEditEngine ee;
+
+	private readonly UserLayer user_layer;
+
+	private readonly SurfaceDiff? user_surface_diff;
+	private ImageSurface? user_surface;
+
+	private ShapeEngineCollection s_engines;
+
+	private int selected_point_index, selected_shape_index;
+
+	private readonly bool redraw_everything;
+
+	/// <summary>
+	/// A history item for when shapes are finalized.
+	/// </summary>
+	/// <param name="passedEE">The EditEngine being used.</param>
+	/// <param name="icon">The history item's icon.</param>
+	/// <param name="text">The history item's title.</param>
+	/// <param name="passedUserSurface">The stored UserLayer surface.</param>
+	/// <param name="passedUserLayer">The UserLayer being modified.</param>
+	/// <param name="passedSelectedPointIndex">The selected point's index.</param>
+	/// <param name="passedSelectedShapeIndex">The selected point's shape index.</param>
+	/// <param name="passedRedrawEverything">Whether every shape should be redrawn when undoing (e.g. finalization).</param>
+	public ShapesHistoryItem (BaseEditEngine passedEE, string icon, string text, ImageSurface passedUserSurface, UserLayer passedUserLayer,
+			int passedSelectedPointIndex, int passedSelectedShapeIndex, bool passedRedrawEverything) : base (icon, text)
 	{
-		private readonly BaseEditEngine ee;
+		ee = passedEE;
 
-		private readonly UserLayer user_layer;
-
-		private readonly SurfaceDiff? user_surface_diff;
-		private ImageSurface? user_surface;
-
-		private ShapeEngineCollection s_engines;
-
-		private int selected_point_index, selected_shape_index;
-
-		private readonly bool redraw_everything;
-
-		/// <summary>
-		/// A history item for when shapes are finalized.
-		/// </summary>
-		/// <param name="passedEE">The EditEngine being used.</param>
-		/// <param name="icon">The history item's icon.</param>
-		/// <param name="text">The history item's title.</param>
-		/// <param name="passedUserSurface">The stored UserLayer surface.</param>
-		/// <param name="passedUserLayer">The UserLayer being modified.</param>
-		/// <param name="passedSelectedPointIndex">The selected point's index.</param>
-		/// <param name="passedSelectedShapeIndex">The selected point's shape index.</param>
-		/// <param name="passedRedrawEverything">Whether every shape should be redrawn when undoing (e.g. finalization).</param>
-		public ShapesHistoryItem (BaseEditEngine passedEE, string icon, string text, ImageSurface passedUserSurface, UserLayer passedUserLayer,
-				int passedSelectedPointIndex, int passedSelectedShapeIndex, bool passedRedrawEverything) : base (icon, text)
-		{
-			ee = passedEE;
-
-			user_layer = passedUserLayer;
+		user_layer = passedUserLayer;
 
 
-			user_surface_diff = SurfaceDiff.Create (passedUserSurface, user_layer.Surface, true);
+		user_surface_diff = SurfaceDiff.Create (passedUserSurface, user_layer.Surface, true);
 
-			if (user_surface_diff == null) {
-				user_surface = passedUserSurface;
-			}
-
-
-			s_engines = BaseEditEngine.SEngines.PartialClone ();
-			selected_point_index = passedSelectedPointIndex;
-			selected_shape_index = passedSelectedShapeIndex;
-
-			redraw_everything = passedRedrawEverything;
+		if (user_surface_diff == null) {
+			user_surface = passedUserSurface;
 		}
 
-		public override void Undo ()
-		{
-			Swap (redraw_everything);
+
+		s_engines = BaseEditEngine.SEngines.PartialClone ();
+		selected_point_index = passedSelectedPointIndex;
+		selected_shape_index = passedSelectedShapeIndex;
+
+		redraw_everything = passedRedrawEverything;
+	}
+
+	public override void Undo ()
+	{
+		Swap (redraw_everything);
+	}
+
+	public override void Redo ()
+	{
+		Swap (false);
+	}
+
+	private void Swap (bool redraw)
+	{
+		// Grab the original surface
+		ImageSurface surf = user_layer.Surface;
+
+		if (user_surface_diff != null) {
+			user_surface_diff.ApplyAndSwap (surf);
+
+			PintaCore.Workspace.Invalidate (user_surface_diff.GetBounds ());
+		} else {
+			// Undo to the "old" surface
+			user_layer.Surface = user_surface!; // NRT - userSurface will be not-null in this branch
+
+			// Store the original surface for Redo
+			user_surface = surf;
+
+			//Redraw everything since surfaces were swapped.
+			PintaCore.Workspace.Invalidate ();
 		}
 
-		public override void Redo ()
-		{
-			Swap (false);
+		Swap (ref s_engines, ref BaseEditEngine.SEngines);
+
+		//Ensure that all of the shapes that should no longer be drawn have their ReEditableLayer removed from the drawing loop.
+		foreach (ShapeEngine se in s_engines) {
+			//Determine if it is currently in the drawing loop and should no longer be. Note: a DrawingLayer could be both removed and then
+			//later added in the same swap operation, but this is faster than looping through each ShapeEngine in BaseEditEngine.SEngines.
+			if (se.DrawingLayer.InTheLoop && !BaseEditEngine.SEngines.Contains (se)) {
+				se.DrawingLayer.TryRemoveLayer ();
+			}
 		}
 
-		private void Swap (bool redraw)
-		{
-			// Grab the original surface
-			ImageSurface surf = user_layer.Surface;
-
-			if (user_surface_diff != null) {
-				user_surface_diff.ApplyAndSwap (surf);
-
-				PintaCore.Workspace.Invalidate (user_surface_diff.GetBounds ());
-			} else {
-				// Undo to the "old" surface
-				user_layer.Surface = user_surface!; // NRT - userSurface will be not-null in this branch
-
-				// Store the original surface for Redo
-				user_surface = surf;
-
-				//Redraw everything since surfaces were swapped.
-				PintaCore.Workspace.Invalidate ();
+		//Ensure that all of the shapes that should now be drawn have their ReEditableLayer in the drawing loop.
+		foreach (ShapeEngine se in BaseEditEngine.SEngines) {
+			//Determine if it is currently out of the drawing loop; if not, it should be.
+			if (!se.DrawingLayer.InTheLoop) {
+				se.DrawingLayer.TryAddLayer ();
 			}
+		}
 
-			Swap (ref s_engines, ref BaseEditEngine.SEngines);
+		Swap (ref selected_point_index, ref ee.SelectedPointIndex);
+		Swap (ref selected_shape_index, ref ee.SelectedShapeIndex);
 
-			//Ensure that all of the shapes that should no longer be drawn have their ReEditableLayer removed from the drawing loop.
-			foreach (ShapeEngine se in s_engines) {
-				//Determine if it is currently in the drawing loop and should no longer be. Note: a DrawingLayer could be both removed and then
-				//later added in the same swap operation, but this is faster than looping through each ShapeEngine in BaseEditEngine.SEngines.
-				if (se.DrawingLayer.InTheLoop && !BaseEditEngine.SEngines.Contains (se)) {
-					se.DrawingLayer.TryRemoveLayer ();
-				}
-			}
+		//Determine if the currently active tool matches the shape's corresponding tool, and if not, switch to it.
+		if (BaseEditEngine.ActivateCorrespondingTool (ee.SelectedShapeIndex, true) != null) {
+			//The currently active tool now matches the shape's corresponding tool.
 
-			//Ensure that all of the shapes that should now be drawn have their ReEditableLayer in the drawing loop.
-			foreach (ShapeEngine se in BaseEditEngine.SEngines) {
-				//Determine if it is currently out of the drawing loop; if not, it should be.
-				if (!se.DrawingLayer.InTheLoop) {
-					se.DrawingLayer.TryAddLayer ();
-				}
-			}
-
-			Swap (ref selected_point_index, ref ee.SelectedPointIndex);
-			Swap (ref selected_shape_index, ref ee.SelectedShapeIndex);
-
-			//Determine if the currently active tool matches the shape's corresponding tool, and if not, switch to it.
-			if (BaseEditEngine.ActivateCorrespondingTool (ee.SelectedShapeIndex, true) != null) {
-				//The currently active tool now matches the shape's corresponding tool.
-
-				if (redraw) {
-					((ShapeTool?) PintaCore.Tools.CurrentTool)?.EditEngine.DrawAllShapes ();
-				}
+			if (redraw) {
+				((ShapeTool?) PintaCore.Tools.CurrentTool)?.EditEngine.DrawAllShapes ();
 			}
 		}
 	}
