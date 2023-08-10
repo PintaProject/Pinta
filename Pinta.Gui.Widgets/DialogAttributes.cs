8b0af8fd44e228abcd60c01154943aa5a88aa601
Lehonti Ramos <john@doe>
2023-08-10 18:59:43 +0200

    File-scoped namespace in `DialogAttributes.cs`

--- Pinta.Gui.Widgets/DialogAttributes.cs
+++ Pinta.Gui.Widgets/DialogAttributes.cs
@@ -13,66 +13,65 @@
 
 using System;
 
-namespace Pinta.Gui.Widgets
+namespace Pinta.Gui.Widgets;
+
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+public class SkipAttribute : Attribute
 {
-	[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
-	public class SkipAttribute : Attribute
-	{
-	}
+}
 
-	[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
-	public class CaptionAttribute : Attribute
-	{
-		public CaptionAttribute (string caption) => Caption = caption;
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+public class CaptionAttribute : Attribute
+{
+	public CaptionAttribute (string caption) => Caption = caption;
 
-		public string Caption { get; set; }
-	}
+	public string Caption { get; set; }
+}
 
-	[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
-	public class DigitsValueAttribute : Attribute
-	{
-		public DigitsValueAttribute (int value) => Value = value;
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+public class DigitsValueAttribute : Attribute
+{
+	public DigitsValueAttribute (int value) => Value = value;
 
-		public int Value { get; set; }
-	}
+	public int Value { get; set; }
+}
 
-	[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
-	public class IncrementValueAttribute : Attribute
-	{
-		public IncrementValueAttribute (double value) => Value = value;
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+public class IncrementValueAttribute : Attribute
+{
+	public IncrementValueAttribute (double value) => Value = value;
 
-		public double Value { get; set; }
-	}
+	public double Value { get; set; }
+}
 
-	[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
-	public class MinimumValueAttribute : Attribute
-	{
-		public MinimumValueAttribute (int value) => Value = value;
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+public class MinimumValueAttribute : Attribute
+{
+	public MinimumValueAttribute (int value) => Value = value;
 
-		public int Value { get; set; }
-	}
+	public int Value { get; set; }
+}
 
-	[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
-	public class MaximumValueAttribute : Attribute
-	{
-		public MaximumValueAttribute (int value) => Value = value;
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+public class MaximumValueAttribute : Attribute
+{
+	public MaximumValueAttribute (int value) => Value = value;
 
-		public int Value { get; set; }
-	}
+	public int Value { get; set; }
+}
 
-	[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
-	public class HintAttribute : Attribute
-	{
-		public HintAttribute (string caption) => Hint = caption;
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+public class HintAttribute : Attribute
+{
+	public HintAttribute (string caption) => Hint = caption;
 
-		public string Hint { get; set; }
-	}
+	public string Hint { get; set; }
+}
 
-	[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
-	public class StaticListAttribute : Attribute
-	{
-		public StaticListAttribute (string dict) => DictionaryName = dict;
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+public class StaticListAttribute : Attribute
+{
+	public StaticListAttribute (string dict) => DictionaryName = dict;
 
-		public string DictionaryName { get; set; }
-	}
+	public string DictionaryName { get; set; }
 }
