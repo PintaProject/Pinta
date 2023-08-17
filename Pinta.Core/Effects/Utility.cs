d3ff4570f2559718059dad5c99b1b090b7077d21
Lehonti Ramos <lehonti@ramos>
2023-08-17 08:18:22 +0200

    Fixed formatting

--- Pinta.Core/Effects/Utility.cs
+++ Pinta.Core/Effects/Utility.cs
@@ -6,7 +6,6 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 using System;
-using System.Collections.Generic;
 using System.Reflection;
 
 namespace Pinta.Core;
