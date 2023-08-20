cef8511901fe06f9779f6ec5defe5e27677d8881
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2023-08-20 16:59:21 +0200

    Same ROI array is used throughout `AdjustmentBenchmarks` (with a similar change in `EffectsBenchmarks`) (#445)

--- tests/PintaBenchmarks/AdjustmentsBenchmarks.cs
+++ tests/PintaBenchmarks/AdjustmentsBenchmarks.cs
@@ -7,38 +7,38 @@ namespace PintaBenchmarks;
 
 [MemoryDiagnoser]
 [Config (typeof (MillisecondConfig))]
-public class AdjustmentsBenchmarks
+public sealed class AdjustmentsBenchmarks
 {
 	private readonly ImageSurface surface;
 	private readonly ImageSurface dest_surface;
-	private readonly RectangleI bounds;
+	private readonly RectangleI[] bounds;
 
 	public AdjustmentsBenchmarks ()
 	{
 		surface = TestData.Get2000PixelImage ();
 		dest_surface = new ImageSurface (Format.Argb32, 2000, 2000);
-		bounds = surface.GetBounds ();
+		bounds = new[] { surface.GetBounds () };
 	}
 
 	[Benchmark]
 	public void AutoLevelEffect ()
 	{
 		var effect = new AutoLevelEffect ();
-		effect.Render (surface, dest_surface, new[] { bounds });
+		effect.Render (surface, dest_surface, bounds);
 	}
 
 	[Benchmark]
 	public void BlackAndWhiteEffect ()
 	{
 		var effect = new BlackAndWhiteEffect ();
-		effect.Render (surface, dest_surface, new[] { bounds });
+		effect.Render (surface, dest_surface, bounds);
 	}
 
 	[Benchmark]
 	public void BrightnessContrastEffect ()
 	{
 		var effect = new BrightnessContrastEffect ();
-		effect.Render (surface, dest_surface, new[] { bounds });
+		effect.Render (surface, dest_surface, bounds);
 	}
 
 	[Benchmark]
@@ -46,51 +46,51 @@ public class AdjustmentsBenchmarks
 	{
 		var effect = new CurvesEffect ();
 
-		var points = new SortedList<int, int> ();
-
-		points.Add (0, 0);
-		points.Add (75, 110);
-		points.Add (225, 175);
-		points.Add (255, 255);
+		var points = new SortedList<int, int> {
+			{ 0, 0 },
+			{ 75, 110 },
+			{ 225, 175 },
+			{ 255, 255 }
+		};
 
 		(effect.EffectData as CurvesData)!.ControlPoints = new[] { points };
 		(effect.EffectData as CurvesData)!.Mode = ColorTransferMode.Luminosity;
 
-		effect.Render (surface, dest_surface, new[] { bounds });
+		effect.Render (surface, dest_surface, bounds);
 	}
 
 	[Benchmark]
 	public void HueSaturationEffect ()
 	{
 		var effect = new HueSaturationEffect ();
-		effect.Render (surface, dest_surface, new[] { bounds });
+		effect.Render (surface, dest_surface, bounds);
 	}
 
 	[Benchmark]
 	public void InvertColorsEffect ()
 	{
 		var effect = new InvertColorsEffect ();
-		effect.Render (surface, dest_surface, new[] { bounds });
+		effect.Render (surface, dest_surface, bounds);
 	}
 
 	[Benchmark]
 	public void LevelsEffect ()
 	{
 		var effect = new LevelsEffect ();
-		effect.Render (surface, dest_surface, new[] { bounds });
+		effect.Render (surface, dest_surface, bounds);
 	}
 
 	[Benchmark]
 	public void PosterizeEffect ()
 	{
 		var effect = new PosterizeEffect ();
-		effect.Render (surface, dest_surface, new[] { bounds });
+		effect.Render (surface, dest_surface, bounds);
 	}
 
 	[Benchmark]
 	public void SepiaEffect ()
 	{
 		var effect = new SepiaEffect ();
-		effect.Render (surface, dest_surface, new[] { bounds });
+		effect.Render (surface, dest_surface, bounds);
 	}
 }
