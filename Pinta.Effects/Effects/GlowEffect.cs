PintaProject/Pinta

commit baaa889ae60ba328152fea11dbda6b05376e177f

Port pixel ops to Span<ColorBgra>

Pinta.Effects/Effects/GlowEffect.cs
@@ -63,12 +63,16 @@ namespace Pinta.Effects
 			contrastEffect.Data.Contrast = Data.Contrast;
 			contrastEffect.Render (dest, dest, rois);
 
+			var dst_data = dest.GetData ();
+			var src_data = src.GetReadOnlyData ();
+			int src_width = src.Width;
+			int dst_width = dest.Width;
+
 			foreach (Gdk.Rectangle roi in rois) {
 				for (int y = roi.Top; y <= roi.GetBottom (); ++y) {
-					ColorBgra* dstPtr = dest.GetPointAddressUnchecked (roi.Left, y);
-					ColorBgra* srcPtr = src.GetPointAddressUnchecked (roi.Left, y);
-
-					screenBlendOp.Apply (dstPtr, srcPtr, dstPtr, roi.Width);
+					var dst_row = dst_data.Slice (y * dst_width + roi.Left, roi.Width);
+					var src_row = dst_data.Slice (y * src_width + roi.Left, roi.Width);
+					screenBlendOp.Apply (dst_row, src_row, dst_row);
 				}
 			}
 		}

Pinta.Core/Classes/Layer.cs
@@ -186,9 +186,6 @@ namespace Pinta.Core
 		public unsafe void HueSaturation (int hueDelta, int satDelta, int lightness)
 		{
 			ImageSurface dest = Surface.Clone ();
-			ColorBgra* dstPtr = (ColorBgra*) dest.DataPtr;
-
-			int len = Surface.Data.Length / 4;
 
 			// map the range [0,100] -> [0,100] and the range [101,200] -> [103,400]
 			if (satDelta > 100)
@@ -201,7 +198,9 @@ namespace Pinta.Core
 			else
 				op = new UnaryPixelOps.HueSaturationLightness (hueDelta, satDelta, lightness);
 
-			op.Apply (dstPtr, len);
+			dest.Flush ();
+			op.Apply (dest.GetData ());
+			dest.MarkDirty ();
 
 			using (Context g = new Context (Surface)) {
 				g.AppendPath (PintaCore.Workspace.ActiveDocument.Selection.SelectionPath);

Pinta.Core/Effects/BinaryPixelOp.cs
@@ -18,21 +18,14 @@ namespace Pinta.Core
 	/// result of the form: c = F(a, b)
 	/// </summary>
 	[Serializable]
-	public unsafe abstract class BinaryPixelOp : PixelOp
+	public abstract class BinaryPixelOp : PixelOp
 	{
-		public abstract ColorBgra Apply (ColorBgra lhs, ColorBgra rhs);
+		public abstract ColorBgra Apply (in ColorBgra lhs, in ColorBgra rhs);
 
-		public unsafe virtual void Apply (ColorBgra* dst, ColorBgra* lhs, ColorBgra* rhs, int length)
+		public virtual void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> lhs, ReadOnlySpan<ColorBgra> rhs)
 		{
-			unsafe {
-				while (length > 0) {
-					*dst = Apply (*lhs, *rhs);
-					++dst;
-					++lhs;
-					++rhs;
-					--length;
-				}
-			}
+			for (int i = 0; i < dst.Length; ++i)
+				dst[i] = Apply (lhs[i], rhs[i]);
 		}
 
 		/// <summary>
@@ -79,34 +72,25 @@ namespace Pinta.Core
 			// Cache the width and height properties
 			int width = roiSize.Width;
 			int height = roiSize.Height;
+			var lhs_data = lhs.GetReadOnlyData ();
+			int lhs_width = lhs.Width;
+			var rhs_data = rhs.GetReadOnlyData ();
+			int rhs_width = rhs.Width;
+			var dst_data = dst.GetData ();
+			int dst_width = dst.Width;
 
 			// Do the work.
-			unsafe {
-				for (int row = 0; row < height; ++row) {
-					ColorBgra* dstPtr = dst.GetPointAddress (dstOffset.X, dstOffset.Y + row);
-					ColorBgra* lhsPtr = lhs.GetPointAddress (lhsOffset.X, lhsOffset.Y + row);
-					ColorBgra* rhsPtr = rhs.GetPointAddress (rhsOffset.X, rhsOffset.Y + row);
-
-					Apply (dstPtr, lhsPtr, rhsPtr, width);
-				}
+			for (int row = 0; row < height; ++row) {
+				Apply (dst_data.Slice ((dstOffset.Y + row) * dst_width + dstOffset.X, width),
+				       lhs_data.Slice ((lhsOffset.Y + row) * lhs_width + lhsOffset.X, width),
+				       rhs_data.Slice ((rhsOffset.Y + row) * rhs_width + rhsOffset.X, width));
 			}
 		}
 
-		public unsafe override void Apply (ColorBgra* dst, ColorBgra* src, int length)
+		public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 		{
-			unsafe {
-				while (length > 0) {
-					*dst = Apply (*dst, *src);
-					++dst;
-					++src;
-					--length;
-				}
-			}
-		}
-
-		public override void Apply (Cairo.ImageSurface dst, Point dstOffset, Cairo.ImageSurface src, Point srcOffset, int roiLength)
-		{
-			Apply (dst.GetPointAddress (dstOffset), src.GetPointAddress (srcOffset), roiLength);
+			for (int i = 0; i < src.Length; ++i)
+				dst[i] = Apply (dst[i], src[i]);
 		}
 
 		public void Apply (Cairo.ImageSurface dst, Cairo.ImageSurface src)
@@ -115,12 +99,13 @@ namespace Pinta.Core
 				throw new ArgumentException ("dst.Size != src.Size");
 			}
 
-			unsafe {
-				for (int y = 0; y < dst.Height; ++y) {
-					ColorBgra* dstPtr = dst.GetRowAddressUnchecked (y);
-					ColorBgra* srcPtr = src.GetRowAddressUnchecked (y);
-					Apply (dstPtr, srcPtr, dst.Width);
-				}
+			var src_data = src.GetReadOnlyData ();
+			var dst_data = dst.GetData ();
+			int width = src.Width;
+
+			for (int y = 0; y < dst.Height; ++y) {
+				Apply (dst_data.Slice (y * width, width),
+				      src_data.Slice (y * width, width));
 			}
 		}
 
@@ -134,14 +119,15 @@ namespace Pinta.Core
 				throw new ArgumentException ("lhs.Size != rhs.Size");
 			}
 
-			unsafe {
-				for (int y = 0; y < dst.Height; ++y) {
-					ColorBgra* dstPtr = dst.GetRowAddressUnchecked (y);
-					ColorBgra* lhsPtr = lhs.GetRowAddressUnchecked (y);
-					ColorBgra* rhsPtr = rhs.GetRowAddressUnchecked (y);
+			var lhs_data = lhs.GetReadOnlyData ();
+			var rhs_data = rhs.GetReadOnlyData ();
+			var dst_data = dst.GetData ();
+			int width = dst.Width;
 
-					Apply (dstPtr, lhsPtr, rhsPtr, dst.Width);
-				}
+			for (int y = 0; y < dst.Height; ++y) {
+				Apply (dst_data.Slice (y * width, width),
+				      lhs_data.Slice (y * width, width),
+				      rhs_data.Slice (y * width, width));
 			}
 		}
 

Pinta.Core/Effects/PixelOp.cs
@@ -12,7 +12,7 @@ using Cairo;
 namespace Pinta.Core
 {
 	[Serializable]
-	public unsafe abstract class PixelOp //: IPixelOp
+	public abstract class PixelOp //: IPixelOp
 	{
 		public PixelOp ()
 		{
@@ -89,25 +89,27 @@ namespace Pinta.Core
 			// Cache the width and height properties
 			int width = roiSize.Width;
 			int height = roiSize.Height;
+			var src_data = src.GetReadOnlyData ();
+			int src_width = src.Width;
+			var dst_data = dst.GetData ();
+			int dst_width = dst.Width;
 
 			// Do the work.
-			unsafe {
-				for (int row = 0; row < height; ++row) {
-					ColorBgra* dstPtr = dst.GetPointAddress (dstOffset.X, dstOffset.Y + row);
-					ColorBgra* srcPtr = src.GetPointAddress (srcOffset.X, srcOffset.Y + row);
-					Apply (dstPtr, srcPtr, width);
-				}
+			for (int row = 0; row < height; ++row) {
+				Apply (dst_data.Slice ((dstOffset.Y + row) * dst_width + dstOffset.X, width),
+				       src_data.Slice ((srcOffset.Y + row) * src_width + srcOffset.X, width));
 			}
 		}
 
 		public virtual void Apply (ImageSurface dst, Gdk.Point dstOffset, ImageSurface src, Gdk.Point srcOffset, int scanLength)
 		{
-			Apply (dst.GetPointAddress (dstOffset), src.GetPointAddress (srcOffset), scanLength);
+			Apply (dst.GetData ().Slice (dstOffset.Y * dst.Width + dstOffset.X, scanLength),
+			       src.GetData ().Slice (srcOffset.Y * src.Width + srcOffset.X, scanLength));
 		}
 
-		public virtual void Apply (ColorBgra* dst, ColorBgra* src, int length)
+		public virtual void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 		{
-			throw new System.NotImplementedException ("Derived class must implement Apply(ColorBgra*,ColorBgra*,int)");
+			throw new System.NotImplementedException ("Derived class must implement Apply(Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)");
 		}
 	}
 }

Pinta.Core/Effects/UnaryPixelOp.cs
@@ -17,42 +17,33 @@ namespace Pinta.Core
 	/// result of the form: d = F(c)
 	/// </summary>
 	[Serializable]
-	public unsafe abstract class UnaryPixelOp : PixelOp
+	public abstract class UnaryPixelOp : PixelOp
 	{
 		public UnaryPixelOp ()
 		{
 		}
 
-		public abstract ColorBgra Apply (ColorBgra color);
+		public abstract ColorBgra Apply (in ColorBgra color);
 
-		public unsafe override void Apply (ColorBgra* dst, ColorBgra* src, int length)
+		public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 		{
-			unsafe {
-				while (length > 0) {
-					*dst = Apply (*src);
-					++dst;
-					++src;
-					--length;
-				}
-			}
+			//Debug.Assert (dst.Length == src.Length);
+			for (int i = 0; i < src.Length; ++i)
+				dst[i] = Apply (src[i]);
 		}
 
-		public unsafe virtual void Apply (ColorBgra* ptr, int length)
+		public virtual void Apply (Span<ColorBgra> dst)
 		{
-			unsafe {
-				while (length > 0) {
-					*ptr = Apply (*ptr);
-					++ptr;
-					--length;
-				}
-			}
+			for (int i = 0; i < dst.Length; ++i)
+				dst[i] = Apply (dst[i]);
 		}
 
-		private unsafe void ApplyRectangle (ImageSurface surface, Gdk.Rectangle rect)
+		private void ApplyRectangle (ImageSurface surface, Gdk.Rectangle rect)
 		{
-			for (int y = rect.Left; y <= rect.GetBottom (); ++y) {
-				ColorBgra* ptr = surface.GetPointAddress (rect.Left, y);
-				Apply (ptr, rect.Width);
+			var data = surface.GetData ();
+			int width = surface.Width;
+			for (int y = rect.Top; y <= rect.GetBottom (); ++y) {
+				Apply (data.Slice (y * width + rect.Left, rect.Width));
 			}
 		}
 
@@ -63,10 +54,8 @@ namespace Pinta.Core
 			if (regionBounds != Gdk.Rectangle.Intersect (surface.GetBounds (), regionBounds))
 				throw new ArgumentOutOfRangeException ("roi", "Region is out of bounds");
 
-			unsafe {
-				for (int x = startIndex; x < startIndex + length; ++x)
-					ApplyRectangle (surface, roi[x]);
-			}
+			for (int x = startIndex; x < startIndex + length; ++x)
+				ApplyRectangle (surface, roi[x]);
 		}
 
 		public void Apply (ImageSurface surface, Gdk.Rectangle[] roi)
@@ -74,27 +63,21 @@ namespace Pinta.Core
 			Apply (surface, roi, 0, roi.Length);
 		}
 
-		public unsafe void Apply (ImageSurface surface, Gdk.Rectangle roi)
+		public void Apply (ImageSurface surface, Gdk.Rectangle roi)
 		{
 			ApplyRectangle (surface, roi);
 		}
 
-		public override void Apply (ImageSurface dst, Gdk.Point dstOffset, ImageSurface src, Gdk.Point srcOffset, int scanLength)
-		{
-			Apply (dst.GetPointAddress (dstOffset), src.GetPointAddress (srcOffset), scanLength);
-		}
-
 		public void Apply (ImageSurface dst, ImageSurface src, Gdk.Rectangle roi)
 		{
-			ColorBgra* src_data_ptr = (ColorBgra*) src.DataPtr;
+			var src_data = src.GetReadOnlyData ();
+			var dst_data = dst.GetData ();
 			int src_width = src.Width;
-			ColorBgra* dst_data_ptr = (ColorBgra*) dst.DataPtr;
 			int dst_width = dst.Width;
 
 			for (int y = roi.Y; y <= roi.GetBottom (); ++y) {
-				ColorBgra* dstPtr = dst.GetPointAddressUnchecked (dst_data_ptr, dst_width, roi.X, y);
-				ColorBgra* srcPtr = src.GetPointAddressUnchecked (src_data_ptr, src_width, roi.X, y);
-				Apply (dstPtr, srcPtr, roi.Width);
+				Apply (dst_data.Slice (y * dst_width + roi.X, roi.Width),
+				      src_data.Slice (y * src_width + roi.X, roi.Width));
 			}
 		}
 

Pinta.Core/Effects/UnaryPixelOps.cs
@@ -7,7 +7,6 @@
 
 using System;
 
-
 namespace Pinta.Core
 {
 	/// <summary>
@@ -27,21 +26,18 @@ namespace Pinta.Core
 		public class Identity
 		    : UnaryPixelOp
 		{
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				return color;
 			}
 
-			public unsafe override void Apply (ColorBgra* dst, ColorBgra* src, int length)
+			public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 			{
-				for (int i = 0; i < length; i++) {
-					*dst = *src;
-					dst++;
-					src++;
-				}
+				for (int i = 0; i < src.Length; i++)
+					dst[i] = src[i];
 			}
 
-			public unsafe override void Apply (ColorBgra* ptr, int length)
+			public override void Apply (Span<ColorBgra> dst)
 			{
 				return;
 			}
@@ -56,27 +52,21 @@ namespace Pinta.Core
 		{
 			private ColorBgra setColor;
 
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				return setColor;
 			}
 
-			public unsafe override void Apply (ColorBgra* dst, ColorBgra* src, int length)
+			public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 			{
-				while (length > 0) {
-					*dst = setColor;
-					++dst;
-					--length;
-				}
+				for (int i = 0; i < src.Length; i++)
+					dst[i] = setColor;
 			}
 
-			public unsafe override void Apply (ColorBgra* ptr, int length)
+			public override void Apply (Span<ColorBgra> dst)
 			{
-				while (length > 0) {
-					*ptr = setColor;
-					++ptr;
-					--length;
-				}
+				for (int i = 0; i < dst.Length; i++)
+					dst[i] = setColor;
 			}
 
 			public Constant (ColorBgra setColor)
@@ -94,7 +84,7 @@ namespace Pinta.Core
 		{
 			private ColorBgra blendColor;
 
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				int a = blendColor.A;
 				int invA = 255 - a;
@@ -124,32 +114,26 @@ namespace Pinta.Core
 			private int channel;
 			private byte setValue;
 
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
-				color[channel] = setValue;
+				ColorBgra result = color;
+				result[channel] = setValue;
 				return color;
 			}
 
-			public override unsafe void Apply (ColorBgra* dst, ColorBgra* src, int length)
+			public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 			{
-				while (length > 0) {
-					*dst = *src;
-					(*dst)[channel] = setValue;
-					++dst;
-					++src;
-					--length;
+				for (int i = 0; i < src.Length; ++i) {
+					dst[i] = src[i];
+					dst[i][channel] = setValue;
 				}
 			}
 
-			public override unsafe void Apply (ColorBgra* ptr, int length)
+			public override void Apply (Span<ColorBgra> dst)
 			{
-				while (length > 0) {
-					(*ptr)[channel] = setValue;
-					++ptr;
-					--length;
-				}
+				for (int i = 0; i < dst.Length; ++i)
+					dst[i][channel] = setValue;
 			}
-
 
 			public SetChannel (int channel, byte setValue)
 			{
@@ -171,28 +155,21 @@ namespace Pinta.Core
 		{
 			private UInt32 addValue;
 
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				return ColorBgra.FromUInt32 ((color.Bgra & 0x00ffffff) + addValue);
 			}
 
-			public override unsafe void Apply (ColorBgra* dst, ColorBgra* src, int length)
+			public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 			{
-				while (length > 0) {
-					dst->Bgra = (src->Bgra & 0x00ffffff) + addValue;
-					++dst;
-					++src;
-					--length;
-				}
+				for (int i = 0; i < src.Length; ++i)
+					dst[i].Bgra = (src[i].Bgra & 0x00ffffff) + addValue;
 			}
 
-			public override unsafe void Apply (ColorBgra* ptr, int length)
+			public override void Apply (Span<ColorBgra> dst)
 			{
-				while (length > 0) {
-					ptr->Bgra = (ptr->Bgra & 0x00ffffff) + addValue;
-					++ptr;
-					--length;
-				}
+				for (int i = 0; i < dst.Length; ++i)
+					dst[i].Bgra = (dst[i].Bgra & 0x00ffffff) + addValue;
 			}
 
 			public SetAlphaChannel (byte alphaValue)
@@ -208,28 +185,21 @@ namespace Pinta.Core
 		public class SetAlphaChannelTo255
 		    : UnaryPixelOp
 		{
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				return ColorBgra.FromUInt32 (color.Bgra | 0xff000000);
 			}
 
-			public override unsafe void Apply (ColorBgra* dst, ColorBgra* src, int length)
+			public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 			{
-				while (length > 0) {
-					dst->Bgra = src->Bgra | 0xff000000;
-					++dst;
-					++src;
-					--length;
-				}
+				for (int i = 0; i < src.Length; ++i)
+					dst[i].Bgra = src[i].Bgra | 0xff000000;
 			}
 
-			public override unsafe void Apply (ColorBgra* ptr, int length)
+			public override void Apply (Span<ColorBgra> dst)
 			{
-				while (length > 0) {
-					ptr->Bgra |= 0xff000000;
-					++ptr;
-					--length;
-				}
+				for (int i = 0; i < dst.Length; ++i)
+					dst[i].Bgra |= 0xff000000;
 			}
 		}
 
@@ -240,7 +210,7 @@ namespace Pinta.Core
 		public class Invert
 		    : UnaryPixelOp
 		{
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				//Note: Cairo images use premultiplied alpha values
 				//The formula for changing B would be: (255 - B * 255 / A) * A / 255
@@ -265,7 +235,7 @@ namespace Pinta.Core
 				setSaturation = (double) sat / 100;
 			}
 
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				// The higher the saturation, the more red it is
 				int saturation = GetSaturation (color);
@@ -284,7 +254,7 @@ namespace Pinta.Core
 			}
 
 			//Saturation formula from RgbColor.cs, public HsvColor ToHsv()
-			private int GetSaturation (ColorBgra color)
+			private int GetSaturation (in ColorBgra color)
 			{
 				double min;
 				double max;
@@ -320,7 +290,7 @@ namespace Pinta.Core
 		public class InvertWithAlpha
 		    : UnaryPixelOp
 		{
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				return ColorBgra.FromBgra ((byte) (255 - color.B), (byte) (255 - color.G), (byte) (255 - color.R), (byte) (255 - color.A));
 			}
@@ -334,7 +304,7 @@ namespace Pinta.Core
 		public class AverageChannels
 		    : UnaryPixelOp
 		{
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				byte average = (byte) (((int) color.R + (int) color.G + (int) color.B) / 3);
 				return ColorBgra.FromBgra (average, average, average, color.A);
@@ -345,39 +315,30 @@ namespace Pinta.Core
 		public class Desaturate
 		    : UnaryPixelOp
 		{
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				byte i = color.GetIntensityByte ();
 				return ColorBgra.FromBgra (i, i, i, color.A);
 			}
 
-			public unsafe override void Apply (ColorBgra* ptr, int length)
+			public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 			{
-				while (length > 0) {
-					byte i = ptr->GetIntensityByte ();
-
-					ptr->R = i;
-					ptr->G = i;
-					ptr->B = i;
-
-					++ptr;
-					--length;
+				for (int i = 0; i < src.Length; ++i) {
+					ref ColorBgra result = ref dst[i];
+					byte val = src[i].GetIntensityByte ();
+					dst[i] = ColorBgra.FromBgra(val, val, val, src[i].A);
+					result.R = result.G = result.B = val;
+					result.A = src[i].A;
 				}
 			}
 
-			public unsafe override void Apply (ColorBgra* dst, ColorBgra* src, int length)
+			public override void Apply (Span<ColorBgra> dst)
 			{
-				while (length > 0) {
-					byte i = src->GetIntensityByte ();
+				for (int i = 0; i < dst.Length; ++i) {
+					ref ColorBgra result = ref dst[i];
+					byte val = result.GetIntensityByte ();
+					result.R = result.G = result.B = val;
 
-					dst->B = i;
-					dst->G = i;
-					dst->R = i;
-					dst->A = src->A;
-
-					++dst;
-					++src;
-					--length;
 				}
 			}
 		}
@@ -395,7 +356,7 @@ namespace Pinta.Core
 				}
 			}
 
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				byte lumi = color.GetIntensityByte ();
 				int diff = Curve[lumi] - lumi;
@@ -425,31 +386,30 @@ namespace Pinta.Core
 				}
 			}
 
-			public override unsafe void Apply (ColorBgra* dst, ColorBgra* src, int length)
+			public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 			{
-				while (--length >= 0) {
-					dst->B = CurveB[src->B];
-					dst->G = CurveG[src->G];
-					dst->R = CurveR[src->R];
-					dst->A = src->A;
-
-					++dst;
-					++src;
+				for (int i = 0; i < src.Length; ++i) {
+					ref ColorBgra d = ref dst[i];
+					ref readonly ColorBgra s = ref src[i];
+					d.B = CurveB[s.B];
+					d.G = CurveG[s.G];
+					d.R = CurveR[s.R];
+					d.A = s.A;
 				}
 			}
 
-			public override unsafe void Apply (ColorBgra* ptr, int length)
+			public override void Apply (Span<ColorBgra> dst)
 			{
-				while (--length >= 0) {
-					ptr->B = CurveB[ptr->B];
-					ptr->G = CurveG[ptr->G];
-					ptr->R = CurveR[ptr->R];
+				for (int i = 0; i < dst.Length; ++i) {
+					ref ColorBgra d = ref dst[i];
+					d.B = CurveB[d.B];
+					d.G = CurveG[d.G];
+					d.R = CurveR[d.R];
 
-					++ptr;
 				}
 			}
 
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				return ColorBgra.FromBgra (CurveB[color.B], CurveG[color.G], CurveR[color.R], color.A);
 			}
@@ -770,9 +730,10 @@ namespace Pinta.Core
 				}
 			}
 
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra src_color)
 			{
 				//adjust saturation
+				ColorBgra color = src_color;
 				byte intensity = color.GetIntensityByte ();
 				color.R = Utility.ClampToByte ((intensity * 1024 + (color.R - intensity) * satFactor) >> 10);
 				color.G = Utility.ClampToByte ((intensity * 1024 + (color.G - intensity) * satFactor) >> 10);
@@ -845,34 +806,31 @@ namespace Pinta.Core
 				return levels;
 			}
 
-			public override ColorBgra Apply (ColorBgra color)
+			public override ColorBgra Apply (in ColorBgra color)
 			{
 				return ColorBgra.FromBgra (blueLevels[color.B], greenLevels[color.G], redLevels[color.R], color.A);
 			}
 
-			public unsafe override void Apply (ColorBgra* ptr, int length)
+			public override void Apply (Span<ColorBgra> dst, ReadOnlySpan<ColorBgra> src)
 			{
-				while (length > 0) {
-					ptr->B = this.blueLevels[ptr->B];
-					ptr->G = this.greenLevels[ptr->G];
-					ptr->R = this.redLevels[ptr->R];
-
-					++ptr;
-					--length;
+				for (int i = 0; i < src.Length; ++i) {
+					ref ColorBgra d = ref dst[i];
+					ref readonly ColorBgra s = ref src[i];
+					d.B = blueLevels[s.B];
+					d.G = greenLevels[s.G];
+					d.R = redLevels[s.R];
+					d.A = s.A;
 				}
 			}
 
-			public unsafe override void Apply (ColorBgra* dst, ColorBgra* src, int length)
+			public override void Apply (Span<ColorBgra> dst)
 			{
-				while (length > 0) {
-					dst->B = this.blueLevels[src->B];
-					dst->G = this.greenLevels[src->G];
-					dst->R = this.redLevels[src->R];
-					dst->A = src->A;
+				for (int i = 0; i < dst.Length; ++i) {
+					ref ColorBgra d = ref dst[i];
+					d.B = blueLevels[d.B];
+					d.G = greenLevels[d.G];
+					d.R = redLevels[d.R];
 
-					++dst;
-					++src;
-					--length;
 				}
 			}
 		}

commit 23cec394de7243dfee7ba66538faebfbf7d08d54

Fix a regression from baaa889a that caused incorrect behaviour in the Glow effect

Pinta.Effects/Effects/GlowEffect.cs
@@ -62,7 +62,7 @@ public sealed class GlowEffect : BaseEffect
 		foreach (Core.RectangleI roi in rois) {
 			for (int y = roi.Top; y <= roi.Bottom; ++y) {
 				var dst_row = dst_data.Slice (y * dst_width + roi.Left, roi.Width);
-				var src_row = dst_data.Slice (y * src_width + roi.Left, roi.Width);
+				var src_row = src_data.Slice (y * src_width + roi.Left, roi.Width);
 				screen_blend_op.Apply (dst_row, src_row, dst_row);
 			}
 		}