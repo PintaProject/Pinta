35ce66a0b2b2826bf3a1ac6521c36b43ed76303a
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2023-10-02 03:30:14 +0200

    Split `JuliaFractalEffect.Render` into several, smaller methods (#534)
    * Split `JuliaFractalEffect.Render` into several, smaller methods
    
    * Returned value directly instead of reassigning `c` again
    
    * Sorted things out so that `GetPixelColor` can be made `static`
    
    * Used `PointI` instead of (now removed) `LoopContext`
    
    ---------
    
    Co-authored-by: Lehonti Ramos <lehonti@ramos>

--- Pinta.Effects/Effects/JuliaFractalEffect.cs
+++ Pinta.Effects/Effects/JuliaFractalEffect.cs
@@ -42,91 +42,110 @@ public sealed class JuliaFractalEffect : BaseEffect
 	private static double Julia (double x, double y, double r, double i)
 	{
 		double c = 0;
-
-		while (c < 256 && x * x + y * y < 10000) {
+		while (c < 256 && ((x * x) + (y * y) < 10000)) {
 			double t = x;
-			x = x * x - y * y + r;
-			y = 2 * t * y + i;
+			x = (x * x) - (y * y) + r;
+			y = (2 * t * y) + i;
 			++c;
 		}
-
-		c -= 2 - 2 * log2_10000 / Math.Log (x * x + y * y);
-
-		return c;
+		return c - (2 - 2 * log2_10000 / Math.Log ((x * x) + (y * y)));
 	}
 
 	public override void Render (ImageSurface src, ImageSurface dst, ReadOnlySpan<RectangleI> rois)
 	{
-		const double jr = 0.3125;
-		const double ji = 0.03;
-
-		int w = dst.Width;
-		int h = dst.Height;
-		double invH = 1.0 / h;
-		double invZoom = 1.0 / Data.Zoom;
-		double invQuality = 1.0 / Data.Quality;
-		double aspect = (double) h / (double) w;
-		int count = Data.Quality * Data.Quality + 1;
-		double invCount = 1.0 / (double) count;
-		double angleTheta = (Data.Angle * Math.PI * 2) / 360.0;
-
+		JuliaSettings settings = CreateSettings (dst);
 		Span<ColorBgra> dst_data = dst.GetPixelData ();
-		int dst_width = dst.Width;
-
 		foreach (Core.RectangleI rect in rois) {
 			for (int y = rect.Top; y <= rect.Bottom; y++) {
-				var dst_row = dst_data.Slice (y * dst_width, dst_width);
-
+				var dst_row = dst_data.Slice (y * settings.w, settings.w);
 				for (int x = rect.Left; x <= rect.Right; x++) {
-					int r = 0;
-					int g = 0;
-					int b = 0;
-					int a = 0;
-
-					for (double i = 0; i < count; i++) {
-						double u = (2.0 * x - w + (i * invCount)) * invH;
-						double v = (2.0 * y - h + ((i * invQuality) % 1)) * invH;
-
-						double radius = Math.Sqrt ((u * u) + (v * v));
-						double radiusP = radius;
-						double theta = Math.Atan2 (v, u);
-						double thetaP = theta + angleTheta;
-
-						double uP = radiusP * Math.Cos (thetaP);
-						double vP = radiusP * Math.Sin (thetaP);
-
-						double jX = (uP - vP * aspect) * invZoom;
-						double jY = (vP + uP * aspect) * invZoom;
-
-						double j = Julia (jX, jY, jr, ji);
-
-						double c = Data.Factor * j;
-
-						b += Utility.ClampToByte (c - 768);
-						g += Utility.ClampToByte (c - 512);
-						r += Utility.ClampToByte (c - 256);
-						a += Utility.ClampToByte (c - 0);
-					}
-
-					dst_row[x] = ColorBgra.FromBgra (Utility.ClampToByte (b / count), Utility.ClampToByte (g / count), Utility.ClampToByte (r / count), Utility.ClampToByte (a / count));
+					PointI target = new (x, y);
+					dst_row[x] = GetPixelColor (settings, target);
 				}
 			}
 		}
+	}
+
+	private sealed record JuliaSettings (
+		int w,
+		int h,
+		double invH,
+		double invZoom,
+		double invQuality,
+		double aspect,
+		int count,
+		double invCount,
+		double angleTheta,
+		int factor);
+	private JuliaSettings CreateSettings (ImageSurface dst)
+	{
+		var w = dst.Width;
+		var h = dst.Height;
+		var count = Data.Quality * Data.Quality + 1;
+		return new (
+			w: w,
+			h: h,
+			invH: 1.0 / h,
+			invZoom: 1.0 / Data.Zoom,
+			invQuality: 1.0 / Data.Quality,
+			aspect: (double) h / (double) w,
+			count: count,
+			invCount: 1.0 / (double) count,
+			angleTheta: (Data.Angle * Math.PI * 2) / 360.0,
+			factor: Data.Factor
+		);
+	}
+
+	const double Jr = 0.3125;
+	const double Ji = 0.03;
+	private static ColorBgra GetPixelColor (JuliaSettings settings, PointI target)
+	{
+		int r = 0;
+		int g = 0;
+		int b = 0;
+		int a = 0;
+
+		for (double i = 0; i < settings.count; i++) {
+			double u = (2.0 * target.X - settings.w + (i * settings.invCount)) * settings.invH;
+			double v = (2.0 * target.Y - settings.h + ((i * settings.invQuality) % 1)) * settings.invH;
+
+			double radius = Math.Sqrt ((u * u) + (v * v));
+			double radiusP = radius;
+			double theta = Math.Atan2 (v, u);
+			double thetaP = theta + settings.angleTheta;
+
+			double uP = radiusP * Math.Cos (thetaP);
+			double vP = radiusP * Math.Sin (thetaP);
+
+			double jX = (uP - vP * settings.aspect) * settings.invZoom;
+			double jY = (vP + uP * settings.aspect) * settings.invZoom;
+
+			double j = Julia (jX, jY, Jr, Ji);
+
+			double c = settings.factor * j;
+
+			b += Utility.ClampToByte (c - 768);
+			g += Utility.ClampToByte (c - 512);
+			r += Utility.ClampToByte (c - 256);
+			a += Utility.ClampToByte (c - 0);
+		}
+
+		return ColorBgra.FromBgra (Utility.ClampToByte (b / settings.count), Utility.ClampToByte (g / settings.count), Utility.ClampToByte (r / settings.count), Utility.ClampToByte (a / settings.count));
 	}
 	#endregion
 
 	public sealed class JuliaFractalData : EffectData
 	{
 		[Caption ("Factor"), MinimumValue (1), MaximumValue (10)]
-		public int Factor = 4;
+		public int Factor { get; set; } = 4;
 
 		[Caption ("Quality"), MinimumValue (1), MaximumValue (5)]
-		public int Quality = 2;
+		public int Quality { get; set; } = 2;
 
 		[Caption ("Zoom"), MinimumValue (0), MaximumValue (50)]
-		public int Zoom = 1;
+		public int Zoom { get; set; } = 1;
 
 		[Caption ("Angle")]
-		public double Angle = 0;
+		public double Angle { get; set; } = 0;
 	}
 }
