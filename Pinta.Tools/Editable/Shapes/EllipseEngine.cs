8b9df3bc033397c6621df16cb04d1d8259b638bb
Lehonti Ramos <lehonti@ramos>
2023-10-02 19:27:14 +0200

    Split `EllipseEngine.GeneratePoints` into smaller generator methods

--- Pinta.Tools/Editable/Shapes/EllipseEngine.cs
+++ Pinta.Tools/Editable/Shapes/EllipseEngine.cs
@@ -25,6 +25,8 @@
 // THE SOFTWARE.
 
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using Cairo;
 using Pinta.Core;
 
@@ -80,150 +82,179 @@ public class EllipseEngine : ShapeEngine
 	/// </summary>
 	public override void GeneratePoints (int brush_width)
 	{
-		List<GeneratedPoint> points = new List<GeneratedPoint> ();
+		var points = CreatePoints ().ToImmutableArray ();
+		var fallbackPoints = CreateFallbackPoints (points);
+		GeneratedPoints = points.Concat (fallbackPoints).ToArray ();
+	}
 
-		//An ellipse requires exactly 4 control points in order to draw anything.
-		if (ControlPoints.Count == 4) {
-			//This is mostly for time efficiency/optimization, but it can also help readability.
-			PointD cp0 = ControlPoints[0].Position, cp1 = ControlPoints[1].Position,
-				cp2 = ControlPoints[2].Position, cp3 = ControlPoints[3].Position;
-
-			//An ellipse also requires that all 4 control points compose a perfect rectangle parallel/perpendicular to the window.
-			//So, confirm that it is indeed a perfect rectangle.
-			bool perfectRectangle = IsPerfectRectangle (cp0, cp1, cp2, cp3);
-
-			if (perfectRectangle) {
-				//It is expected that the 4 control points always form a perfect rectangle parallel/perpendicular to the window.
-				//However, we must first determine which control point is at the top left and which is at the bottom right.
-				//It is also expected that the 4 control points are adjacent to each other by index and position, e.g.: 0, 1, 2, 3.
-
-				PointD topLeft = cp0;
-				PointD bottomRight = cp0;
-
-				//Compare the second point with the first.
-				if (cp1.X < topLeft.X || cp1.Y < topLeft.Y) {
-					//The second point is either more left or more up than the first.
-
-					topLeft = cp1;
-
-					//Compare the third point with the second.
-					if (cp2.X < topLeft.X || cp2.Y < topLeft.Y) {
-						//The third point is either more left or more up than the second.
-
-						topLeft = cp2;
-
-						//The first point remains the bottom right.
-					} else {
-						//The third point is neither more left nor more up than the second.
-
-						//The second point remains the top left.
-
-						bottomRight = cp3;
-					}
-				} else {
-					//The second point is neither more left nor more up than the first.
-
-					PointD secondPoint = cp1;
-
-					//Compare the third point with the second.
-					if (cp2.X < secondPoint.X || cp2.Y < secondPoint.Y) {
-						//The third point is either more left or more up than the second.
-
-						topLeft = cp3;
-						bottomRight = cp1;
-					} else {
-						//The third point is neither more left nor more up than the second.
-
-						//The first point remains the top left.
-
-						bottomRight = cp2;
-					}
-				}
-
-				//Now we can calculate the width and height.
-				double width = bottomRight.X - topLeft.X;
-				double height = bottomRight.Y - topLeft.Y;
-
-				//Some elliptic math code taken from Cairo Extensions, and some from DocumentSelection code written for GSoC 2013.
-
-				//Calculate an appropriate interval at which to increment t based on
-				//the bounding rectangle's width and height properties. The increment
-				//for t determines how many intermediate Points to calculate for the
-				//ellipse. For each curve, t will go from tInterval to 1. The lower
-				//the value of tInterval, the higher number of intermediate Points
-				//that will be calculated and stored into the Polygon collection.
-				double tInterval = .02d;
-
-				double rx = width / 2d; //1/2 of the bounding Rectangle Width.
-				double ry = height / 2d; //1/2 of the bounding Rectangle Height.
-				double cx = topLeft.X + rx; //The middle of the bounding Rectangle, horizontally speaking.
-				double cy = topLeft.Y + ry; //The middle of the bounding Rectangle, vertically speaking.
-				double c1 = 0.5522847498307933984022516322796d; //tan(pi / 8d) * 4d / 3d ~= 0.5522847498307933984022516322796d
-
-				points.AddRange (calculateCurvePoints (tInterval,
-					cx + rx, cy,
-					cx + rx, cy - c1 * ry,
-					cx + c1 * rx, cy - ry,
-					cx, cy - ry,
-					3));
-
-				points.AddRange (calculateCurvePoints (tInterval,
-					cx, cy - ry,
-					cx - c1 * rx, cy - ry,
-					cx - rx, cy - c1 * ry,
-					cx - rx, cy,
-					0));
-
-				points.AddRange (calculateCurvePoints (tInterval,
-					cx - rx, cy,
-					cx - rx, cy + c1 * ry,
-					cx - c1 * rx, cy + ry,
-					cx, cy + ry,
-					1));
-
-				points.AddRange (calculateCurvePoints (tInterval,
-					cx, cy + ry,
-					cx + c1 * rx, cy + ry,
-					cx + rx, cy + c1 * ry,
-					cx + rx, cy,
-					2));
-
-				// Close the curve.
-				points.Add (new GeneratedPoint (new PointD (cx + rx, cy), 3));
-			}
-		}
-
+	private IEnumerable<GeneratedPoint> CreateFallbackPoints (ImmutableArray<GeneratedPoint> points)
+	{
 		//Make sure there are now generated points; otherwise, one of the ellipse conditions was not met.
-		if (points.Count == 0) {
-			//Something went wrong. Just copy the control points. Note: it's important that there be many generated points even if
-			//everything is just a linear connection of control points. This is because the generated points are used in the check
-			//that determines if the mouse clicks on the shape.
+		if (points.Length != 0)
+			yield break;
 
-			int nextNum;
+		//Something went wrong. Just copy the control points. Note: it's important that there be many generated points even if
+		//everything is just a linear connection of control points. This is because the generated points are used in the check
+		//that determines if the mouse clicks on the shape.
 
-			PointD currentPoint, nextPoint;
+		int nextNum;
 
-			//Go through each control point.
-			for (int currentNum = 0; currentNum < ControlPoints.Count; ++currentNum) {
-				//Determine the next control point.
+		PointD currentPoint, nextPoint;
 
-				nextNum = currentNum + 1;
+		//Go through each control point.
+		for (int currentNum = 0; currentNum < ControlPoints.Count; ++currentNum) {
+			//Determine the next control point.
 
-				if (nextNum >= ControlPoints.Count) {
-					nextNum = 0;
-				}
+			nextNum = currentNum + 1;
 
-				currentPoint = ControlPoints[currentNum].Position;
-				nextPoint = ControlPoints[nextNum].Position;
+			if (nextNum >= ControlPoints.Count) {
+				nextNum = 0;
+			}
 
-				//Lerp from the current point to the next point.
-				for (float lerpPos = 0.0f; lerpPos < 1.0f; lerpPos += 0.01f) {
-					points.Add (new GeneratedPoint (Utility.Lerp (currentPoint, nextPoint, lerpPos), currentNum));
-				}
+			currentPoint = ControlPoints[currentNum].Position;
+			nextPoint = ControlPoints[nextNum].Position;
+
+			//Lerp from the current point to the next point.
+			for (float lerpPos = 0.0f; lerpPos < 1.0f; lerpPos += 0.01f)
+				yield return new GeneratedPoint (Utility.Lerp (currentPoint, nextPoint, lerpPos), currentNum);
+		}
+	}
+
+	private IEnumerable<GeneratedPoint> CreatePoints ()
+	{
+		//An ellipse requires exactly 4 control points in order to draw anything.
+		if (ControlPoints.Count != 4)
+			yield break;
+
+		//This is mostly for time efficiency/optimization, but it can also help readability.
+		PointD
+			cp0 = ControlPoints[0].Position, cp1 = ControlPoints[1].Position,
+			cp2 = ControlPoints[2].Position, cp3 = ControlPoints[3].Position;
+
+		//An ellipse also requires that all 4 control points compose a perfect rectangle parallel/perpendicular to the window.
+		//So, confirm that it is indeed a perfect rectangle.
+		bool perfectRectangle = IsPerfectRectangle (cp0, cp1, cp2, cp3);
+
+		if (!perfectRectangle)
+			yield break;
+
+		//It is expected that the 4 control points always form a perfect rectangle parallel/perpendicular to the window.
+		//However, we must first determine which control point is at the top left and which is at the bottom right.
+		//It is also expected that the 4 control points are adjacent to each other by index and position, e.g.: 0, 1, 2, 3.
+
+		PointD topLeft = cp0;
+		PointD bottomRight = cp0;
+
+		//Compare the second point with the first.
+		if (cp1.X < topLeft.X || cp1.Y < topLeft.Y) {
+			//The second point is either more left or more up than the first.
+
+			topLeft = cp1;
+
+			//Compare the third point with the second.
+			if (cp2.X < topLeft.X || cp2.Y < topLeft.Y) {
+				//The third point is either more left or more up than the second.
+
+				topLeft = cp2;
+
+				//The first point remains the bottom right.
+			} else {
+				//The third point is neither more left nor more up than the second.
+
+				//The second point remains the top left.
+
+				bottomRight = cp3;
+			}
+		} else {
+			//The second point is neither more left nor more up than the first.
+
+			PointD secondPoint = cp1;
+
+			//Compare the third point with the second.
+			if (cp2.X < secondPoint.X || cp2.Y < secondPoint.Y) {
+				//The third point is either more left or more up than the second.
+
+				topLeft = cp3;
+				bottomRight = cp1;
+			} else {
+				//The third point is neither more left nor more up than the second.
+
+				//The first point remains the top left.
+
+				bottomRight = cp2;
 			}
 		}
 
-		GeneratedPoints = points.ToArray ();
+		//Now we can calculate the width and height.
+		double width = bottomRight.X - topLeft.X;
+		double height = bottomRight.Y - topLeft.Y;
+
+		//Some elliptic math code taken from Cairo Extensions, and some from DocumentSelection code written for GSoC 2013.
+
+		//Calculate an appropriate interval at which to increment t based on
+		//the bounding rectangle's width and height properties. The increment
+		//for t determines how many intermediate Points to calculate for the
+		//ellipse. For each curve, t will go from tInterval to 1. The lower
+		//the value of tInterval, the higher number of intermediate Points
+		//that will be calculated and stored into the Polygon collection.
+		double tInterval = .02d;
+
+		double rx = width / 2d; //1/2 of the bounding Rectangle Width.
+		double ry = height / 2d; //1/2 of the bounding Rectangle Height.
+		double cx = topLeft.X + rx; //The middle of the bounding Rectangle, horizontally speaking.
+		double cy = topLeft.Y + ry; //The middle of the bounding Rectangle, vertically speaking.
+		double c1 = 0.5522847498307933984022516322796d; //tan(pi / 8d) * 4d / 3d ~= 0.5522847498307933984022516322796d
+
+		foreach (
+			var p in
+			calculateCurvePoints (
+				tInterval,
+				cx + rx, cy,
+				cx + rx, cy - c1 * ry,
+				cx + c1 * rx, cy - ry,
+				cx, cy - ry,
+				3
+			)
+		) yield return p;
+
+		foreach (
+			var p in
+			calculateCurvePoints (
+				tInterval,
+				cx, cy - ry,
+				cx - c1 * rx, cy - ry,
+				cx - rx, cy - c1 * ry,
+				cx - rx, cy,
+				0
+			)
+		) yield return p;
+
+		foreach (
+			var p in
+			calculateCurvePoints (
+				tInterval,
+				cx - rx, cy,
+				cx - rx, cy + c1 * ry,
+				cx - c1 * rx, cy + ry,
+				cx, cy + ry,
+				1
+			)
+		) yield return p;
+
+		foreach (
+			var p in
+			calculateCurvePoints (
+				tInterval,
+				cx, cy + ry,
+				cx + c1 * rx, cy + ry,
+				cx + rx, cy + c1 * ry,
+				cx + rx, cy,
+				2
+			)
+		) yield return p;
+
+		// Close the curve.
+		yield return new GeneratedPoint (new PointD (cx + rx, cy), 3);
 	}
 
 	/// <summary>
@@ -240,23 +271,12 @@ public class EllipseEngine : ShapeEngine
 	/// <param name="y3">Ending point Y (included in the returned Point(s)).</param>
 	/// <param name="cPIndex">The index of the previous ControlPoint to the generated points.</param>
 	/// <returns></returns>
-	protected static List<GeneratedPoint> calculateCurvePoints (
+	protected static IEnumerable<GeneratedPoint> calculateCurvePoints (
 		double tInterval,
 		double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3,
 		int cPIndex)
 	{
-		List<GeneratedPoint> calculatedPoints = new List<GeneratedPoint> ((int) (1d / tInterval));
-
-		double oneMinusT;
-		double oneMinusTSquared;
-		double oneMinusTCubed;
-
-		double tSquared;
-		double tCubed;
-
-		double oneMinusTSquaredTimesTTimesThree;
-		double oneMinusTTimesTSquaredTimesThree;
-
+		//Generates points of partial Polygon containing the calculated Points in the curve.
 		for (double t = 0; t < 1d; t += tInterval) {
 			//There are 3 "layers" in a cubic Bezier curve's calculation. These "layers"
 			//must be calculated for each intermediate Point (for each value of t from
@@ -269,23 +289,23 @@ public class EllipseEngine : ShapeEngine
 
 			//Note: the code below is an optimized version of the commented explanation above.
 
-			oneMinusT = 1d - t;
-			oneMinusTSquared = oneMinusT * oneMinusT;
-			oneMinusTCubed = oneMinusTSquared * oneMinusT;
+			double oneMinusT = 1d - t;
+			double oneMinusTSquared = oneMinusT * oneMinusT;
+			double oneMinusTCubed = oneMinusTSquared * oneMinusT;
 
-			tSquared = t * t;
-			tCubed = tSquared * t;
+			double tSquared = t * t;
+			double tCubed = tSquared * t;
 
-			oneMinusTSquaredTimesTTimesThree = oneMinusTSquared * t * 3d;
-			oneMinusTTimesTSquaredTimesThree = oneMinusT * tSquared * 3d;
+			double oneMinusTSquaredTimesTTimesThree = oneMinusTSquared * t * 3d;
+			double oneMinusTTimesTSquaredTimesThree = oneMinusT * tSquared * 3d;
 
-			calculatedPoints.Add (new GeneratedPoint (new PointD (
-				(oneMinusTCubed * x0 + oneMinusTSquaredTimesTTimesThree * x1 + oneMinusTTimesTSquaredTimesThree * x2 + tCubed * x3),
-				(oneMinusTCubed * y0 + oneMinusTSquaredTimesTTimesThree * y1 + oneMinusTTimesTSquaredTimesThree * y2 + tCubed * y3)),
-				cPIndex));
+			yield return new GeneratedPoint (
+				new PointD (
+					X: oneMinusTCubed * x0 + oneMinusTSquaredTimesTTimesThree * x1 + oneMinusTTimesTSquaredTimesThree * x2 + tCubed * x3,
+					Y: oneMinusTCubed * y0 + oneMinusTSquaredTimesTTimesThree * y1 + oneMinusTTimesTSquaredTimesThree * y2 + tCubed * y3
+				),
+				cPIndex
+			);
 		}
-
-		//Return the partial Polygon containing the calculated Points in the curve.
-		return calculatedPoints;
 	}
 }
