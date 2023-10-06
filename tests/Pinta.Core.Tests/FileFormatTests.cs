dae3cada24c911b8f5da89a04119ffd73d8e245b
Lehonti Ramos <lehonti@ramos>
2023-10-06 14:42:20 +0200

    Added more tests and more files (two versions of the output ppm)

--- tests/Pinta.Core.Tests/FileFormatTests.cs
+++ tests/Pinta.Core.Tests/FileFormatTests.cs
@@ -5,7 +5,9 @@ namespace Pinta.Core.Tests;
 [TestFixture]
 internal sealed class FileFormatTests
 {
-	[TestCase ("sixcolorsinput.png", "sixcolorsoutput.ppm")]
+	[TestCase ("sixcolorsinput.png", "sixcolorsoutput_lf.ppm")]
+	[TestCase ("sixcolorsinput.png", "sixcolorsoutput_crlf.ppm")]
+	[TestCase ("sixcolorsoutput_lf.ppm", "sixcolorsoutput_crlf.ppm")]
 	public void Files_NotEqual (string file1, string file2)
 	{
 		Assert.IsFalse (Utilities.AreFilesEqual (file1, file2));
