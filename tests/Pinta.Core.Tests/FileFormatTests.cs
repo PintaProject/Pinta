cd62ca41e15d0b26a0ecc6d54d8488c775510bd0
Lehonti Ramos <lehonti@ramos>
2023-10-10 17:30:22 +0200

    Corrected file names

--- tests/Pinta.Core.Tests/FileFormatTests.cs
+++ tests/Pinta.Core.Tests/FileFormatTests.cs
@@ -7,8 +7,8 @@ namespace Pinta.Core.Tests;
 [TestFixture]
 internal sealed class FileFormatTests
 {
-	[TestCase ("sixcolorsinput.png", "sixcolorsoutput_lf.ppm")]
-	[TestCase ("sixcolorsinput.png", "sixcolorsoutput_crlf.ppm")]
+	[TestCase ("sixcolorsinput.gif", "sixcolorsoutput_lf.ppm")]
+	[TestCase ("sixcolorsinput.gif", "sixcolorsoutput_crlf.ppm")]
 	[TestCase ("sixcolorsoutput_lf.ppm", "sixcolorsoutput_crlf.ppm")]
 	public void Files_NotEqual (string file1, string file2)
 	{
