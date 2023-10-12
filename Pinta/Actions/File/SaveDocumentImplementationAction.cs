6b94335b090ddf6cbee8162d4bf06e003aa604d0
Cameron White <cameronwhite91@gmail.com>
2023-10-12 09:41:26 -0400

    Fix the .NET 6 build by reverting usage of .NET 7 APIs

--- Pinta/Actions/File/SaveDocumentImplementationAction.cs
+++ Pinta/Actions/File/SaveDocumentImplementationAction.cs
@@ -1,21 +1,21 @@
-// 
+//
 // SaveDocumentImplmentationAction.cs
-//  
+//
 // Author:
 //       Jonathan Pobst <monkey@jpobst.com>
-// 
+//
 // Copyright (c) 2010 Jonathan Pobst
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -154,7 +154,9 @@ internal sealed class SaveDocumentImplmentationAction : IActionHandler
 			throw new ArgumentException ("Attempted to save a document with no associated file");
 
 		if (format == null) {
-			ArgumentNullException.ThrowIfNullOrEmpty (document.FileType);
+			if (string.IsNullOrEmpty (document.FileType))
+				throw new ArgumentNullException ($"nameof{document.FileType} must contain value.");
+
 			format = PintaCore.ImageFormats.GetFormatByExtension (document.FileType);
 		}
 
