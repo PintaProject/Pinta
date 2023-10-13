58c28da5898650fdf6b90c00a04e5cc64fa0469a
Lehonti Ramos <lehonti@ramos>
2023-10-13 15:05:47 +0200

    Expanded tests

--- tests/Pinta.Core.Tests/AngleTest.cs
+++ tests/Pinta.Core.Tests/AngleTest.cs
@@ -119,4 +119,76 @@ internal sealed class AngleTest
 		var result = left - right;
 		Assert.AreEqual (result.Revolutions, expectedResult);
 	}
+
+	[TestCase (0d, 0d)]
+	[TestCase (Math.PI * 0.5d, 90d)]
+	[TestCase (Math.PI, 180d)]
+	[TestCase (Math.PI * 1.5d, 270d)]
+	[TestCase (-Math.PI, 180d)]
+	public void Radians_To_Degrees (double radians, double expectedDegrees)
+	{
+		RadiansAngle radiansAngle = new (radians);
+		DegreesAngle degreesAngle = radiansAngle.ToDegrees ();
+		Assert.AreEqual (degreesAngle.Degrees, expectedDegrees);
+	}
+
+	[TestCase (0d, 0d)]
+	[TestCase (Math.PI * 0.5d, 0.25d)]
+	[TestCase (Math.PI, 0.5d)]
+	[TestCase (Math.PI * 1.5d, 0.75d)]
+	[TestCase (-Math.PI, 0.5d)]
+	public void Radians_To_Revolutions (double radians, double expectedRevolutions)
+	{
+		RadiansAngle radiansAngle = new (radians);
+		RevolutionsAngle revolutionsAngle = radiansAngle.ToRevolutions ();
+		Assert.AreEqual (revolutionsAngle.Revolutions, expectedRevolutions);
+	}
+
+	[TestCase (0d, 0d)]
+	[TestCase (90d, Math.PI * 0.5d)]
+	[TestCase (180d, Math.PI)]
+	[TestCase (270d, Math.PI * 1.5d)]
+	[TestCase (-180d, Math.PI)]
+	public void Degrees_To_Radians (double degrees, double expectedRadians)
+	{
+		DegreesAngle degreesAngle = new (degrees);
+		RadiansAngle radiansAngle = degreesAngle.ToRadians ();
+		Assert.AreEqual (radiansAngle.Radians, expectedRadians);
+	}
+
+	[TestCase (0d, 0d)]
+	[TestCase (90d, 0.25d)]
+	[TestCase (180d, 0.5d)]
+	[TestCase (270d, 0.75d)]
+	[TestCase (-180d, 0.5)]
+	public void Degrees_To_Revolutions (double degrees, double expectedRevolutions)
+	{
+		DegreesAngle degreesAngle = new (degrees);
+		RevolutionsAngle revolutionsAngle = degreesAngle.ToRevolutions ();
+		Assert.AreEqual (revolutionsAngle.Revolutions, expectedRevolutions);
+	}
+
+	[TestCase (0d, 0d)]
+	[TestCase (0.25d, Math.PI * 0.5d)]
+	[TestCase (0.5d, Math.PI)]
+	[TestCase (0.75d, Math.PI * 1.5d)]
+	[TestCase (-0.5d, Math.PI)]
+	public void Revolutions_To_Radians (double revolutions, double expectedRadians)
+	{
+		RevolutionsAngle revolutionsAngle = new (revolutions);
+		RadiansAngle radiansAngle = revolutionsAngle.ToRadians ();
+		Assert.AreEqual (radiansAngle.Radians, expectedRadians);
+	}
+
+	[TestCase (0d, 0d)]
+	[TestCase (0.25d, 90d)]
+	[TestCase (0.5d, 180d)]
+	[TestCase (0.75d, 270d)]
+	[TestCase (-0.5, 180d)]
+	public void Revolutions_To_Degrees (double revolutions, double expectedDegrees)
+	{
+		RevolutionsAngle revolutionsAngle = new (revolutions);
+		DegreesAngle degreesAngle = revolutionsAngle.ToDegrees ();
+		Assert.AreEqual (degreesAngle.Degrees, expectedDegrees);
+	}
 }
