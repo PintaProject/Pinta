41babb814b93f23de44284550c54e871590d00fd
Cameron White <cameronwhite91@gmail.com>
2023-10-17 19:05:16 -0400

    Fix nullable warning

--- tests/Pinta.Core.Tests/Utilities.cs
+++ tests/Pinta.Core.Tests/Utilities.cs
@@ -98,7 +98,7 @@ internal static class Utilities
 		var file = Gio.FileHelper.NewForPath (imageFilePath);
 		using var fs = file.Read (null);
 		try {
-			var bg = GdkPixbuf.Pixbuf.NewFromStream (fs, cancellable: null);
+			var bg = GdkPixbuf.Pixbuf.NewFromStream (fs, cancellable: null)!; // NRT: only nullable when error is thrown.
 			var surf = CairoExtensions.CreateImageSurface (Format.Argb32, bg.Width, bg.Height);
 			var context = new Cairo.Context (surf);
 			context.DrawPixbuf (bg, 0, 0);
