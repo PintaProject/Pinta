a34c830e21c65630979b9c98719c04b2192c776e
Lehonti Ramos <lehonti@ramos>
2023-10-17 15:47:47 +0200

    Better naming

--- Pinta.Core/Effects/ImmutableColorGradient.cs
+++ Pinta.Core/Effects/ImmutableColorGradient.cs
@@ -1,61 +1,63 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
 namespace Pinta.Core;
 
 public sealed class ImmutableColorGradient
 {
-	private readonly ColorBgra _startColor;
-	private readonly ColorBgra _endColor;
-	private readonly double _minPosition;
-	private readonly double _maxPosition;
-	private readonly IReadOnlyList<KeyValuePair<double, ColorBgra>> _sortedStops;
+	public ColorBgra StartColor { get; }
+	public ColorBgra EndColor { get; }
+	public double MinPosition { get; }
+	public double MaxPosition { get; }
+
+	public ImmutableArray<KeyValuePair<double, ColorBgra>> SortedStops { get; }
 
 	public ImmutableColorGradient (
 		ColorBgra startColor,
 		ColorBgra endColor,
-		double minValue,
-		double maxValue,
+		double minPosition,
+		double maxPosition,
 		IEnumerable<KeyValuePair<double, ColorBgra>> gradientStops)
 	{
-		if (minValue >= maxValue) throw new ArgumentException ($"{nameof (minValue)} has to be lower than {nameof (maxValue)}");
-		var sortedStops = gradientStops.OrderBy (kvp => kvp.Key).ToArray ();
-		if (sortedStops.Length > 0 && sortedStops[0].Key <= minValue) throw new ArgumentException ($"Lowest key in {nameof (gradientStops)} has to be greater than {nameof (minValue)}");
-		if (sortedStops.Length > 0 && sortedStops[^1].Key >= maxValue) throw new ArgumentException ($"Greatest key in {nameof (gradientStops)} has to be lower than {nameof (maxValue)}");
+		if (minPosition >= maxPosition) throw new ArgumentException ($"{nameof (minPosition)} has to be lower than {nameof (maxPosition)}");
+		var sortedStops = gradientStops.OrderBy (kvp => kvp.Key).ToImmutableArray ();
+		if (sortedStops.Length > 0 && sortedStops[0].Key <= minPosition) throw new ArgumentException ($"Lowest key in {nameof (gradientStops)} has to be greater than {nameof (minPosition)}");
+		if (sortedStops.Length > 0 && sortedStops[^1].Key >= maxPosition) throw new ArgumentException ($"Greatest key in {nameof (gradientStops)} has to be lower than {nameof (maxPosition)}");
 
-		_startColor = startColor;
-		_endColor = endColor;
-		_minPosition = minValue;
-		_maxPosition = maxValue;
-		_sortedStops = sortedStops;
+		StartColor = startColor;
+		EndColor = endColor;
+		MinPosition = minPosition;
+		MaxPosition = maxPosition;
+		SortedStops = sortedStops;
 	}
 
 	public ColorBgra GetColor (double position)
 	{
-		if (position == _minPosition) return _startColor;
-		if (position == _maxPosition) return _endColor;
-		if (position < _minPosition) throw new ArgumentOutOfRangeException (nameof (position));
-		if (position > _maxPosition) throw new ArgumentOutOfRangeException (nameof (position));
-		if (_sortedStops.Count == 0) return HandleNoStops (position);
+		if (position < MinPosition) throw new ArgumentOutOfRangeException (nameof (position));
+		if (position > MaxPosition) throw new ArgumentOutOfRangeException (nameof (position));
+		if (position == MinPosition) return StartColor;
+		if (position == MaxPosition) return EndColor;
+		if (SortedStops.Length == 0) return HandleNoStops (position);
 		return HandleWithStops (position);
 	}
 
 	private ColorBgra HandleNoStops (double position)
 	{
-		double valueSpan = _maxPosition - _minPosition;
-		double positionOffset = position - _minPosition;
+		double valueSpan = MaxPosition - MinPosition;
+		double positionOffset = position - MinPosition;
 		double fraction = positionOffset / valueSpan;
-		return ColorBgra.Lerp (_startColor, _endColor, fraction);
+		return ColorBgra.Lerp (StartColor, EndColor, fraction);
 	}
 
 	private ColorBgra HandleWithStops (double position)
 	{
-		int immediateLowerIndex = BinarySearchLowerOrEqual (_sortedStops, position);
-		var immediatelyLower = immediateLowerIndex < 0 ? KeyValuePair.Create (_minPosition, _startColor) : _sortedStops[immediateLowerIndex];
+		int immediateLowerIndex = BinarySearchLowerOrEqual (SortedStops, position);
+		var immediatelyLower = immediateLowerIndex < 0 ? KeyValuePair.Create (MinPosition, StartColor) : SortedStops[immediateLowerIndex];
 		if (immediatelyLower.Key == position) return immediatelyLower.Value;
 		int immediatelyHigherIndex = immediateLowerIndex + 1;
-		var immediatelyHigher = (immediatelyHigherIndex < _sortedStops.Count) ? _sortedStops[immediatelyHigherIndex] : KeyValuePair.Create (_maxPosition, _endColor);
+		var immediatelyHigher = (immediatelyHigherIndex < SortedStops.Length) ? SortedStops[immediatelyHigherIndex] : KeyValuePair.Create (MaxPosition, EndColor);
 		double valueSpan = immediatelyHigher.Key - immediatelyLower.Key;
 		double positionOffset = position - immediatelyLower.Key;
 		double fraction = positionOffset / valueSpan;
@@ -64,12 +66,12 @@ public sealed class ImmutableColorGradient
 
 	/// <returns>Index of number immediately lower than target. If not found, it returns -1</returns>
 	/// <param name="arr">Array assumed to be sorted by key</param>
-	public static int BinarySearchLowerOrEqual (IReadOnlyList<KeyValuePair<double, ColorBgra>> arr, double target)
+	public static int BinarySearchLowerOrEqual (ImmutableArray<KeyValuePair<double, ColorBgra>> arr, double target)
 	{
 		// TODO: Make this method more generic?
-		if (arr.Count == 0) return -1;
+		if (arr.Length == 0) return -1;
 		int left = 0;
-		int right = arr.Count - 1;
+		int right = arr.Length - 1;
 		while (left <= right) {
 			int mid = left + (right - left) / 2;
 			if (arr[mid].Key == target) {
