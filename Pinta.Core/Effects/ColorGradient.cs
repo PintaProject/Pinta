02a05bb10addbccc2aa734d6ee67b14d5eb82211
Lehonti Ramos <lehonti@ramos>
2023-10-18 17:57:21 +0200

    Made gradient stop type private

--- Pinta.Core/Effects/ColorGradient.cs
+++ Pinta.Core/Effects/ColorGradient.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 
 namespace Pinta.Core;
@@ -12,7 +11,7 @@ public sealed class ColorGradient : ColorMapping
 	public double MinimumPosition { get; }
 	public double MaximumPosition { get; }
 
-	private readonly ImmutableArray<GradientStop> sorted_stops; // Before making it public, make sure it's a nice structure
+	private readonly IReadOnlyList<GradientStop> sorted_stops; // TODO: Before making it public think of nice structures
 
 	internal ColorGradient (
 		ColorBgra startColor,
@@ -22,7 +21,7 @@ public sealed class ColorGradient : ColorMapping
 		IEnumerable<KeyValuePair<double, ColorBgra>> gradientStops)
 	{
 		if (minPosition >= maxPosition) throw new ArgumentException ($"{nameof (minPosition)} has to be lower than {nameof (maxPosition)}");
-		var sortedStops = gradientStops.Select (kvp => new GradientStop (kvp.Key, kvp.Value)).OrderBy (stop => stop.Position).ToImmutableArray ();
+		var sortedStops = gradientStops.Select (kvp => new GradientStop (kvp.Key, kvp.Value)).OrderBy (stop => stop.Position).ToArray ();
 		if (sortedStops.Length > 0 && sortedStops[0].Position <= minPosition) throw new ArgumentException ($"Lowest key in {nameof (gradientStops)} has to be greater than {nameof (minPosition)}");
 		if (sortedStops.Length > 0 && sortedStops[^1].Position >= maxPosition) throw new ArgumentException ($"Greatest key in {nameof (gradientStops)} has to be lower than {nameof (maxPosition)}");
 
@@ -38,7 +37,7 @@ public sealed class ColorGradient : ColorMapping
 		if (position == MinimumPosition) return StartColor;
 		if (position == MaximumPosition) return EndColor;
 		if (!IsMapped (position)) throw new ArgumentOutOfRangeException (nameof (position));
-		if (sorted_stops.Length == 0) return HandleNoStops (position);
+		if (sorted_stops.Count == 0) return HandleNoStops (position);
 		return HandleWithStops (position);
 	}
 
@@ -56,23 +55,23 @@ public sealed class ColorGradient : ColorMapping
 		var immediatelyLower = immediateLowerIndex < 0 ? new GradientStop (MinimumPosition, StartColor) : sorted_stops[immediateLowerIndex];
 		if (immediatelyLower.Position == position) return immediatelyLower.Color;
 		int immediatelyHigherIndex = immediateLowerIndex + 1;
-		var immediatelyHigher = (immediatelyHigherIndex < sorted_stops.Length) ? sorted_stops[immediatelyHigherIndex] : new GradientStop (MaximumPosition, EndColor);
+		var immediatelyHigher = (immediatelyHigherIndex < sorted_stops.Count) ? sorted_stops[immediatelyHigherIndex] : new GradientStop (MaximumPosition, EndColor);
 		double valueSpan = immediatelyHigher.Position - immediatelyLower.Position;
 		double positionOffset = position - immediatelyLower.Position;
 		double fraction = positionOffset / valueSpan;
 		return ColorBgra.Lerp (immediatelyLower.Color, immediatelyHigher.Color, fraction);
 	}
 
-	public readonly record struct GradientStop (double Position, ColorBgra Color);
+	private readonly record struct GradientStop (double Position, ColorBgra Color);
 
 	/// <returns>Index of number immediately lower than target. If not found, it returns -1</returns>
 	/// <param name="arr">Array assumed to be sorted by key</param>
-	private static int BinarySearchLowerOrEqual (ImmutableArray<GradientStop> arr, double target)
+	private static int BinarySearchLowerOrEqual (IReadOnlyList<GradientStop> arr, double target)
 	{
 		// TODO: Make this method more generic?
-		if (arr.Length == 0) return -1;
+		if (arr.Count == 0) return -1;
 		int left = 0;
-		int right = arr.Length - 1;
+		int right = arr.Count - 1;
 		while (left <= right) {
 			int mid = left + (right - left) / 2;
 			if (arr[mid].Position == target) {
