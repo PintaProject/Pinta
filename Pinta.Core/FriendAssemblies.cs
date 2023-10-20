ff8bac31cd6358782d7b77291fe76c448f815307
Lehonti Ramos <lehonti@ramos>
2023-10-20 15:53:34 +0200

    `Pinta.Core` internals visible to other assemblies

---
+++ Pinta.Core/FriendAssemblies.cs
@@ -0,0 +1,14 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo ("Pinta")]
+
+[assembly: InternalsVisibleTo ("Pinta.Core.Tests")]
+[assembly: InternalsVisibleTo ("Pinta.Effects.Tests")]
+[assembly: InternalsVisibleTo ("PintaBenchmarks")]
+
+[assembly: InternalsVisibleTo ("Pinta.Docking")]
+[assembly: InternalsVisibleTo ("Pinta.Effects")]
+[assembly: InternalsVisibleTo ("Pinta.Gui.Addins")]
+[assembly: InternalsVisibleTo ("Pinta.Gui.Widgets")]
+[assembly: InternalsVisibleTo ("Pinta.Resources")]
+[assembly: InternalsVisibleTo ("Pinta.Tools")]
