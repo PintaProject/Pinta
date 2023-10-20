42d228f34b7db9b5c9ff093b3b2e70fde36c15fa
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2023-10-21 00:18:19 +0200

    Update FriendAssemblies.cs

--- Pinta.Core/FriendAssemblies.cs
+++ Pinta.Core/FriendAssemblies.cs
@@ -1,7 +1,5 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo ("Pinta")]
-
 [assembly: InternalsVisibleTo ("Pinta.Core.Tests")]
 [assembly: InternalsVisibleTo ("Pinta.Effects.Tests")]
 [assembly: InternalsVisibleTo ("PintaBenchmarks")]
