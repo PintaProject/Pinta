1e58738ef4ff92bd5db183b7cb0c0d67efc44416
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2023-10-21 00:29:08 +0200

    `Pinta.Core` internals visible to other assemblies (#582)
    * `Pinta.Core` internals visible to other assemblies
    
    * Less assemblies
    
    * Update FriendAssemblies.cs
    
    ---------
    
    Co-authored-by: Lehonti Ramos <lehonti@ramos>

---
+++ Pinta.Core/FriendAssemblies.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo ("Pinta.Core.Tests")]
+[assembly: InternalsVisibleTo ("Pinta.Effects.Tests")]
+[assembly: InternalsVisibleTo ("PintaBenchmarks")]
