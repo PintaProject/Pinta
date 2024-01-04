262305a497e5dbbb98e18ad9f546a0a469552348
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2024-01-04 22:57:23 +0100

    Extracted methods and settings creation in `ZoomBlurEffect` (#656)

--- Pinta.Effects/Effects/ZoomBlurEffect.cs
+++ Pinta.Effects/Effects/ZoomBlurEffect.cs
@@ -39,81 +39,102 @@ public sealed class ZoomBlurEffect : BaseEffect
 	}
 
 	#region Algorithm Code Ported From PDN
-	public override void Render (ImageSurface src, ImageSurface dst, ReadOnlySpan<RectangleI> rois)
-	{
-		if (Data.Amount == 0) {
-			// Copy src to dest
-			return;
-		}
 
-		int src_width = src.Width;
-		ReadOnlySpan<ColorBgra> src_data = src.GetReadOnlyPixelData ();
-		int dst_width = dst.Width;
-		Span<ColorBgra> dst_data = dst.GetPixelData ();
-		Core.RectangleI src_bounds = src.GetBounds ();
+	private sealed record ZoomBlurSettings (
+		int src_width,
+		int dst_width,
+		long fcx,
+		long fcy,
+		long fz);
+
+	private ZoomBlurSettings CreateSettings (ImageSurface src, ImageSurface dst)
+	{
+		PointI offset = Data.Offset;
 
 		long w = dst.Width;
 		long h = dst.Height;
-		long fox = (long) (dst.Width * Data.Offset.X * 32768.0);
-		long foy = (long) (dst.Height * Data.Offset.Y * 32768.0);
-		long fcx = fox + (w << 15);
-		long fcy = foy + (h << 15);
-		long fz = Data.Amount;
+		long fox = (long) (w * offset.X * 32768.0);
+		long foy = (long) (h * offset.Y * 32768.0);
 
-		const int n = 64;
+		return new (
+			src_width: src.Width,
+			dst_width: dst.Width,
+			fcx: fox + (w << 15),
+			fcy: foy + (h << 15),
+			fz: Data.Amount
+		);
+	}
+
+	public override void Render (ImageSurface src, ImageSurface dst, ReadOnlySpan<RectangleI> rois)
+	{
+		if (Data.Amount == 0)
+			return; // Copy src to dest
+
+		ZoomBlurSettings settings = CreateSettings (src, dst);
+
+		ReadOnlySpan<ColorBgra> src_data = src.GetReadOnlyPixelData ();
+		Span<ColorBgra> dst_data = dst.GetPixelData ();
+		RectangleI src_bounds = src.GetBounds ();
 
 		foreach (var rect in rois) {
 			for (int y = rect.Top; y <= rect.Bottom; ++y) {
-				var src_row = src_data.Slice (y * src_width, src_width);
-				var dst_row = dst_data.Slice (y * dst_width, dst_width);
-
+				var src_row = src_data.Slice (y * settings.src_width, settings.src_width);
+				var dst_row = dst_data.Slice (y * settings.dst_width, settings.dst_width);
 				for (int x = rect.Left; x <= rect.Right; ++x) {
-					long fx = (x << 16) - fcx;
-					long fy = (y << 16) - fcy;
-
-					int sr = 0;
-					int sg = 0;
-					int sb = 0;
-					int sa = 0;
-					int sc = 0;
-
-					ColorBgra src_pixel = src_row[x];
-					sr += src_pixel.R * src_pixel.A;
-					sg += src_pixel.G * src_pixel.A;
-					sb += src_pixel.B * src_pixel.A;
-					sa += src_pixel.A;
-					++sc;
-
-					for (int i = 0; i < n; ++i) {
-						fx -= ((fx >> 4) * fz) >> 10;
-						fy -= ((fy >> 4) * fz) >> 10;
-
-						int u = (int) (fx + fcx + 32768 >> 16);
-						int v = (int) (fy + fcy + 32768 >> 16);
-
-						if (src_bounds.Contains (u, v)) {
-							ColorBgra src_pixel_2 = src.GetColorBgra (src_data, src_width, new (u, v));
-
-							sr += src_pixel_2.R * src_pixel_2.A;
-							sg += src_pixel_2.G * src_pixel_2.A;
-							sb += src_pixel_2.B * src_pixel_2.A;
-							sa += src_pixel_2.A;
-							++sc;
-						}
-					}
-
-					if (sa != 0) {
-						dst_row[x] = ColorBgra.FromBgra (
-							b: Utility.ClampToByte (sb / sa),
-							g: Utility.ClampToByte (sg / sa),
-							r: Utility.ClampToByte (sr / sa),
-							a: Utility.ClampToByte (sa / sc)
-						);
-					} else {
-						dst_row[x].Bgra = 0;
-					}
+					dst_row[x] = GetFinalPixelColor (src, settings, src_data, src_bounds, y, src_row, x);
 				}
 			}
+		}
+	}
+
+	private static ColorBgra GetFinalPixelColor (ImageSurface src, ZoomBlurSettings settings, ReadOnlySpan<ColorBgra> src_data, RectangleI src_bounds, int y, ReadOnlySpan<ColorBgra> src_row, int x)
+	{
+		const int n = 64;
+
+		long fx = (x << 16) - settings.fcx;
+		long fy = (y << 16) - settings.fcy;
+
+		int sr = 0;
+		int sg = 0;
+		int sb = 0;
+		int sa = 0;
+		int sc = 0;
+
+		ColorBgra src_pixel = src_row[x];
+		sr += src_pixel.R * src_pixel.A;
+		sg += src_pixel.G * src_pixel.A;
+		sb += src_pixel.B * src_pixel.A;
+		sa += src_pixel.A;
+		++sc;
+
+		for (int i = 0; i < n; ++i) {
+
+			fx -= ((fx >> 4) * settings.fz) >> 10;
+			fy -= ((fy >> 4) * settings.fz) >> 10;
+
+			int u = (int) (fx + settings.fcx + 32768 >> 16);
+			int v = (int) (fy + settings.fcy + 32768 >> 16);
+
+			if (src_bounds.Contains (u, v)) {
+				ColorBgra src_pixel_2 = src.GetColorBgra (src_data, settings.src_width, new (u, v));
+
+				sr += src_pixel_2.R * src_pixel_2.A;
+				sg += src_pixel_2.G * src_pixel_2.A;
+				sb += src_pixel_2.B * src_pixel_2.A;
+				sa += src_pixel_2.A;
+				++sc;
+			}
+		}
+
+		if (sa != 0) {
+			return ColorBgra.FromBgra (
+				b: Utility.ClampToByte (sb / sa),
+				g: Utility.ClampToByte (sg / sa),
+				r: Utility.ClampToByte (sr / sa),
+				a: Utility.ClampToByte (sa / sc)
+			);
+		} else {
+			return ColorBgra.FromUInt32 (0);
 		}
 	}
 	#endregion
@@ -124,7 +145,7 @@ public sealed class ZoomBlurEffect : BaseEffect
 		public int Amount { get; set; } = 10;
 
 		[Caption ("Offset")]
-		public Core.PointI Offset { get; set; } = new (0, 0);
+		public PointI Offset { get; set; } = new (0, 0);
 
 		[Skip]
 		public override bool IsDefault => Amount == 0;
