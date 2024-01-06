85964f4387038a69c94dde9a699adc5ffdcd2371
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2024-01-06 22:20:11 +0100

    Removed unused namespaces

--- Pinta.Gui.Widgets/Dialogs/SimpleEffectDialog.cs
+++ Pinta.Gui.Widgets/Dialogs/SimpleEffectDialog.cs
@@ -30,7 +30,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
