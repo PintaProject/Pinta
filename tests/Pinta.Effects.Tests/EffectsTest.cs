f3aa34bfcb71ba82022eb10cfae80d62749014c8
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2024-01-13 18:45:30 +0100

    Removed image for test

--- tests/Pinta.Effects.Tests/EffectsTest.cs
+++ tests/Pinta.Effects.Tests/EffectsTest.cs
@@ -48,12 +48,10 @@ internal sealed class EffectsTest
 	}
 
 	[Test]
+	[Ignore ("Produces non-deterministic results because the random seed is not fixed")]
 	public void Clouds1 ()
 	{
-		var effect = new CloudsEffect ();
-		effect.Data.ColorSchemeSource = ColorSchemeSource.PredefinedGradient;
-		effect.Data.ColorScheme = PredefinedGradients.BlackAndWhite;
-		Utilities.TestEffect (effect, "clouds1.png");
+		// TODO:
 	}
 
 	[Test]
