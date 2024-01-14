f0b9f96429ee380e65c19f80d1e99c94b36248f3
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2024-01-14 13:41:53 +0100

    Initial implementation

---
+++ Pinta.Effects/Effects/VoronoiDiagramEffect.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.Linq;
+using Cairo;
+using Pinta.Core;
+using Pinta.Gui.Widgets;
+
+namespace Pinta.Effects;
+
+public sealed class VoronoiDiagramEffect : BaseEffect
+{
+	// TODO: Icon
+
+	public override bool IsTileable => false;
+
+	public override string Name => Translations.GetString ("Voronoi Diagram");
+
+	public override bool IsConfigurable => true;
+
+	public override string EffectMenuCategory => Translations.GetString ("Render");
+
+	public VoronoiDiagramData Data => (VoronoiDiagramData) EffectData!; // NRT - Set in constructor
+
+	public VoronoiDiagramEffect ()
+	{
+		EffectData = new VoronoiDiagramData ();
+	}
+
+	public override void LaunchConfiguration ()
+	{
+		EffectHelper.LaunchSimpleEffectDialog (this);
+	}
+
+	protected override void Render (ImageSurface src, ImageSurface dst, RectangleI roi)
+	{
+		int w = dst.Width;
+		int h = dst.Height;
+
+		ImmutableArray<PointI> points =
+			CreatePoints (roi, Data.PointCount, Data.PointLocationsSeed)
+			.OrderBy (p => p.X)
+			.ToImmutableArray ();
+
+		ImmutableArray<ColorBgra> colors = CreateColors (points.Length, Data.ColorsSeed).ToImmutableArray ();
+
+		Func<PointI, PointI, double> distanceCalculator = GetDistanceCalculator (Data.DistanceCalculationMethod);
+
+		Span<ColorBgra> dst_data = dst.GetPixelData ();
+
+		for (int y = roi.Top; y <= roi.Bottom; y++) {
+			var dst_row = dst_data.Slice (y * w, w);
+			for (int x = roi.Left; x <= roi.Right; x++) {
+				PointI pixelLocation = new (x, y);
+				double shortestDistance = double.MaxValue;
+				int closestIndex = 0;
+				for (var i = 0; i < points.Length; i++) {
+					var point = points[i];
+					double distance = distanceCalculator (point, pixelLocation);
+					if (distance > shortestDistance) continue;
+					closestIndex = i;
+				}
+				dst_row[x] = colors[closestIndex];
+			}
+		}
+	}
+
+	private static Func<PointI, PointI, double> GetDistanceCalculator (DistanceCalculationMethod distanceCalculationMethod)
+	{
+		return distanceCalculationMethod switch {
+			DistanceCalculationMethod.Euclidean => Euclidean,
+			DistanceCalculationMethod.Manhattan => Manhattan,
+			_ => throw new InvalidEnumArgumentException (nameof (distanceCalculationMethod), (int) distanceCalculationMethod, typeof (DistanceCalculationMethod)),
+		};
+
+		static double Euclidean (PointI targetPoint, PointI pixelLocation)
+		{
+			PointI difference = pixelLocation - targetPoint;
+			return difference.Magnitude ();
+		}
+
+		static double Manhattan (PointI targetPoint, PointI pixelLocation)
+		{
+			PointI difference = pixelLocation - targetPoint;
+			return Math.Abs (difference.X) + Math.Abs (difference.Y);
+		}
+	}
+
+	private static HashSet<PointI> CreatePoints (RectangleI roi, int pointCount, RandomSeed pointLocationsSeed)
+	{
+		int effectivePointCount = Math.Min (pointCount, roi.Width * roi.Height);
+
+		Random randomPositioner = new (pointLocationsSeed.Value);
+		HashSet<PointI> result = new (); // Ensures points' uniqueness
+
+		while (result.Count < effectivePointCount) {
+
+			PointI point = new (
+				X: randomPositioner.Next (roi.Left, roi.Right + 1),
+				Y: randomPositioner.Next (roi.Top, roi.Bottom + 1)
+			);
+
+			result.Add (point);
+		}
+
+		return result;
+	}
+
+	private static HashSet<ColorBgra> CreateColors (int colorCount, RandomSeed colorsSeed)
+	{
+		Random randomColorizer = new (colorsSeed.Value);
+		HashSet<ColorBgra> result = new (); // Ensures points' uniqueness
+
+		while (result.Count < colorCount)
+			result.Add (randomColorizer.RandomColorBgra ());
+
+		return result;
+	}
+
+	public sealed class VoronoiDiagramData : EffectData
+	{
+		[Caption ("Distance Calculation Method")]
+		public DistanceCalculationMethod DistanceCalculationMethod { get; set; } = DistanceCalculationMethod.Euclidean;
+
+		[Caption ("Point Count"), MinimumValue (1), MaximumValue (65_535)]
+		public int PointCount { get; set; } = 100;
+
+		[Caption ("Colors Seed")]
+		public RandomSeed ColorsSeed { get; set; } = new (0);
+
+		[Caption ("Point Locations Seed")]
+		public RandomSeed PointLocationsSeed { get; set; } = new (0);
+	}
+
+	public enum DistanceCalculationMethod
+	{
+		Euclidean,
+		Manhattan,
+	}
+}
