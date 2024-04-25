bc36e51f2927ef52b2f3bfbb2d8131e774dd6ada
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2024-04-25 22:18:02 +0200

    Extracted reflection code into its own class

---
+++ Pinta.Gui.Widgets/Dialogs/MemberReflector.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+
+namespace Pinta.Gui.Widgets;
+
+internal sealed class MemberReflector
+{
+	public MemberInfo OriginalMemberInfo { get; }
+	public Type MemberType { get; }
+	public ImmutableArray<Attribute> Attributes { get; }
+
+	private readonly Func<object, object?> getter;
+	private readonly Action<object, object> setter;
+
+	public MemberReflector (MemberInfo memberInfo)
+	{
+		ImmutableArray<Attribute> attributes =
+			memberInfo
+			.GetCustomAttributes<Attribute> (false)
+			.ToImmutableArray ();
+
+		string? caption =
+			attributes
+			.OfType<CaptionAttribute> ()
+			.Select (h => h.Caption)
+			.FirstOrDefault ();
+
+		OriginalMemberInfo = memberInfo;
+		MemberType = GetTypeForMember (memberInfo);
+		Attributes = attributes;
+
+		getter = CreateGetter (memberInfo);
+		setter = CreateSetter (memberInfo);
+	}
+
+	public object? GetValue (object o)
+		=> getter (o);
+
+	public void SetValue (object o, object val)
+		=> setter (o, val);
+
+	private static Action<object, object> CreateSetter (MemberInfo memberInfo)
+	{
+		switch (memberInfo) {
+			case FieldInfo f:
+				return f.SetValue;
+			case PropertyInfo p:
+				MethodInfo setter = p.GetSetMethod () ?? throw new ArgumentException ("Property has no 'set' method", nameof (memberInfo));
+				return (o, v) => setter.Invoke (o, new[] { v });
+			default:
+				throw new ArgumentException ($"Member type {memberInfo.GetType ()} not supported", nameof (memberInfo));
+		}
+	}
+
+	private static Func<object, object?> CreateGetter (MemberInfo memberInfo)
+	{
+		switch (memberInfo) {
+			case FieldInfo f:
+				return f.GetValue;
+			case PropertyInfo p:
+				MethodInfo getter = p.GetGetMethod () ?? throw new ArgumentException ("Property has no 'get' method", nameof (memberInfo));
+				return o => getter.Invoke (o, Array.Empty<object> ());
+			default:
+				throw new ArgumentException ($"Member type {memberInfo.GetType ()} not supported", nameof (memberInfo));
+		}
+	}
+
+	private static Type GetTypeForMember (MemberInfo mi) =>
+		mi switch {
+			FieldInfo fi => fi.FieldType,
+			PropertyInfo pi => pi.PropertyType,
+			_ => throw new ArgumentException ("Invalid member type", nameof (mi)),
+		};
+}
--- Pinta.Gui.Widgets/Dialogs/SimpleEffectDialog.cs
+++ Pinta.Gui.Widgets/Dialogs/SimpleEffectDialog.cs
@@ -111,301 +111,40 @@ public sealed class SimpleEffectDialog : Gtk.Dialog
 
 	#region EffectData Parser
 
-	private sealed record MemberSettings (
-		MemberInfo mi,
-		Type mType,
-		string caption,
-		string? hint,
-		bool skip,
-		bool combo,
-		ImmutableArray<Attribute> attrs);
-
-	private static MemberSettings CreateMemberSettings (MemberInfo mi)
-	{
-		ImmutableArray<Attribute> attrs =
-			mi
-			.GetCustomAttributes<Attribute> (false)
-			.ToImmutableArray ();
-
-		string? caption = attrs.OfType<CaptionAttribute> ().Select (h => h.Caption).FirstOrDefault ();
-
-		return new (
-			mi: mi,
-			mType: GetTypeForMember (mi),
-			caption: caption ?? MakeCaption (mi.Name),
-			hint: attrs.OfType<HintAttribute> ().Select (h => h.Hint).FirstOrDefault (),
-			skip: attrs.OfType<SkipAttribute> ().Any (),
-			combo: attrs.OfType<StaticListAttribute> ().Any (),
-			attrs: attrs);
-	}
-
 	private IEnumerable<Gtk.Widget> GenerateDialogWidgets (EffectData effectData, IAddinLocalizer localizer) =>
 			effectData
 			.GetType ()
 			.GetMembers ()
 			.Where (m => m is FieldInfo || m is PropertyInfo)
-			.Select (CreateMemberSettings)
-			.Where (settings => !settings.skip && string.Compare (settings.mi.Name, nameof (EffectData.IsDefault), true) != 0)
+			.Select (CreateSettings)
+			.Where (settings => !settings.skip && string.Compare (settings.reflector.OriginalMemberInfo.Name, nameof (EffectData.IsDefault), true) != 0)
 			.Select (settings => GetMemberWidgets (settings, effectData, localizer))
 			.SelectMany (widgets => widgets);
 
-	private IEnumerable<Gtk.Widget> GetMemberWidgets (MemberSettings settings, EffectData effectData, IAddinLocalizer localizer)
+	private sealed record MemberSettings (
+		MemberReflector reflector,
+		string caption,
+		string? hint,
+		bool skip,
+		bool isComboBox);
+
+	private static MemberSettings CreateSettings (MemberInfo memberInfo)
 	{
-		var widgetFactory = GetWidgetFactory (settings);
+		MemberReflector reflector = new (memberInfo);
 
-		if (widgetFactory is not null)
-			yield return widgetFactory (localizer.GetString (settings.caption), effectData, settings);
+		string? caption =
+			reflector.Attributes
+			.OfType<CaptionAttribute> ()
+			.Select (h => h.Caption)
+			.FirstOrDefault ();
 
-		if (settings.hint != null)
-			yield return CreateHintLabel (localizer.GetString (settings.hint));
-	}
-
-	private WidgetFactory? GetWidgetFactory (MemberSettings settings)
-	{
-		if (settings.mType == typeof (RandomSeed))
-			return CreateSeed;
-		else if (settings.mType == typeof (int))
-			return CreateSlider;
-		else if (settings.mType == typeof (DegreesAngle))
-			return CreateAnglePicker;
-		else if (settings.mType == typeof (double))
-			return CreateDoubleSlider;
-		else if (settings.combo && settings.mType == typeof (string))
-			return CreateComboBox;
-		else if (settings.mType == typeof (bool))
-			return CreateCheckBox;
-		else if (settings.mType == typeof (PointI))
-			return CreatePointPicker;
-		else if (settings.mType == typeof (PointD))
-			return CreateOffsetPicker;
-		else if (settings.mType.IsEnum)
-			return CreateEnumComboBox;
-		else
-			return null;
-	}
-
-	private delegate Gtk.Widget WidgetFactory (string caption, EffectData effectData, MemberSettings settings);
-
-	#endregion
-
-	#region Control Builders
-	private ComboBoxWidget CreateEnumComboBox (string caption, EffectData effectData, MemberSettings settings)
-	{
-		var myType = GetTypeForMember (settings.mi)!; // NRT - We're looping through members we got from reflection
-
-		var member_names = Enum.GetNames (myType);
-
-		var mapping_items =
-			from member_name in member_names
-			let members = myType.GetMember (member_name)
-			let attrs = members[0].GetCustomAttributes<CaptionAttribute> (false).Take (1).ToArray ()
-			let label = attrs.Length > 0 ? attrs[0].Caption : member_name
-			let translatedLabel = Translations.GetString (label)
-			select KeyValuePair.Create (translatedLabel, member_name);
-
-		Dictionary<string, string> label_to_member = new (mapping_items);
-		List<string> labels = new ();
-		foreach (var kvp in mapping_items) {
-			label_to_member[kvp.Key] = kvp.Value;
-			labels.Add (kvp.Key);
-		}
-
-		ComboBoxWidget widget = new (labels) { Label = caption };
-
-		if (GetValue (settings.mi, effectData) is object obj)
-			widget.Active = Array.IndexOf (member_names, obj.ToString ());
-
-		widget.Changed += (_, _) => SetValue (settings.mi, effectData, Enum.Parse (myType, label_to_member[widget.ActiveText]));
-
-		return widget;
-	}
-
-	private ComboBoxWidget CreateComboBox (string caption, EffectData effectData, MemberSettings settings)
-	{
-		Dictionary<string, object>? dict = null;
-
-		foreach (var attr in settings.attrs)
-			if (attr is StaticListAttribute attribute && GetValue (attribute.DictionaryName, effectData) is Dictionary<string, object> d)
-				dict = d;
-
-		var entries = dict == null ? ImmutableArray<string>.Empty : dict.Keys.ToImmutableArray ();
-
-		var widget = new ComboBoxWidget (entries) { Label = caption };
-
-		if (GetValue (settings.mi, effectData) is string s)
-			widget.Active = entries.IndexOf (s);
-
-		widget.Changed += (_, _) => SetValue (settings.mi, effectData, widget.ActiveText);
-
-		return widget;
-	}
-
-	private HScaleSpinButtonWidget CreateDoubleSlider (string caption, EffectData effectData, MemberSettings settings)
-	{
-		var widget = new HScaleSpinButtonWidget {
-			Label = caption,
-			MinimumValue = settings.attrs.OfType<MinimumValueAttribute> ().Select (m => m.Value).FirstOrDefault (-100),
-			MaximumValue = settings.attrs.OfType<MaximumValueAttribute> ().Select (m => m.Value).FirstOrDefault (100),
-			IncrementValue = settings.attrs.OfType<IncrementValueAttribute> ().Select (i => i.Value).FirstOrDefault (0.01),
-			DigitsValue = settings.attrs.OfType<DigitsValueAttribute> ().Select (d => d.Value).FirstOrDefault (2),
-		};
-
-		if (GetValue (settings.mi, effectData) is double d)
-			widget.DefaultValue = d;
-
-		widget.ValueChanged += (_, _) => {
-			DelayedUpdate (() => {
-				SetValue (settings.mi, effectData, widget.Value);
-				return false;
-			});
-		};
-
-		return widget;
-	}
-
-	private HScaleSpinButtonWidget CreateSlider (string caption, EffectData effectData, MemberSettings settings)
-	{
-		var widget = new HScaleSpinButtonWidget {
-			Label = caption,
-			MinimumValue = settings.attrs.OfType<MinimumValueAttribute> ().Select (m => m.Value).FirstOrDefault (-100),
-			MaximumValue = settings.attrs.OfType<MaximumValueAttribute> ().Select (m => m.Value).FirstOrDefault (100),
-			IncrementValue = settings.attrs.OfType<IncrementValueAttribute> ().Select (i => i.Value).FirstOrDefault (1.0),
-			DigitsValue = settings.attrs.OfType<DigitsValueAttribute> ().Select (d => d.Value).FirstOrDefault (0),
-		};
-
-		if (GetValue (settings.mi, effectData) is int i)
-			widget.DefaultValue = i;
-
-		widget.ValueChanged += (_, _) => {
-			DelayedUpdate (() => {
-				SetValue (settings.mi, effectData, widget.ValueAsInt);
-				return false;
-			});
-		};
-
-		return widget;
-	}
-
-	private Gtk.CheckButton CreateCheckBox (string caption, EffectData effectData, MemberSettings settings)
-	{
-		var widget = new Gtk.CheckButton { Label = caption };
-
-		if (GetValue (settings.mi, effectData) is bool b)
-			widget.Active = b;
-
-		widget.OnToggled += (_, _) => SetValue (settings.mi, effectData, widget.Active);
-
-		return widget;
-	}
-
-	private PointPickerWidget CreateOffsetPicker (string caption, EffectData effectData, MemberSettings settings)
-	{
-		var widget = new PointPickerWidget { Label = caption };
-
-		if (GetValue (settings.mi, effectData) is PointD p)
-			widget.DefaultOffset = p;
-
-		widget.PointPicked += (_, _) => SetValue (settings.mi, effectData, widget.Offset);
-
-		return widget;
-	}
-
-	private PointPickerWidget CreatePointPicker (string caption, EffectData effectData, MemberSettings settings)
-	{
-		var widget = new PointPickerWidget { Label = caption };
-
-		if (GetValue (settings.mi, effectData) is PointI p)
-			widget.DefaultPoint = p;
-
-		widget.PointPicked += (_, _) => SetValue (settings.mi, effectData, widget.Point);
-
-		return widget;
-	}
-
-	private AnglePickerWidget CreateAnglePicker (string caption, EffectData effectData, MemberSettings settings)
-	{
-		var widget = new AnglePickerWidget { Label = caption };
-
-		if (GetValue (settings.mi, effectData) is DegreesAngle d)
-			widget.DefaultValue = d;
-
-		widget.ValueChanged += (_, _) => {
-			DelayedUpdate (() => {
-				SetValue (settings.mi, effectData, widget.Value);
-				return false;
-			});
-		};
-
-		return widget;
-	}
-
-	private static Gtk.Label CreateHintLabel (string hint)
-	{
-		var label = Gtk.Label.New (hint);
-		label.Wrap = true;
-		label.Halign = Gtk.Align.Start;
-		label.MaxWidthChars = 40;
-		return label;
-	}
-
-	private ReseedButtonWidget CreateSeed (string caption, EffectData effectData, MemberSettings settings)
-	{
-		int min_value = settings.attrs.OfType<MinimumValueAttribute> ().Select (m => m.Value).FirstOrDefault (0);
-		int max_value = settings.attrs.OfType<MaximumValueAttribute> ().Select (m => m.Value).FirstOrDefault (int.MaxValue - 1);
-
-		ReseedButtonWidget widget = new () { Label = caption };
-		Random random = new ();
-		widget.Clicked += (_, _) => SetValue (
-			settings.mi,
-			effectData,
-			new RandomSeed (
-				random.Next (
-					min_value,
-					max_value
-				)
-			)
+		return new MemberSettings (
+			reflector: reflector,
+			caption: caption ?? MakeCaption (memberInfo.Name),
+			hint: reflector.Attributes.OfType<HintAttribute> ().Select (h => h.Hint).FirstOrDefault (),
+			skip: reflector.Attributes.OfType<SkipAttribute> ().Any (),
+			isComboBox: reflector.Attributes.OfType<StaticListAttribute> ().Any ()
 		);
-		return widget;
-	}
-
-	#endregion
-
-	#region Static Reflection Methods
-
-	private static object? GetValue (MemberInfo mi, object o)
-		=> mi switch {
-			FieldInfo fi => fi.GetValue (o),
-			PropertyInfo pi => pi.GetGetMethod ()?.Invoke (o, Array.Empty<object> ()),
-			_ => null,
-		};
-
-	private void SetValue (MemberInfo mi, object o, object val)
-	{
-		string fieldName;
-
-		switch (mi) {
-			case FieldInfo fi:
-				fi.SetValue (o, val);
-				fieldName = fi.Name;
-				break;
-			case PropertyInfo pi:
-				pi.GetSetMethod ()?.Invoke (o, new[] { val });
-				fieldName = pi.Name;
-				break;
-			default:
-				throw new ArgumentException ("Invalid member type", nameof (mi));
-		}
-
-		EffectDataChanged?.Invoke (this, new PropertyChangedEventArgs (fieldName));
-	}
-
-	private static Type GetTypeForMember (MemberInfo mi)
-	{
-		return mi switch {
-			FieldInfo fi => fi.FieldType,
-			PropertyInfo pi => pi.PropertyType,
-			_ => throw new ArgumentException ("Invalid member type", nameof (mi)),
-		};
 	}
 
 	private static string MakeCaption (string name)
@@ -438,6 +177,235 @@ public sealed class SimpleEffectDialog : Gtk.Dialog
 		}
 	}
 
+	private IEnumerable<Gtk.Widget> GetMemberWidgets (MemberSettings settings, EffectData effectData, IAddinLocalizer localizer)
+	{
+		var widgetFactory = GetWidgetFactory (settings);
+
+		if (widgetFactory is not null)
+			yield return widgetFactory (localizer.GetString (settings.caption), effectData, settings);
+
+		if (settings.hint != null)
+			yield return CreateHintLabel (localizer.GetString (settings.hint));
+	}
+
+	private WidgetFactory? GetWidgetFactory (MemberSettings settings)
+	{
+		Type memberType = settings.reflector.MemberType;
+		if (memberType == typeof (RandomSeed))
+			return CreateSeed;
+		else if (memberType == typeof (int))
+			return CreateSlider;
+		else if (memberType == typeof (DegreesAngle))
+			return CreateAnglePicker;
+		else if (memberType == typeof (double))
+			return CreateDoubleSlider;
+		else if (settings.isComboBox && memberType == typeof (string))
+			return CreateComboBox;
+		else if (memberType == typeof (bool))
+			return CreateCheckBox;
+		else if (memberType == typeof (PointI))
+			return CreatePointPicker;
+		else if (memberType == typeof (PointD))
+			return CreateOffsetPicker;
+		else if (memberType.IsEnum)
+			return CreateEnumComboBox;
+		else
+			return null;
+	}
+
+	private delegate Gtk.Widget WidgetFactory (string caption, EffectData effectData, MemberSettings settings);
+
+	#endregion
+
+	#region Control Builders
+	private ComboBoxWidget CreateEnumComboBox (string caption, EffectData effectData, MemberSettings settings)
+	{
+		var member_names = Enum.GetNames (settings.reflector.MemberType);
+
+		var mapping_items =
+			from member_name in member_names
+			let members = settings.reflector.MemberType.GetMember (member_name)
+			let attrs = members[0].GetCustomAttributes<CaptionAttribute> (false).Take (1).ToArray ()
+			let label = attrs.Length > 0 ? attrs[0].Caption : member_name
+			let translatedLabel = Translations.GetString (label)
+			select KeyValuePair.Create (translatedLabel, member_name);
+
+		Dictionary<string, string> label_to_member = new (mapping_items);
+		List<string> labels = new ();
+		foreach (var kvp in mapping_items) {
+			label_to_member[kvp.Key] = kvp.Value;
+			labels.Add (kvp.Key);
+		}
+
+		ComboBoxWidget widget = new (labels) { Label = caption };
+
+		if (settings.reflector.GetValue (effectData) is object obj)
+			widget.Active = Array.IndexOf (member_names, obj.ToString ());
+
+		widget.Changed += (_, _) => SetAndNotify (settings.reflector, effectData, Enum.Parse (settings.reflector.MemberType, label_to_member[widget.ActiveText]));
+
+		return widget;
+	}
+
+	private ComboBoxWidget CreateComboBox (string caption, EffectData effectData, MemberSettings settings)
+	{
+		Dictionary<string, object>? dict = null;
+
+		foreach (var attr in settings.reflector.Attributes)
+			if (attr is StaticListAttribute attribute && GetValue (attribute.DictionaryName, effectData) is Dictionary<string, object> d)
+				dict = d;
+
+		var entries = dict == null ? ImmutableArray<string>.Empty : dict.Keys.ToImmutableArray ();
+
+		var widget = new ComboBoxWidget (entries) { Label = caption };
+
+		if (settings.reflector.GetValue (effectData) is string s)
+			widget.Active = entries.IndexOf (s);
+
+		widget.Changed += (_, _) => SetAndNotify (settings.reflector, effectData, widget.ActiveText);
+
+		return widget;
+	}
+
+	private HScaleSpinButtonWidget CreateDoubleSlider (string caption, EffectData effectData, MemberSettings settings)
+	{
+		var attributes = settings.reflector.Attributes;
+
+		var widget = new HScaleSpinButtonWidget {
+			Label = caption,
+			MinimumValue = attributes.OfType<MinimumValueAttribute> ().Select (m => m.Value).FirstOrDefault (-100),
+			MaximumValue = attributes.OfType<MaximumValueAttribute> ().Select (m => m.Value).FirstOrDefault (100),
+			IncrementValue = attributes.OfType<IncrementValueAttribute> ().Select (i => i.Value).FirstOrDefault (0.01),
+			DigitsValue = attributes.OfType<DigitsValueAttribute> ().Select (d => d.Value).FirstOrDefault (2),
+		};
+
+		if (settings.reflector.GetValue (effectData) is double d)
+			widget.DefaultValue = d;
+
+		widget.ValueChanged += (_, _) => {
+			DelayedUpdate (() => {
+				SetAndNotify (settings.reflector, effectData, widget.Value);
+				return false;
+			});
+		};
+
+		return widget;
+	}
+
+	private HScaleSpinButtonWidget CreateSlider (string caption, EffectData effectData, MemberSettings settings)
+	{
+		var attributes = settings.reflector.Attributes;
+
+		var widget = new HScaleSpinButtonWidget {
+			Label = caption,
+			MinimumValue = attributes.OfType<MinimumValueAttribute> ().Select (m => m.Value).FirstOrDefault (-100),
+			MaximumValue = attributes.OfType<MaximumValueAttribute> ().Select (m => m.Value).FirstOrDefault (100),
+			IncrementValue = attributes.OfType<IncrementValueAttribute> ().Select (i => i.Value).FirstOrDefault (1.0),
+			DigitsValue = attributes.OfType<DigitsValueAttribute> ().Select (d => d.Value).FirstOrDefault (0),
+		};
+
+		if (settings.reflector.GetValue (effectData) is int i)
+			widget.DefaultValue = i;
+
+		widget.ValueChanged += (_, _) => {
+			DelayedUpdate (() => {
+				SetAndNotify (settings.reflector, effectData, widget.ValueAsInt);
+				return false;
+			});
+		};
+
+		return widget;
+	}
+
+	private Gtk.CheckButton CreateCheckBox (string caption, EffectData effectData, MemberSettings settings)
+	{
+		var widget = new Gtk.CheckButton { Label = caption };
+
+		if (settings.reflector.GetValue (effectData) is bool b)
+			widget.Active = b;
+
+		widget.OnToggled += (_, _) => SetAndNotify (settings.reflector, effectData, widget.Active);
+
+		return widget;
+	}
+
+	private PointPickerWidget CreateOffsetPicker (string caption, EffectData effectData, MemberSettings settings)
+	{
+		var widget = new PointPickerWidget { Label = caption };
+
+		if (settings.reflector.GetValue (effectData) is PointD p)
+			widget.DefaultOffset = p;
+
+		widget.PointPicked += (_, _) => SetAndNotify (settings.reflector, effectData, widget.Offset);
+
+		return widget;
+	}
+
+	private PointPickerWidget CreatePointPicker (string caption, EffectData effectData, MemberSettings settings)
+	{
+		var widget = new PointPickerWidget { Label = caption };
+
+		if (settings.reflector.GetValue (effectData) is PointI p)
+			widget.DefaultPoint = p;
+
+		widget.PointPicked += (_, _) => SetAndNotify (settings.reflector, effectData, widget.Point);
+
+		return widget;
+	}
+
+	private AnglePickerWidget CreateAnglePicker (string caption, EffectData effectData, MemberSettings settings)
+	{
+		var widget = new AnglePickerWidget { Label = caption };
+
+		if (settings.reflector.GetValue (effectData) is DegreesAngle d)
+			widget.DefaultValue = d;
+
+		widget.ValueChanged += (_, _) => {
+			DelayedUpdate (() => {
+				SetAndNotify (settings.reflector, effectData, widget.Value);
+				return false;
+			});
+		};
+
+		return widget;
+	}
+
+	private static Gtk.Label CreateHintLabel (string hint)
+	{
+		var label = Gtk.Label.New (hint);
+		label.Wrap = true;
+		label.Halign = Gtk.Align.Start;
+		label.MaxWidthChars = 40;
+		return label;
+	}
+
+	private void SetAndNotify (MemberReflector settings, object o, object val)
+	{
+		settings.SetValue (o, val);
+		EffectDataChanged?.Invoke (this, new PropertyChangedEventArgs (settings.OriginalMemberInfo.Name));
+	}
+
+	private ReseedButtonWidget CreateSeed (string caption, EffectData effectData, MemberSettings settings)
+	{
+		var attributes = settings.reflector.Attributes;
+
+		int min_value = attributes.OfType<MinimumValueAttribute> ().Select (m => m.Value).FirstOrDefault (0);
+		int max_value = attributes.OfType<MaximumValueAttribute> ().Select (m => m.Value).FirstOrDefault (int.MaxValue - 1);
+
+		ReseedButtonWidget widget = new () { Label = caption };
+		Random random = new ();
+		widget.Clicked += (_, _) => {
+			int seedValue = random.Next (min_value, max_value);
+			RandomSeed seed = new (seedValue);
+			SetAndNotify (settings.reflector, effectData, seed);
+		};
+		return widget;
+	}
+
+	#endregion
+
+	#region Static Reflection Methods
+
 	private static object? GetValue (string name, object o)
 	{
 		if (o.GetType ().GetField (name) is FieldInfo fi)
@@ -448,6 +416,8 @@ public sealed class SimpleEffectDialog : Gtk.Dialog
 
 		return null;
 	}
+
+	#endregion
 
 	private void DelayedUpdate (TimeoutHandler handler)
 	{
@@ -468,7 +438,7 @@ public sealed class SimpleEffectDialog : Gtk.Dialog
 			}
 		);
 	}
-	#endregion
+	
 }
 
 /// <summary>
