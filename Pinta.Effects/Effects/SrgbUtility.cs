16df2bf7df52c1c4e1f288272d078d1602f76260
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2024-06-19 02:24:44 +0200

    Yet another license header updated

--- Pinta.Effects/Effects/SrgbUtility.cs
+++ Pinta.Effects/Effects/SrgbUtility.cs
@@ -2,7 +2,7 @@
 // Paint.NET                                                                   //
 // Copyright (C) dotPDN LLC, Rick Brewster, Tom Jackson, and contributors.     //
 // Portions Copyright (C) Microsoft Corporation. All Rights Reserved.          //
-// See src/Resources/Files/License.txt for full licensing and attribution      //
+// See license-pdn.txt for full licensing and attribution                      //
 // details.                                                                    //
 // .                                                                           //
 /////////////////////////////////////////////////////////////////////////////////
