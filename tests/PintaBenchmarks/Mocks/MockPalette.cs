3a00a09ca2fa447aef287773b8c6a18dc3e916ec
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2024-06-30 17:08:05 +0200

    Suppressed warnings

--- tests/PintaBenchmarks/Mocks/MockPalette.cs
+++ tests/PintaBenchmarks/Mocks/MockPalette.cs
@@ -8,8 +8,10 @@ internal sealed class MockPalette : IPaletteService
 	public Color PrimaryColor { get; set; } = new (0, 0, 0); // Black
 	public Color SecondaryColor { get; set; } = new (1, 1, 1); // White
 
+#pragma warning disable CS0067 // The event 'MockPalette.PrimaryColorChanged' is never used
 	public event EventHandler? PrimaryColorChanged;
 	public event EventHandler? SecondaryColorChanged;
+#pragma warning restore CS0067 // The event 'MockPalette.PrimaryColorChanged' is never used
 
 	public void SetColor (bool setPrimary, Color color, bool addToRecent = true)
 	{
