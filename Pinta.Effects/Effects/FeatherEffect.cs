47eca2b2fa4bda14a12f2cfe7ebe4f3ab71f5531
potatoes <48143760+potatoes1286@users.noreply.github.com>
2024-08-28 13:30:24 -0400

    Improve feathering

--- Pinta.Effects/Effects/FeatherEffect.cs
+++ Pinta.Effects/Effects/FeatherEffect.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading;
 using Cairo;
 using Pinta.Core;
@@ -43,35 +42,30 @@ public sealed class FeatherEffect : BaseEffect
 		int src_height = src.Height;
 		var dst_data = dest.GetPixelData ();
 
-		// reset dest to src
-		// Removing this causes preview to not update to lower radius levels
-		var src_row = src_data.Slice (y * src_width, src_width);
-		var dst_row = dst_data.Slice (y * src_width, src_width);
-		for (int x = roi.Left; x <= roi.Right; x++) {
-			dst_row[x].Bgra = src_row[x].Bgra;
-		}
-
 
 		// Collect a list of pixels that surround the object
 		List<PointI> borderPixels = new List<PointI> ();
 		for (int x = roi.Left; x <= roi.Right; x++) {
 			PointI potentialBorderPixel = new (x, y);
-			if (src.GetColorBgra (src_data, src_width, potentialBorderPixel).A <= Data.Tolerance) {
-				// Test pixel above, below, left, & right
-				PointI[] testPixels = [
-					new PointI(x - 1, y),
-					new PointI(x + 1, y),
-					new PointI(x, y - 1),
-					new PointI(x, y + 1)
-				];
 
-				foreach (var pixel in testPixels) {
+			if (Data.FeatherCanvasEdge && (x == 0 || x == src_width - 1 || y == 0 || y == src_height - 1)) {
+				borderPixels.Add (potentialBorderPixel);
+			} else if (src.GetColorBgra (src_data, src_width, potentialBorderPixel).A <= Data.Tolerance) {
+				// Test pixel above, below, left, & right
+
+				foreach (var pixel in new PointI[] { new(x - 1, y), new(x + 1, y), new(x, y - 1), new(x, y + 1) }) {
 					var px = pixel.X;
 					var py = pixel.Y;
 					if (px < 0 || px >= src_width || py < 0 || py >= src_height)
 						continue;
 					if (src.GetColorBgra (src_data, src_width, pixel).A > Data.Tolerance) {
 						borderPixels.Add (potentialBorderPixel);
+						// Remove comments below to draw border pixels
+						// You will also have to comment out the border pixel pass because it will overwrite this
+						//int pos = src_width * y + x;
+						//dst_data[pos].Bgra = 0;
+						//dst_data[pos].A = 255;
+
 						break;
 					}
 				}
@@ -80,6 +74,7 @@ public sealed class FeatherEffect : BaseEffect
 
 
 		// Pass through all border pixels and reduce the alpha of pixels around it
+
 		foreach (var borderPixel in borderPixels) {
 			var top = Math.Max (borderPixel.Y - radius, roi.Top);
 			var bottom = Math.Min (borderPixel.Y + radius, roi.Bottom);
@@ -111,13 +106,29 @@ public sealed class FeatherEffect : BaseEffect
 			int bottom = roi.Bottom;
 			int currentRow = top;
 
+			var src_data = src.GetReadOnlyPixelData ();
+			int src_width = src.Width;
+			var dst_data = dest.GetPixelData ();
+
+			// reset dest to src
+			// Removing this causes preview to not update to lower radius levels
+			// this is on the main thread to prevent race conditions
+			for (int y = roi.Top; y <= roi.Bottom; y++) {
+				var src_row = src_data.Slice (y * src_width, src_width);
+				var dst_row = dst_data.Slice (y * src_width, src_width);
+				for (int x = roi.Left; x <= roi.Right; x++) {
+					dst_row[x].Bgra = src_row[x].Bgra;
+				}
+			}
+
 			int threadCount = PintaCore.System.RenderThreads;
-			var slaves = new Thread[threadCount - 1];
-			for (int threadId = 1; threadId < threadCount; threadId++) {
+			var slaves = new Thread[threadCount];
+			for (int threadId = 0; threadId < threadCount; threadId++) {
 				var slave = new Thread (() => {
 					while (true) {
-						int rowIndex = Interlocked.Increment (ref currentRow);
-						if (rowIndex >= bottom)
+						int rowIndex = Interlocked.Increment (ref currentRow) - 1;
+						// Not sure why but bottom is 1 less than it should be? Effect doesn't hit bottom-most pixel without + 1
+						if (rowIndex >= bottom + 1)
 							return;
 
 						RenderAsync(src, dest, roi, rowIndex);
@@ -125,7 +136,7 @@ public sealed class FeatherEffect : BaseEffect
 				}) { Priority = ThreadPriority.BelowNormal };
 				slave.Start ();
 
-				slaves[threadId - 1] = slave;
+				slaves[threadId] = slave;
 			}
 
 			foreach (var slave in slaves)
@@ -140,5 +151,8 @@ public sealed class FeatherEffect : BaseEffect
 
 		[Caption ("Tolerance"), MinimumValue (0), MaximumValue (255)]
 		public int Tolerance { get; set; } = 20;
+
+		[Caption ("Feather Canvas Edge")]
+		public bool FeatherCanvasEdge { get; set; } = false;
 	}
 }
