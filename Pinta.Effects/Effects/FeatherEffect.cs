b96d3605db728a41978861854e1e50d9e017b5bf
potatoes1286 <48143760+potatoes1286@users.noreply.github.com>
2024-08-30 23:42:52 -0400

    Rewrite feathereffect

--- Pinta.Effects/Effects/FeatherEffect.cs
+++ Pinta.Effects/Effects/FeatherEffect.cs
@@ -1,6 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Threading;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
 using Cairo;
 using Pinta.Core;
 using Pinta.Gui.Widgets;
@@ -24,130 +25,105 @@ public sealed class FeatherEffect : BaseEffect
 	public FeatherData Data => (FeatherData) EffectData!;  // NRT - Set in constructor
 
 	private readonly IChromeService chrome;
+	private readonly ISystemService system;
 
 	public FeatherEffect (IServiceProvider services)
 	{
 		chrome = services.GetService<IChromeService> ();
+		system = services.GetService<ISystemService> ();
 		EffectData = new FeatherData ();
 	}
 
 	public override void LaunchConfiguration ()
 		=> chrome.LaunchSimpleEffectDialog (this);
 
-	public void RenderAsync (ImageSurface src, ImageSurface dest, RectangleI roi, int y)
+	protected override void Render (ImageSurface src, ImageSurface dest, RectangleI roi)
 	{
-		int radius = Data.Radius;
-		var src_data = src.GetReadOnlyPixelData ();
-		int src_width = src.Width;
+		int bottom = roi.Bottom;
 		int src_height = src.Height;
-		var dst_data = dest.GetPixelData ();
+		int radius = Data.Radius;
+		int threads = system.RenderThreads;
 
-
-		// Collect a list of pixels that surround the object
-		List<PointI> borderPixels = new List<PointI> ();
-		for (int x = roi.Left; x <= roi.Right; x++) {
-			PointI potentialBorderPixel = new (x, y);
-
-			if (Data.FeatherCanvasEdge && (x == 0 || x == src_width - 1 || y == 0 || y == src_height - 1)) {
-				borderPixels.Add (potentialBorderPixel);
-			} else if (src.GetColorBgra (src_data, src_width, potentialBorderPixel).A <= Data.Tolerance) {
-				// Test pixel above, below, left, & right
-
-				foreach (var pixel in new PointI[] { new (x - 1, y), new (x + 1, y), new (x, y - 1), new (x, y + 1) }) {
-					var px = pixel.X;
-					var py = pixel.Y;
-					if (px < 0 || px >= src_width || py < 0 || py >= src_height)
-						continue;
-					if (src.GetColorBgra (src_data, src_width, pixel).A > Data.Tolerance) {
-						borderPixels.Add (potentialBorderPixel);
-						// Remove comments below to draw border pixels
-						// You will also have to comment out the border pixel pass because it will overwrite this
-						//int pos = src_width * y + x;
-						//dst_data[pos].Bgra = 0;
-						//dst_data[pos].A = 255;
-
-						break;
-					}
-				}
-			}
-		}
-
-
-		// Pass through all border pixels and reduce the alpha of pixels around it
-
-		foreach (var borderPixel in borderPixels) {
-			var top = Math.Max (borderPixel.Y - radius, roi.Top);
-			var bottom = Math.Min (borderPixel.Y + radius, roi.Bottom);
-			var left = Math.Max (borderPixel.X - radius, roi.Left);
-			var right = Math.Min (borderPixel.X + radius, roi.Right);
-
-			for (int py = top; py <= bottom; py++) {
-				for (int px = left; px <= right; px++) {
-					var dx = borderPixel.X - px;
-					var dy = borderPixel.Y - py;
-					float distance = MathF.Sqrt (dx * dx + dy * dy);
-					// If within actual distance
-					if (distance <= radius) {
-						int pixel_index = py * src_width + px;
-						float mult = distance / radius;
-						byte alpha = (byte) (src_data[pixel_index].A * mult);
-						if (alpha < dst_data[pixel_index].A)
-							dst_data[pixel_index].Bgra = src_data[pixel_index].ToStraightAlpha ().NewAlpha (alpha).ToPremultipliedAlpha ().Bgra;
-					}
-				}
-			}
-		}
-	}
-
-	public override void Render (ImageSurface src, ImageSurface dest, ReadOnlySpan<RectangleI> rois)
-	{
-		foreach (var roi in rois) {
-			int top = roi.Top;
-			int bottom = roi.Bottom;
-			int currentRow = top;
-
+		ConcurrentBag<PointI> borderPixels = new ConcurrentBag<PointI> ();
+		// Color in any pixel that the stencil says we need to fill
+		// First pass
+		// Clean up dest, then collect all border pixels
+		Parallel.For (0, bottom, new ParallelOptions { MaxDegreeOfParallelism = threads }, y => {
 			var src_data = src.GetReadOnlyPixelData ();
 			int src_width = src.Width;
 			var dst_data = dest.GetPixelData ();
 
 			// reset dest to src
 			// Removing this causes preview to not update to lower radius levels
-			// this is on the main thread to prevent race conditions
-			for (int y = roi.Top; y <= roi.Bottom; y++) {
-				var src_row = src_data.Slice (y * src_width, src_width);
-				var dst_row = dst_data.Slice (y * src_width, src_width);
-				for (int x = roi.Left; x <= roi.Right; x++) {
-					dst_row[x].Bgra = src_row[x].Bgra;
+			var src_row = src_data.Slice (y * src_width, src_width);
+			var dst_row = dst_data.Slice (y * src_width, src_width);
+			for (int x = roi.Left; x <= roi.Right; x++) {
+				dst_row[x].Bgra = src_row[x].Bgra;
+			}
+
+			Span<PointI> pixels = [new (0, 0), new (0, 0), new (0, 0), new (0, 0)];
+			// Collect a list of pixels that surround the object (border pixels)
+			for (int x = roi.Left; x <= roi.Right; x++) {
+				PointI potentialBorderPixel = new (x, y);
+				if (Data.FeatherCanvasEdge && (x == 0 || x == src_width - 1 || y == 0 || y == src_height - 1)) {
+					borderPixels.Add (potentialBorderPixel);
+				} else if (src.GetColorBgra (src_data, src_width, potentialBorderPixel).A <= Data.Tolerance) {
+					// Test pixel above, below, left, & right
+					pixels[0] = new (x - 1, y);
+					pixels[1] = new (x + 1, y);
+					pixels[2] = new (x, y - 1);
+					pixels[3] = new (x, y + 1);
+
+					foreach (var pixel in pixels) {
+						var px = pixel.X;
+						var py = pixel.Y;
+						if (px < 0 || px >= src_width || py < 0 || py >= src_height)
+							continue;
+						if (src.GetColorBgra (src_data, src_width, new PointI (px, py)).A > Data.Tolerance) {
+							borderPixels.Add (potentialBorderPixel);
+							// Remove comments below to draw border pixels
+							// You will also have to comment out the feather pass because it will overwrite this
+							//int pos = src_width * y + x;
+							//dst_data[pos].Bgra = 0;
+							//dst_data[pos].A = 255;
+
+							break;
+						}
+					}
 				}
 			}
+		});
 
-			// crashes on test if try-catch not implemented
-			int threadCount = 0;
-			try {
-				threadCount = PintaCore.System.RenderThreads;
-			} catch {
-				threadCount = 1;
-			}
-			var slaves = new Thread[threadCount];
-			for (int threadId = 0; threadId < threadCount; threadId++) {
-				var slave = new Thread (() => {
-					while (true) {
-						int rowIndex = Interlocked.Increment (ref currentRow) - 1;
-						// Not sure why but bottom is 1 less than it should be? Effect doesn't hit bottom-most pixel without + 1
-						if (rowIndex >= bottom + 1)
-							return;
+		// Second pass
+		// Feather pixels according to distance to border pixels
+		Parallel.For (0, bottom, new ParallelOptions { MaxDegreeOfParallelism = threads }, py => {
+			var src_data = src.GetReadOnlyPixelData ();
+			int src_width = src.Width;
+			var dst_data = dest.GetPixelData ();
 
-						RenderAsync (src, dest, roi, rowIndex);
+			var relevantBorderPixels = borderPixels.Where (borderPixel => borderPixel.Y > py - radius && borderPixel.Y < py + radius).ToArray ();
+
+			for (int px = roi.Left; px <= roi.Right; px++) {
+				int pixel_index = py * src_width + px;
+				byte lowestAlpha = dst_data[pixel_index].A;
+				foreach (var borderPixel in relevantBorderPixels) {
+					if (borderPixel.X > px - radius && borderPixel.X < px + radius) {
+						var dx = borderPixel.X - px;
+						var dy = borderPixel.Y - py;
+						float distance = MathF.Sqrt (dx * dx + dy * dy);
+						// If within distance to border pixel
+						if (distance <= radius) {
+							float mult = distance / radius;
+							byte alpha = (byte) (src_data[pixel_index].A * mult);
+							if (alpha < lowestAlpha)
+								lowestAlpha = alpha;
+						}
 					}
-				}) { Priority = ThreadPriority.BelowNormal };
-				slave.Start ();
-
-				slaves[threadId] = slave;
+				}
+				if (lowestAlpha < dst_data[pixel_index].A)
+					dst_data[pixel_index].Bgra = src_data[pixel_index].ToStraightAlpha ().NewAlpha (lowestAlpha).ToPremultipliedAlpha ().Bgra;
 			}
-
-			foreach (var slave in slaves)
-				slave.Join ();
-		}
+		});
 	}
 
 	public sealed class FeatherData : EffectData
