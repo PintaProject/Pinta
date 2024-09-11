bc8914bd90ba71cb823f5c36485b669313053037
Theodor Larsson <thela038@student.liu.se>
2024-09-11 12:04:39 +0200

    RedrawText if state is NotFinalized

--- Pinta.Tools/Tools/TextTool.cs
+++ Pinta.Tools/Tools/TextTool.cs
@@ -355,8 +355,8 @@ public sealed class TextTool : BaseTool
 
 	private void HandlePintaCorePalettePrimaryColorChanged (object? sender, EventArgs e)
 	{
-		if (is_editing)
-			RedrawText (true, true);
+		if (is_editing || (workspace.HasOpenDocuments && CurrentTextEngine.State == TextMode.NotFinalized))
+			RedrawText (is_editing, true);
 	}
 
 	private void HandleLeftAlignmentButtonToggled (object? sender, EventArgs e)
@@ -432,9 +432,8 @@ public sealed class TextTool : BaseTool
 
 			CurrentTextEngine.SetFont (font, Alignment, underscore_btn.Active);
 		}
-
-		if (is_editing)
-			RedrawText (true, true);
+		if (is_editing || (workspace.HasOpenDocuments && CurrentTextEngine.State == TextMode.NotFinalized))
+			RedrawText (is_editing, true);
 	}
 
 	private int OutlineWidth
