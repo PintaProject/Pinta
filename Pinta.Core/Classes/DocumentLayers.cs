ab51989ec6cc3d792dd6b735fc0cbfe9aa037e57
Filip Johnsson <johnsson.filip@protonmail.com>
2024-09-17 08:47:20 +0200

    Fix formating

--- Pinta.Core/Classes/DocumentLayers.cs
+++ Pinta.Core/Classes/DocumentLayers.cs
@@ -251,7 +251,7 @@ public sealed class DocumentLayers
 		var surf = CairoExtensions.CreateImageSurface (Format.Argb32, document.ImageSize.Width, document.ImageSize.Height);
 
 		var g = new Context (surf);
-		document.Selection.Clip(g);
+		document.Selection.Clip (g);
 
 		g.SetSourceSurface (user_layers[index].Surface, 0, 0);
 		g.Paint ();
