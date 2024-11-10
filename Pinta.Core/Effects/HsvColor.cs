f3878ba5e32720370028b2a49df2558af410402f
potatoes1286 <48143760+potatoes1286@users.noreply.github.com>
2024-11-09 20:01:39 -0500

    Streamline HsvColor

--- Pinta.Core/Effects/HsvColor.cs
+++ Pinta.Core/Effects/HsvColor.cs
@@ -7,7 +7,6 @@
 
 using System;
 using Cairo;
-using Pinta.Core.Extensions;
 
 namespace Pinta.Core;
 
@@ -21,17 +20,6 @@ public readonly struct HsvColor
 	public double Hue { get; init; } // 0-360
 	public double Sat { get; init; } // 0-1
 	public double Val { get; init; } // 0-1
-
-	public static bool operator == (HsvColor c1, HsvColor c2)
-	{
-		if (Math.Abs (c1.Hue - c2.Hue) < 0.1 &&
-		    Math.Abs (c1.Sat - c2.Sat) < 0.001 &&
-		    Math.Abs (c1.Val - c2.Val) < 0.001)
-			return true;
-		return false;
-	}
-
-	public static bool operator != (HsvColor c1, HsvColor c2) => !(c1 == c2);
 
 	public HsvColor (double hue, double sat, double val)
 	{
@@ -50,18 +38,10 @@ public readonly struct HsvColor
 	}
 
 	public static HsvColor FromColor (Color c) => c.ToHsv ();
-	public Color ToColor (double alpha = 1) => Color.FromHsv (this);
+	public Color ToColor (double alpha = 1) => Color.FromHsv (this, alpha);
+	public static HsvColor FromBgra (ColorBgra c) => c.ToCairoColor ().ToHsv ();
+	public ColorBgra ToBgra () => this.ToColor ().ToColorBgra ();
 
-	public static HsvColor FromRgbD (double r, double g, double b) => new Color (r, g, b).ToHsv ();
-
-	public static HsvColor FromRgbI (int r, int g, int b) => new Color (r / 255.0, g / 255.0, b / 255.0).ToHsv ();
-
-	public static HsvColor FromBgra (ColorBgra c) => new Color (c.R / 255.0, c.G / 255.0, c.B / 255.0).ToHsv ();
-	public ColorBgra ToBgra ()
-	{
-		var c = this.ToColor ();
-		return ColorBgra.FromBgr ((byte) (c.B * 255.0), (byte) (c.G * 255.0), (byte) (c.R * 255.0));
-	}
 
 	public override readonly string ToString ()
 		=> $"({Hue:F2}, {Sat:F2}, {Val:F2})";
