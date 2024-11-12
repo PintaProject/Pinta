c1ce64eba8b91bf370ba54fecd857b6ed90ea069
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2024-11-12 17:20:59 +0100

    Disposing `Pixbuf` objects

--- tests/PintaBenchmarks/Utilities/TestData.cs
+++ tests/PintaBenchmarks/Utilities/TestData.cs
@@ -1,5 +1,6 @@
 using Cairo;
 using Pinta.Core;
+using GdkPixbuf;
 
 namespace PintaBenchmarks;
 
@@ -17,9 +18,9 @@ internal class TestData
 
 		using var fs = file.Read (null);
 		try {
-			var bg = GdkPixbuf.Pixbuf.NewFromStream (fs, cancellable: null)!; // NRT: only nullable when error is thrown.
-			var surf = CairoExtensions.CreateImageSurface (Format.Argb32, bg.Width, bg.Height);
-			using var context = new Cairo.Context (surf);
+			using Pixbuf bg = Pixbuf.NewFromStream (fs, cancellable: null)!; // NRT: only nullable when error is thrown.
+			ImageSurface surf = CairoExtensions.CreateImageSurface (Format.Argb32, bg.Width, bg.Height); // Not disposing because it will be returned
+			using Context context = new (surf);
 			context.DrawPixbuf (bg, 0, 0);
 			return surf;
 		} finally {
