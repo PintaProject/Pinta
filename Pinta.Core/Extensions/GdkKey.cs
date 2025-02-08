PintaProject/Pinta

commit a767c4aebe81ea92914364654123e182730f075f

Remove the Gdk.Key enum now that the constants from gir.core can be used
Gdk.Key is now just a wrapper type around the uint key value for better type safety

Pinta.Core/Extensions/ToolBoxButton.cs
@@ -47,7 +47,7 @@ public sealed class ToolBoxButton : ToggleButton
 		Show ();
 
 		string shortcutText = "";
-		if (tool.ShortcutKey != 0) {
+		if (tool.ShortcutKey != Gdk.Key.Invalid) {
 			var shortcutLabel = Translations.GetString ("Shortcut key");
 			shortcutText = $"{shortcutLabel}: {tool.ShortcutKey.ToString ().ToUpperInvariant ()}\n";
 		}

Pinta.Core/Classes/BaseTool.cs
@@ -118,10 +118,11 @@ public abstract class BaseTool
 		=> Enumerable.Empty<IToolHandle> ();
 
 	/// <summary>
-	/// The shortcut key used to activate this tool in the toolbox. Return 0 for no shortcut key.
+	/// The shortcut key used to activate this tool in the toolbox.
+	/// Return Gdk.Key.Invalid for no shortcut key.
 	/// </summary>
 	public virtual Gdk.Key ShortcutKey
-		=> 0;
+		=> Gdk.Key.Invalid;
 
 	/// <summary>
 	/// Affects the order of the tool in the toolbox. Lower numbers will appear first.

Pinta.Core/Extensions/GdkExtensions.cs
@@ -67,17 +67,6 @@ public static class GdkExtensions
 		=> m.HasFlag (Gdk.ModifierType.Button3Mask);
 
 	/// <summary>
-	/// Returns whether this key is a Ctrl key (or the Cmd key on macOS).
-	/// </summary>
-	public static bool IsControlKey (this Gdk.Key key)
-	{
-		if (PintaCore.System.OperatingSystem == OS.Mac)
-			return key == Gdk.Key.Meta_L || key == Gdk.Key.Meta_R;
-		else
-			return key == Gdk.Key.Control_L || key == Gdk.Key.Control_R;
-	}
-
-	/// <summary>
 	/// Returns whether any of the Ctrl/Cmd/Shift/Alt modifiers are active.
 	/// This prevents Caps Lock, Num Lock, etc from appearing as active modifier keys.
 	/// </summary>
@@ -183,14 +172,6 @@ public static class GdkExtensions
 		g.Paint ();
 
 		return Gdk.Texture.NewForPixbuf (i.ToPixbuf ());
-	}
-
-	public static Gdk.Key ToUpper (this Gdk.Key k1)
-	{
-		if (Enum.TryParse (k1.ToString ().ToUpperInvariant (), out Gdk.Key result))
-			return result;
-		else
-			return k1;
 	}
 
 	// TODO-GTK4 (bindings, unsubmitted) - need gir.core async bindings for Gdk.Clipboard

Pinta.Core/Extensions/Gtk/GtkExtensions.cs
@@ -25,8 +25,6 @@
 // THE SOFTWARE.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using GObject;
@@ -135,13 +133,13 @@ public static partial class GtkExtensions
 	/// Provides convenient access to the Gdk.Key of the key being pressed.
 	/// </summary>
 	public static Gdk.Key GetKey (this Gtk.EventControllerKey.KeyPressedSignalArgs args)
-		=> (Gdk.Key) args.Keyval;
+		=> new Gdk.Key (args.Keyval);
 
 	/// <summary>
 	/// Provides convenient access to the Gdk.Key of the key being released.
 	/// </summary>
 	public static Gdk.Key GetKey (this Gtk.EventControllerKey.KeyReleasedSignalArgs args)
-		=> (Gdk.Key) args.Keyval;
+		=> new Gdk.Key (args.Keyval);
 
 	internal sealed class TextWrapper : Gtk.Text
 	{

Pinta.Core/Managers/PaletteManager.cs
@@ -96,7 +96,7 @@ public sealed class PaletteManager : IPaletteService
 
 	public bool DoKeyPress (Gtk.EventControllerKey.KeyPressedSignalArgs args)
 	{
-		if (args.State.HasModifierKey () || args.GetKey ().ToUpper () != Gdk.Key.X)
+		if (args.State.HasModifierKey () || args.GetKey ().ToUpper ().Value != Gdk.Constants.KEY_X)
 			return false;
 
 		Color temp = PrimaryColor;

Pinta.Gui.Widgets/Widgets/ColorPickerDialog.cs
@@ -2,7 +2,6 @@ using System;
 using System.Globalization;
 using System.Linq;
 using Cairo;
-using Gdk;
 using Pinta.Core;
 using Color = Cairo.Color;
 using Context = Cairo.Context;
@@ -760,7 +759,7 @@ public sealed class ColorPickerDialog : Gtk.Dialog
 
 		var keyboard_gesture = Gtk.EventControllerKey.New ();
 		keyboard_gesture.OnKeyPressed += (_, e) => {
-			if (e.GetKey () == Key.x)
+			if (e.GetKey ().Value == Gdk.Constants.KEY_x)
 				CycleColors ();
 			return true;
 		};

Pinta.Tools/Editable/EditEngines/BaseEditEngine.cs
@@ -457,33 +457,33 @@ public abstract class BaseEditEngine
 	public virtual bool HandleKeyDown (Document document, ToolKeyEventArgs e)
 	{
 		Gdk.Key keyPressed = e.Key;
-		switch (keyPressed) {
-			case Gdk.Key.Delete:
+		switch (keyPressed.Value) {
+			case Gdk.Constants.KEY_Delete:
 				HandleDelete ();
 				return true;
-			case Gdk.Key.Return:
-			case Gdk.Key.KP_Enter:
+			case Gdk.Constants.KEY_Return:
+			case Gdk.Constants.KEY_KP_Enter:
 				FinalizeAllShapes ();
 				return true;
-			case Gdk.Key.space:
+			case Gdk.Constants.KEY_space:
 				HandleSpace (e);
 				return true;
-			case Gdk.Key.Up:
+			case Gdk.Constants.KEY_Up:
 				HandleUp ();
 				return true;
-			case Gdk.Key.Down:
+			case Gdk.Constants.KEY_Down:
 				HandleDown ();
 				return true;
-			case Gdk.Key.Left:
+			case Gdk.Constants.KEY_Left:
 				HandleLeft (e);
 				return true;
-			case Gdk.Key.Right:
+			case Gdk.Constants.KEY_Right:
 				HandleRight (e);
 				return true;
-			case Gdk.Key.bracketleft:
+			case Gdk.Constants.KEY_bracketleft:
 				BrushWidth--;
 				return true;
-			case Gdk.Key.bracketright:
+			case Gdk.Constants.KEY_bracketright:
 				BrushWidth++;
 				return true;
 			default:
@@ -703,15 +703,15 @@ public abstract class BaseEditEngine
 		if (keyReleased.IsControlKey ())
 			DrawActiveShape (false, false, true, e.IsShiftPressed, false, false);
 
-		switch (keyReleased) {
-			case Gdk.Key.Delete:
-			case Gdk.Key.Return:
-			case Gdk.Key.KP_Enter:
-			case Gdk.Key.space:
-			case Gdk.Key.Up:
-			case Gdk.Key.Down:
-			case Gdk.Key.Left:
-			case Gdk.Key.Right:
+		switch (keyReleased.Value) {
+			case Gdk.Constants.KEY_Delete:
+			case Gdk.Constants.KEY_Return:
+			case Gdk.Constants.KEY_KP_Enter:
+			case Gdk.Constants.KEY_space:
+			case Gdk.Constants.KEY_Up:
+			case Gdk.Constants.KEY_Down:
+			case Gdk.Constants.KEY_Left:
+			case Gdk.Constants.KEY_Right:
 				return true;
 			default:
 				return false;

commit 74936281fe7682f981a2a1b7ca84acb406e2757b

Fix regression from a767c4ae which broke the tooltip keyboard shortcuts
Changing Gdk.Key into a wrapper struct caused the old approach of using the enum names to fail. Using gdk_keyval_name() is more straightforward.

Fixes: #1257

Pinta.Core/Extensions/GdkKey.cs
@@ -9,6 +9,12 @@ public readonly record struct Key (uint Value)
 {
 	public static Key Invalid { get; } = new (Gdk.Constants.KEY_VoidSymbol);
 
+	/// <summary>
+	/// Returns the name of the key value, e.g. 'A' for 'KEY_A'
+	/// </summary>
+	public string Name ()
+		=> Gdk.Functions.KeyvalName (Value) ?? string.Empty;
+
 	public Key ToUpper ()
 		=> new (Gdk.Functions.KeyvalToUpper (Value));
 

Pinta.Core/Extensions/ToolBoxButton.cs
@@ -49,7 +49,7 @@ public sealed class ToolBoxButton : ToggleButton
 		string shortcutText = "";
 		if (tool.ShortcutKey != Gdk.Key.Invalid) {
 			var shortcutLabel = Translations.GetString ("Shortcut key");
-			shortcutText = $"{shortcutLabel}: {tool.ShortcutKey.ToString ().ToUpperInvariant ()}\n";
+			shortcutText = $"{shortcutLabel}: {tool.ShortcutKey.ToUpper ().Name ()}\n";
 		}
 
 		TooltipText = $"{tool.Name}\n{shortcutText}\n{tool.StatusBarText}";