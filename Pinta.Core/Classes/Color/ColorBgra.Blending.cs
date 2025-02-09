9bbf8714f5db0206082e239a7a06975b5848bcdf
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-02-09 09:30:02 +0100

    Added license headers

--- Pinta.Core/Classes/Color/ColorBgra.Blending.cs
+++ Pinta.Core/Classes/Color/ColorBgra.Blending.cs
@@ -1,3 +1,10 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Paint.NET                                                                   //
+// Copyright (C) Rick Brewster, Tom Jackson, and past contributors.            //
+// Portions Copyright (C) Microsoft Corporation. All Rights Reserved.          //
+// See license-pdn.txt for full licensing and attribution details.             //
+/////////////////////////////////////////////////////////////////////////////////
+
 using System;
 
 namespace Pinta.Core;
