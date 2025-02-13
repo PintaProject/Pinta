88f87b831e28f1638aa472b659ffecf2b4676b55
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-02-13 12:54:01 +0100

    a few lines cast like before

--- Pinta.Effects/Effects/TileEffect.cs
+++ Pinta.Effects/Effects/TileEffect.cs
@@ -71,8 +71,8 @@ public sealed class TileEffect : BaseEffect
 		float preliminaryIntensity = Data.Intensity;
 		int tileSize = Data.TileSize;
 		int antiAliasSample = ANTI_ALIAS_LEVEL * ANTI_ALIAS_LEVEL + 1;
-		float sin = MathF.Sin ((float) -rotationTheta.Radians);
-		float cos = MathF.Cos ((float) -rotationTheta.Radians);
+		float sin = (float) Math.Sin (-rotationTheta.Radians);
+		float cos = (float) Math.Cos (-rotationTheta.Radians);
 		return new (
 			size: size,
 			halfWidth: size.Width / 2f,
