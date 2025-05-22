7a85ec44d5e33aa2a5801b08cf1e4b24c55cb5dc
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-05-23 01:56:49 +0200

    Constant setting names were moved to their own classes in `Pinta` and `Pinta.Core` (#1481)

---
+++ Pinta.Core/Classes/SettingNames.cs
@@ -0,0 +1,19 @@
+namespace Pinta.Core;
+
+internal static class SettingNames
+{
+	internal const string CANVAS_GRID_WIDTH = "canvas-grid-width";
+	internal const string CANVAS_GRID_HEIGHT = "canvas-grid-height";
+
+	internal const string DEFAULT_IMAGE_TYPE = "default-image-type";
+
+	internal const string JPG_QUALITY = "jpg-quality";
+
+	internal const string SELECTION_COMBINE_MODE = "selection-combine-mode";
+
+	internal const string SHOW_CANVAS_GRID = "show-canvas-grid";
+
+	internal const string PRIMARY_COLOR = "primary-color";
+	internal const string SECONDARY_COLOR = "secondary-color";
+	internal const string RECENT_COLORS = "recently-used-colors";
+}
---
+++ Pinta/SettingNames.cs
@@ -0,0 +1,23 @@
+namespace Pinta;
+
+internal static class SettingNames
+{
+	internal const string NEW_IMAGE_WIDTH = "new-image-width";
+	internal const string NEW_IMAGE_HEIGHT = "new-image-height";
+	internal const string NEW_IMAGE_BACKGROUND = "new-image-bg";
+
+	internal const string RULER_METRIC = "ruler-metric";
+	internal const string COLOR_SCHEME = "color-scheme";
+	internal const string WINDOW_MAXIMIZED = "window-maximized";
+	internal const string WINDOW_SIZE_WIDTH = "window-size-width";
+	internal const string WINDOW_SIZE_HEIGHT = "window-size-height";
+	internal const string RULER_SHOWN = "ruler-shown";
+	internal const string IMAGE_TABS_SHOWN = "image-tabs-shown";
+	internal const string TOOL_WINDOWS_SHOWN = "tool-windows-shown";
+	internal const string TOOLBAR_SHOWN = "toolbar-shown";
+	internal const string MENUBAR_SHOWN = "menubar-shown";
+	internal const string STATUSBAR_SHOWN = "statusbar-shown";
+	internal const string TOOLBOX_SHOWN = "toolbox-shown";
+	internal const string LAST_DIALOG_DIRECTORY = "last-dialog-directory";
+	internal const string LAST_SELECTED_TOOL = "last-selected-tool";
+}
