d206f53559b00903734f17d1809dbd5699816cb9
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-05-24 03:39:59 +0200

    Refactorings in `Pinta.Docking`

--- Pinta.Docking/Dock.cs
+++ Pinta.Docking/Dock.cs
@@ -22,7 +22,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-using Gtk;
 using Pinta.Core;
 
 namespace Pinta.Docking;
@@ -30,20 +29,27 @@ namespace Pinta.Docking;
 /// <summary>
 /// The root widget, containing all dock items underneath it.
 /// </summary>
-public class Dock : Box
+public sealed class Dock : Gtk.Box
 {
-	private readonly Paned pane = Paned.New (Orientation.Horizontal);
+	private readonly Gtk.Paned pane;
 
-	public DockPanel RightPanel { get; private init; } = new ();
+	public DockPanel RightPanel { get; } = new ();
 
 	public Dock ()
 	{
-		SetOrientation (Orientation.Horizontal);
-
+		Gtk.Paned pane = Gtk.Paned.New (Gtk.Orientation.Horizontal);
 		pane.EndChild = RightPanel;
 		pane.ResizeEndChild = false;
 		pane.ShrinkEndChild = false;
+
+		// --- Initialization (Gtk.Box)
+
+		SetOrientation (Gtk.Orientation.Horizontal);
 		Append (pane);
+
+		// --- References to keep
+
+		this.pane = pane;
 	}
 
 	public void AddItem (DockItem item, DockPlacement placement)
@@ -60,12 +66,10 @@ public class Dock : Box
 		}
 	}
 
-	private const string RightSplitPosKey = "dock-right-splitpos";
-
 	public void SaveSettings (ISettingsService settings)
 	{
 #if false
-		settings.PutSetting (RightSplitPosKey, pane.Position);
+		settings.PutSetting (SettingNames.DOCK_RIGHT_SPLITPOS, pane.Position);
 #endif
 		RightPanel.SaveSettings (settings);
 	}
@@ -74,7 +78,7 @@ public class Dock : Box
 	{
 		// TODO-GTK3(docking) Disabled for now, as the size isn't quite restored properly (gradually increases over time)
 #if false
-		pane.Position = settings.GetSetting<int> (RightSplitPosKey, pane.Position);
+		pane.Position = settings.GetSetting<int> (SettingNames.DOCK_RIGHT_SPLITPOS, pane.Position);
 #endif
 		RightPanel.LoadSettings (settings);
 	}
