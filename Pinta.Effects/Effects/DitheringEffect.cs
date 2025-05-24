1ab0a433d56d4c2940e4d5f050b11072fe2b56df
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-05-24 04:20:29 +0200

    Revert "Changed name of `TotalWeight` and made it a factor, not a divider"
    This reverts commit fe93db36be128f5af2641b7cb2f12b0be7255cfe.

--- Pinta.Effects/Effects/DitheringEffect.cs
+++ Pinta.Effects/Effects/DitheringEffect.cs
@@ -90,7 +90,7 @@ public sealed class DitheringEffect : BaseEffect
 
 					int idx = (thisItem.Y * settings.sourceWidth) + thisItem.X;
 
-					double factor = weight * settings.diffusionMatrix.WeightReductionFactor;
+					double factor = ((double) weight) / settings.diffusionMatrix.TotalWeight;
 
 					dst_data[idx] = AddError (dst_data[idx], factor, errorRed, errorGreen, errorBlue);
 				}
