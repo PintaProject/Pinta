139deafa0b1977aa476e7c84f8237e4130659893
Elvis Alistar <elvis@unity3d.com>
2025-05-30 18:03:56 -0600

    Resize panel to full size on Maximize

--- Pinta.Docking/DockPanel.cs
+++ Pinta.Docking/DockPanel.cs
@@ -85,7 +85,7 @@ public sealed class DockPanel : Gtk.Box
 			popover.Child = null;
 
 			Pane.StartChild = Item;
-			Pane.ResizeStartChild = false;
+			Pane.ResizeStartChild = true;
 			Pane.ShrinkStartChild = false;
 		}
 
