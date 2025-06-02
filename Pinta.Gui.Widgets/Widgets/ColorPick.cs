7ab32bb24d71aa38e5f1e00938c00cda083ecbca
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-06-02 14:45:22 +0200

    Removed unused `enum`

--- Pinta.Gui.Widgets/Widgets/ColorPick.cs
+++ Pinta.Gui.Widgets/Widgets/ColorPick.cs
@@ -2,31 +2,18 @@ using Cairo;
 
 namespace Pinta.Gui.Widgets;
 
-public enum ColorPickType
-{
-	RegularSingle,
-	MainColors,
-}
-
 public abstract record ColorPick
 {
-	public abstract ColorPickType Type { get; }
 	protected ColorPick () { }
 }
 
 public sealed record SingleColor (Color Color)
 	: ColorPick
-{
-	public override ColorPickType Type
- 		=> ColorPickType.RegularSingle;
-}
+{ }
 
 public sealed record PaletteColors (Color Primary, Color Secondary)
 	: ColorPick
 {
-	public override ColorPickType Type
-		=> ColorPickType.MainColors;
-
 	public PaletteColors Swapped ()
 		=> new (Secondary, Primary);
 }
