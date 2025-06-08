179e517ef2ce16191f4373fffb296f66b677d5c2
Cameron White <cameronwhite91@gmail.com>
2025-06-07 23:45:25 -0400

    Update version number to 3.0.1

--- Pinta.Core/PintaCore.cs
+++ Pinta.Core/PintaCore.cs
@@ -45,7 +45,7 @@ public static class PintaCore
 	public static WorkspaceManager Workspace { get; }
 	public static CanvasGridManager CanvasGrid { get; }
 
-	public const string ApplicationVersion = "3.0";
+	public const string ApplicationVersion = "3.0.1";
 	/// <summary>
 	/// The oldest version of Pinta for which add-ins built against it will still
 	/// run in the current version.
