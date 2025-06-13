81ec23e0aad22988d0092dfbeaf02f4c12eeede0
Pedro Paulo <32345702+pedropaulosuzuki@users.noreply.github.com>
2025-06-13 21:45:18 +0000

    Adds axonometric grid to canvas

--- Pinta.Core/Classes/SettingNames.cs
+++ Pinta.Core/Classes/SettingNames.cs
@@ -2,9 +2,6 @@ namespace Pinta.Core;
 
 internal static class SettingNames
 {
-	internal const string CANVAS_GRID_WIDTH = "canvas-grid-width";
-	internal const string CANVAS_GRID_HEIGHT = "canvas-grid-height";
-
 	internal const string DEFAULT_IMAGE_TYPE = "default-image-type";
 
 	internal const string JPG_QUALITY = "jpg-quality";
@@ -12,6 +9,11 @@ internal static class SettingNames
 	internal const string SELECTION_COMBINE_MODE = "selection-combine-mode";
 
 	internal const string SHOW_CANVAS_GRID = "show-canvas-grid";
+	internal const string CANVAS_GRID_WIDTH = "canvas-grid-width";
+	internal const string CANVAS_GRID_HEIGHT = "canvas-grid-height";
+
+	internal const string SHOW_CANVAS_AXONOMETRIC_GRID = "show-canvas-axonometric-grid";
+	internal const string CANVAS_AXONOMETRIC_WIDTH = "canvas-axonometric-grid-width";
 
 	internal const string PRIMARY_COLOR = "primary-color";
 	internal const string SECONDARY_COLOR = "secondary-color";
