6616edd393e71ba3f184d50b3712cab0f3d8fa9b
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-06-30 15:08:18 +0200

    Changed `IColor` interface

--- Pinta.Core/Classes/IColor.cs
+++ Pinta.Core/Classes/IColor.cs
@@ -1,6 +1,26 @@
 namespace Pinta.Core;
 
-public interface IColor<TColor>
+public interface IColor<TColor> where TColor : IColor<TColor>
+{
+	static abstract TColor Black { get; }
+
+	static abstract TColor Red { get; }
+	static abstract TColor Green { get; }
+	static abstract TColor Blue { get; }
+
+	static abstract TColor Yellow { get; }
+	static abstract TColor Magenta { get; }
+	static abstract TColor Cyan { get; }
+
+	static abstract TColor White { get; }
+}
+
+public interface IAlphaColor<TColor> : IColor<TColor> where TColor : IAlphaColor<TColor>
+{
+	static abstract TColor Transparent { get; }
+}
+
+public interface IInterpolableColor<TColor> : IColor<TColor> where TColor : IInterpolableColor<TColor>
 {
 	static abstract TColor Lerp (in TColor from, in TColor to, double frac);
 }
