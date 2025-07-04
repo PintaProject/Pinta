72c40a45c84bafd225087b24a7041727dfc06104
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-07-04 08:14:43 +0200

    Changed `Matrix2D` to `Matrix3x2D`, and added the remaining components

--- Pinta.Core/Classes/Matrix3x2D.cs
+++ Pinta.Core/Classes/Matrix3x2D.cs
@@ -2,17 +2,19 @@ using System;
 
 namespace Pinta.Core;
 
-public readonly record struct Matrix2D (
+public readonly record struct Matrix3x2D (
 	double A11, double A12,
-	double A21, double A22)
+	double A21, double A22,
+	double A31, double A32)
 {
-	public static Matrix2D CreateRotation (RadiansAngle theta)
+	public static Matrix3x2D CreateRotation (RadiansAngle theta)
 	{
 		double radians = theta.Radians;
 		double sin = Math.Sin (radians);
 		double cos = Math.Cos (radians);
 		return new (
 			cos, -sin,
-			sin, cos);
+			sin, cos,
+			0, 0);
 	}
 }
