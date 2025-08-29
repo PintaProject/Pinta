PintaProject/Pinta

commit d897e39c66eb1a230b58ed810db792128c5317cc

Fix the data root directory for installed Windows builds (#1493)
This broke with #622 which changed the directory layout for the installer.

Bug: #1473

Pinta.Core/Managers/SystemManager.cs
@@ -97,7 +97,16 @@ public sealed class SystemManager : ISystemService
 			return Path.Combine (contents_dir!.FullName, "Resources", "share");
 		}
 
+		// On Windows, the installed executable is under Pinta/bin and data is under Pinta/share.
+		if (GetOperatingSystem () == OS.Windows) {
+			string parentDir = Path.Combine (app_dir, "..");
+			bool develMode = Path.Exists (Path.Combine (parentDir, "Pinta.sln"));
+			if (!develMode)
+				return Path.Combine (parentDir, "share");
+		}
+
 		// Otherwise, translations etc are contained under the executable's folder.
+		// (e.g. for local development builds)
 		return app_dir;
 	}
 

commit 00b7e298ca7d544b17bfd8bd733928c596c58cb5

Fix errors with loading icons in local development builds on Windows
This was a regression from d897e39

Pinta.Core/Managers/SystemManager.cs
@@ -100,7 +100,8 @@ public sealed class SystemManager : ISystemService
 		// On Windows, the installed executable is under Pinta/bin and data is under Pinta/share.
 		if (GetOperatingSystem () == OS.Windows) {
 			string parentDir = Path.Combine (app_dir, "..");
-			bool develMode = Path.Exists (Path.Combine (parentDir, "Pinta.sln"));
+			// For development builds, we have a path like Pinta/build/bin
+			bool develMode = Path.Exists (Path.Combine (parentDir, "..", "Pinta.sln"));
 			if (!develMode)
 				return Path.Combine (parentDir, "share");
 		}