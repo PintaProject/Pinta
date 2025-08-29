354946795db826f231d26887ca5b5ed9e644d30b
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-08-29 19:42:11 +0200

    Removed license header

--- Pinta.Core/Effects/UserBlendOps.Normal.cs
+++ Pinta.Core/Effects/UserBlendOps.Normal.cs
@@ -1,12 +1,3 @@
-/////////////////////////////////////////////////////////////////////////////////
-// Paint.NET                                                                   //
-// Copyright (C) dotPDN LLC, Rick Brewster, Tom Jackson, and contributors.     //
-// Portions Copyright (C) Microsoft Corporation. All Rights Reserved.          //
-// See license-pdn.txt for full licensing and attribution details.             //
-//                                                                             //
-// Ported to Pinta by: Jonathan Pobst <monkey@jpobst.com>                      //
-/////////////////////////////////////////////////////////////////////////////////
-
 using System;
 
 namespace Pinta.Core;
