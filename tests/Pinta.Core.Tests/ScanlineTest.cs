6c60f27cfc5ff6d5ef82f2276d41cda0826098dc
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-09-20 05:58:15 +0200

    Removed obsolete tests for `Scanline` and expanded existing ones (#1761)

--- tests/Pinta.Core.Tests/ScanlineTest.cs
+++ tests/Pinta.Core.Tests/ScanlineTest.cs
@@ -7,15 +7,14 @@ namespace Pinta.Core.Tests;
 [TestFixture]
 internal sealed class ScanlineTest
 {
-	// Perhaps most (all?) of these tests can be removed if Scanline is
-	// made into a record struct (a feature offered by C#10+)
-
-	[TestCaseSource (nameof (invalid_constructor_argument_combinations))]
+	[Test]
+	[TestCase (0, 0, -1)]
 	public void Constructor_RejectsInvalidArguments (int x, int y, int length)
 	{
 		Assert.Throws<ArgumentOutOfRangeException> (() => _ = new Scanline (x, y, length));
 	}
 
+	[Test]
 	[TestCaseSource (nameof (sample_initializations))]
 	public void MembersInitializingCorrectly (int x, int y, int length)
 	{
@@ -23,95 +22,102 @@ internal sealed class ScanlineTest
 			x: x,
 			y: y,
 			length: length);
-
+		using var _ = Assert.EnterMultipleScope ();
 		Assert.That (x, Is.EqualTo (scanline.X));
 		Assert.That (y, Is.EqualTo (scanline.Y));
 		Assert.That (length, Is.EqualTo (scanline.Length));
 	}
 
+	[Test]
 	[TestCaseSource (nameof (sample_initializations))]
 	public void EqualsOperator_TrueWithEqual (int x, int y, int length)
 	{
 		Scanline scanline1 = new (x, y, length);
 		Scanline scanline2 = new (x, y, length);
-		bool comparison = scanline1 == scanline2;
-		Assert.That (comparison, Is.True);
+		using var _ = Assert.EnterMultipleScope ();
+		Assert.That (scanline1 == scanline2, Is.True);
+		Assert.That (scanline2 == scanline1, Is.True);
 	}
 
+	[Test]
+	[TestCaseSource (nameof (sample_initializations))]
+	public void HashCodes_Are_Same_For_Equal_Values (int x, int y, int length)
+	{
+		Scanline scanline1 = new (x, y, length);
+		Scanline scanline2 = new (x, y, length);
+		using var _ = Assert.EnterMultipleScope ();
+		Assert.That (scanline1.GetHashCode (), Is.EqualTo (scanline2.GetHashCode ()));
+		Assert.That (scanline2.GetHashCode (), Is.EqualTo (scanline1.GetHashCode ()));
+	}
+
+	[Test]
 	[TestCaseSource (nameof (unequal_values))]
 	public void EqualsOperator_FalseWithUnequal (int x1, int y1, int length1, int x2, int y2, int length2)
 	{
 		Scanline scanline1 = new (x1, y1, length1);
 		Scanline scanline2 = new (x2, y2, length2);
-		bool comparison = scanline1 == scanline2;
-		Assert.That (comparison, Is.False);
+		using var _ = Assert.EnterMultipleScope ();
+		Assert.That (scanline1 == scanline2, Is.False);
+		Assert.That (scanline2 == scanline1, Is.False);
 	}
 
+	[Test]
 	[TestCaseSource (nameof (sample_initializations))]
 	public void NotEqualsOperator_FalseWithEqual (int x, int y, int length)
 	{
 		Scanline scanline1 = new (x, y, length);
 		Scanline scanline2 = new (x, y, length);
-		bool comparison = scanline1 != scanline2;
-		Assert.That (comparison, Is.False);
+		using var _ = Assert.EnterMultipleScope ();
+		Assert.That (scanline1 != scanline2, Is.False);
+		Assert.That (scanline2 != scanline1, Is.False);
 	}
 
+	[Test]
 	[TestCaseSource (nameof (unequal_values))]
 	public void NotEqualsOperator_TrueWithUnequal (int x1, int y1, int length1, int x2, int y2, int length2)
 	{
 		Scanline scanline1 = new (x1, y1, length1);
 		Scanline scanline2 = new (x2, y2, length2);
-		bool comparison = scanline1 != scanline2;
-		Assert.That (comparison, Is.True);
+		using var _ = Assert.EnterMultipleScope ();
+		Assert.That (scanline1 != scanline2, Is.True);
+		Assert.That (scanline2 != scanline1, Is.True);
 	}
 
+	[Test]
 	[TestCaseSource (nameof (sample_initializations))]
 	public void EqualsMethod_TrueWithEqual (int x, int y, int length)
 	{
 		Scanline scanline1 = new (x, y, length);
 		Scanline scanline2 = new (x, y, length);
-		bool comparison = scanline1.Equals (scanline2);
-		Assert.That (comparison, Is.True);
+		using var _ = Assert.EnterMultipleScope ();
+		Assert.That (scanline1.Equals (scanline2), Is.True);
+		Assert.That (scanline2.Equals (scanline1), Is.True);
 	}
 
-	[TestCaseSource (nameof (unequal_types_scanline))]
-	public void EqualsMethod_FalseWithUnequalTypes (Scanline scanline, object? other)
+	[Test]
+	public void EqualsMethod_FalseWithUnequalTypes ([ValueSource (nameof (unrelated_objects))] object? other)
 	{
+		Scanline scanline = new (1, 1, 1);
 		bool comparison = scanline.Equals (other);
 		Assert.That (comparison, Is.False);
 	}
 
+	[Test]
 	[TestCaseSource (nameof (unequal_values))]
 	public void EqualsMethod_FalseWithUnequalValues (int x1, int y1, int length1, int x2, int y2, int length2)
 	{
 		Scanline scanline1 = new (x1, y1, length1);
 		Scanline scanline2 = new (x2, y2, length2);
-		bool comparison = scanline1.Equals (scanline2);
-		Assert.That (comparison, Is.False);
+		using var _ = Assert.EnterMultipleScope ();
+		Assert.That (scanline1.Equals (scanline2), Is.False);
+		Assert.That (scanline2.Equals (scanline1), Is.False);
 	}
 
-	[TestCaseSource (nameof (overflowing_hash_code_cases))]
-	public void HashCode_DoesNotOverflow (int x, int y, int length)
-	{
-		// At the time of writing this code:
-		// - The hash of an int is the int itself.
-		// - The hash of Scanline is the overflow-unchecked addition
-		//   of the hash of all its members.
-		// If any of the above changes, I recommend preserving the existing test cases
-		// _and_ adding other test cases that may be of interest.
-		Scanline scanline = new (x, y, length);
-		Assert.DoesNotThrow (() => _ = scanline.GetHashCode ());
-	}
-
-	static readonly IReadOnlyList<TestCaseData> unequal_types_scanline = [
-		new (new Scanline (1, 1, 1), "This is not a scanline"),
-		new (new Scanline (1, 1, 1), null),
-		new (new Scanline (1, 1, 1), new[] { 1, 1, 1 }),
-		new (new Scanline (1, 1, 1), 111),
-	];
-
-	static readonly IReadOnlyList<TestCaseData> invalid_constructor_argument_combinations = [
-		new (0, 0, -1),
+	static readonly IReadOnlyList<object?> unrelated_objects = [
+		"This is not a scanline",
+		null,
+		new[] { 1, 1, 1 },
+		111,
 	];
 
 	static readonly IReadOnlyList<TestCaseData> sample_initializations = [
@@ -145,12 +151,5 @@ internal sealed class ScanlineTest
 		new (
 			2, 1, 1,
 			1, 1, 1),
-	];
-
-	static readonly IReadOnlyList<TestCaseData> overflowing_hash_code_cases = [
-		new (int.MaxValue, 1, 1),
-		new (1, int.MaxValue, 1),
-		new (1, 1, int.MaxValue),
-		new (int.MaxValue / 2, int.MaxValue / 2, int.MaxValue / 2),
 	];
 }
