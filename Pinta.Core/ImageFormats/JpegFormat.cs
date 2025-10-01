898d3673f8b8ddeba538b51a1dc671eefc19fc67
Cameron White <cameronwhite91@gmail.com>
2025-10-01 19:49:20 -0400

    Work around errors saving JPEG files with the glycin pixbuf loader
    This requires explicitly converting to a Gdk.Pixbuf which doesn't contain alpha.
    
    Bug: #1774

--- Pinta.Core/ImageFormats/JpegFormat.cs
+++ Pinta.Core/ImageFormats/JpegFormat.cs
@@ -1,21 +1,21 @@
-// 
+//
 // JpegFormat.cs
-//  
+//
 // Author:
 //       Maia Kozheva <sikon@ubuntu.com>
-// 
+//
 // Copyright (c) 2010 Maia Kozheva <sikon@ubuntu.com>
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,7 +37,7 @@ public sealed class JpegFormat : GdkPixbufFormat
 	private const int DefaultQuality = 85;
 
 	public JpegFormat ()
-		: base ("jpeg")
+		: base ("jpeg", supportsAlpha: false)
 	{
 	}
 
