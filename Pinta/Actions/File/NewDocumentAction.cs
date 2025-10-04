PintaProject/Pinta

commit 88489eadf55c322c6a9f9efe68456b9e07712b7e

Refactoring `Activated` method in `NewDocumentAction` (#1762)

Pinta/Actions/File/NewDocumentAction.cs
@@ -25,6 +25,7 @@
 // THE SOFTWARE.
 
 using System;
+using System.Threading.Tasks;
 using Pinta.Core;
 
 namespace Pinta.Actions;
@@ -62,51 +63,73 @@ internal sealed class NewDocumentAction : IActionHandler
 
 	private async void Activated (object sender, EventArgs e)
 	{
-		Size imageSize;
-		NewImageDialog.BackgroundType bg_type;
-		bool using_clipboard;
+		NewImageDialogOptions options = await GetDialogOptions ();
 
+		NewImageOptions? response = await PromptNewImage (options);
+
+		if (response == null)
+			return;
+
+		NewImageOptions newImageOptions = response.Value;
+
+		workspace.NewDocument (
+			newImageOptions.NewImageSize,
+			newImageOptions.NewImageBackground);
+
+		settings.PutSetting (SettingNames.NEW_IMAGE_WIDTH, newImageOptions.NewImageSize.Width);
+		settings.PutSetting (SettingNames.NEW_IMAGE_HEIGHT, newImageOptions.NewImageSize.Height);
+		settings.PutSetting (SettingNames.NEW_IMAGE_BACKGROUND, newImageOptions.NewImageBackground);
+	}
+
+	private async Task<NewImageDialogOptions> GetDialogOptions ()
+	{
 		// Try to get the dimensions of an image on the clipboard
 		// for the initial width and height values on the NewImageDialog
-		Gdk.Texture? cb_texture = await GdkExtensions.GetDefaultClipboard ().ReadTextureAsync ();
-		if (cb_texture is null) {
-			// An image was not on the clipboard,
-			// so use saved dimensions from settings
-			imageSize = new (
-				Width: settings.GetSetting<int> (SettingNames.NEW_IMAGE_WIDTH, 800),
-				Height: settings.GetSetting<int> (SettingNames.NEW_IMAGE_HEIGHT, 600));
-			bg_type = settings.GetSetting<NewImageDialog.BackgroundType> (
-				SettingNames.NEW_IMAGE_BACKGROUND,
-				NewImageDialog.BackgroundType.White);
-			using_clipboard = false;
-		} else {
-			imageSize = new (
-				Width: cb_texture.Width,
-				Height: cb_texture.Height);
-			bg_type = NewImageDialog.BackgroundType.White;
-			using_clipboard = true;
-		}
 
+		Gdk.Texture? clipboardTexture = await
+			GdkExtensions
+			.GetDefaultClipboard ()
+			.ReadTextureAsync ();
+
+		if (clipboardTexture is not null)
+			return new (
+				Size: new (
+					Width: clipboardTexture.Width,
+					Height: clipboardTexture.Height),
+
+				Background: BackgroundType.White,
+				UsingClipboard: true);
+
+		// An image was not on the clipboard,
+		// so use saved dimensions from settings
+		return new (
+			Size: new (
+				Width: settings.GetSetting<int> (SettingNames.NEW_IMAGE_WIDTH, 800),
+				Height: settings.GetSetting<int> (SettingNames.NEW_IMAGE_HEIGHT, 600)),
+			Background: settings.GetSetting<BackgroundType> (
+				SettingNames.NEW_IMAGE_BACKGROUND,
+				BackgroundType.White),
+			UsingClipboard: false);
+	}
+
+	private async Task<NewImageOptions?> PromptNewImage (NewImageDialogOptions options)
+	{
 		using NewImageDialog dialog = new (
 			chrome,
 			palette,
-			imageSize,
-			bg_type,
-			using_clipboard);
+			options.Size,
+			options.Background,
+			options.UsingClipboard);
 
 		try {
 			Gtk.ResponseType response = await dialog.RunAsync ();
 
 			if (response != Gtk.ResponseType.Ok)
-				return;
+				return null;
 
-			workspace.NewDocument (
+			return new (
 				dialog.NewImageSize,
 				dialog.NewImageBackground);
-
-			settings.PutSetting (SettingNames.NEW_IMAGE_WIDTH, dialog.NewImageWidth);
-			settings.PutSetting (SettingNames.NEW_IMAGE_HEIGHT, dialog.NewImageHeight);
-			settings.PutSetting (SettingNames.NEW_IMAGE_BACKGROUND, dialog.NewImageBackgroundType);
 
 		} finally {
 			dialog.Destroy ();

Pinta/Messages/ActionOptions.cs
@@ -1,6 +1,16 @@
+using Cairo;
 using Pinta.Core;
 
 namespace Pinta;
 
+public enum BackgroundType
+{
+	White,
+	Transparent,
+	SecondaryColor,
+}
+
+public readonly record struct NewImageDialogOptions (Size Size, BackgroundType Background, bool UsingClipboard);
+public readonly record struct NewImageOptions (Size NewImageSize, Color NewImageBackground);
 public readonly record struct ResizeImageOptions (Size NewSize, ResamplingMode ResamplingMode);
 public readonly record struct ResizeCanvasOptions (Size NewSize, Anchor Anchor, CompoundHistoryItem? CompoundAction);

Pinta/Dialogs/NewImageDialog.cs
@@ -372,13 +372,6 @@ public sealed class NewImageDialog : Gtk.Dialog
 	public Size NewImageSize
 		=> new (NewImageWidth, NewImageHeight);
 
-	public enum BackgroundType
-	{
-		White,
-		Transparent,
-		SecondaryColor,
-	}
-
 	public BackgroundType NewImageBackgroundType {
 		get {
 			if (white_background_radio.Active)

commit dfa36b7a9f75309f7672df2f48190ba5b951fbc1

Fix errors from saving the new image background setting with inconsistent types
This was a regression from 88489ead which accidentally saved the background color value in the settings, rather than the background type setting.

Fixes: #1788

Pinta/Actions/File/NewDocumentAction.cs
@@ -74,11 +74,11 @@ internal sealed class NewDocumentAction : IActionHandler
 
 		workspace.NewDocument (
 			newImageOptions.NewImageSize,
-			newImageOptions.NewImageBackground);
+			newImageOptions.NewImageBackgroundColor);
 
 		settings.PutSetting (SettingNames.NEW_IMAGE_WIDTH, newImageOptions.NewImageSize.Width);
 		settings.PutSetting (SettingNames.NEW_IMAGE_HEIGHT, newImageOptions.NewImageSize.Height);
-		settings.PutSetting (SettingNames.NEW_IMAGE_BACKGROUND, newImageOptions.NewImageBackground);
+		settings.PutSetting (SettingNames.NEW_IMAGE_BACKGROUND, newImageOptions.NewImageBackgroundType);
 	}
 
 	private async Task<NewImageDialogOptions> GetDialogOptions ()
@@ -129,7 +129,8 @@ internal sealed class NewDocumentAction : IActionHandler
 
 			return new (
 				dialog.NewImageSize,
-				dialog.NewImageBackground);
+				dialog.NewImageBackground,
+				dialog.NewImageBackgroundType);
 
 		} finally {
 			dialog.Destroy ();

Pinta/Messages/ActionOptions.cs
@@ -11,6 +11,6 @@ public enum BackgroundType
 }
 
 public readonly record struct NewImageDialogOptions (Size Size, BackgroundType Background, bool UsingClipboard);
-public readonly record struct NewImageOptions (Size NewImageSize, Color NewImageBackground);
+public readonly record struct NewImageOptions (Size NewImageSize, Color NewImageBackgroundColor, BackgroundType NewImageBackgroundType);
 public readonly record struct ResizeImageOptions (Size NewSize, ResamplingMode ResamplingMode);
 public readonly record struct ResizeCanvasOptions (Size NewSize, Anchor Anchor, CompoundHistoryItem? CompoundAction);