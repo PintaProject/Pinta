9f2ea6d5eaa6fdea5ccda8aba415134d48a5765b
Cameron White <cameronwhite91@gmail.com>
2026-04-02 23:37:35 -0400

    Port the offset selection dialog and new image preview to the new gir.core subclass initialization

--- Pinta/Dialogs/OffsetSelectionDialog.cs
+++ Pinta/Dialogs/OffsetSelectionDialog.cs
@@ -1,21 +1,21 @@
-// 
+//
 // OffsetSelectionDialog.cs
-//  
+//
 // Author:
 //       Jonathan Pobst <monkey@jpobst.com>
-// 
+//
 // Copyright (c) 2010 Jonathan Pobst
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,20 +29,27 @@ using Pinta.Gui.Widgets;
 
 namespace Pinta;
 
-public sealed class OffsetSelectionDialog : Gtk.Dialog
+[GObject.Subclass<Gtk.Dialog>]
+public sealed partial class OffsetSelectionDialog
 {
-	private readonly HScaleSpinButtonWidget offset_spinbox;
+	private readonly HScaleSpinButtonWidget offset_spinbox = new (initialValue: 0);
 
 	public int Offset
 		=> offset_spinbox.ValueAsInt;
 
-	public OffsetSelectionDialog (IChromeService chrome)
+	public static OffsetSelectionDialog New (IChromeService chrome)
+	{
+		OffsetSelectionDialog dialog = NewWithProperties ([]);
+		dialog.TransientFor = chrome.MainWindow;
+		return dialog;
+	}
+
+	partial void Initialize ()
 	{
 		DefaultWidth = 400;
 		DefaultHeight = 100;
 
 		Title = Translations.GetString ("Offset Selection");
-		TransientFor = chrome.MainWindow;
 		Modal = true;
 
 		Resizable = false;
@@ -50,17 +57,15 @@ public sealed class OffsetSelectionDialog : Gtk.Dialog
 		this.AddCancelOkButtons ();
 		this.SetDefaultResponse (Gtk.ResponseType.Ok);
 
-		offset_spinbox = new HScaleSpinButtonWidget (0) {
-			Label = Translations.GetString ("Offset"),
-			MaximumValue = 100,
-			MinimumValue = -100,
-		};
+		offset_spinbox.Label = Translations.GetString ("Offset");
+		offset_spinbox.MinimumValue = -100;
+		offset_spinbox.MaximumValue = 100;
 
-		Gtk.Box content_area = this.GetContentAreaBox ();
-		content_area.WidthRequest = 400;
-		content_area.SetAllMargins (6);
-		content_area.Spacing = 6;
-		content_area.Append (offset_spinbox);
+		Gtk.Box contentArea = this.GetContentAreaBox ();
+		contentArea.WidthRequest = 400;
+		contentArea.SetAllMargins (6);
+		contentArea.Spacing = 6;
+		contentArea.Append (offset_spinbox);
 	}
 }
 
