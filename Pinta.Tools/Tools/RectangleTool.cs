28b59283876bc96aa6ba33f95d56ff465a3a3d54
Sam <43098166+Aryezz@users.noreply.github.com>
2026-04-11 04:57:14 +0200

    FIX: don't use lazy initialization for BaseEditEngine, fixes #1420 (#2096)
    - Initializing BaseEditEngine lazily would clear the SEngines collection in BaseEditEngine, meaning existing engines would not get finalized.
    - Use abstract property for edit engine instead of nullable field
    
    Fixes: #1420
    
    Co-authored-by: Samuel Burkhardt <samuel@burkhardt.dev>

--- Pinta.Tools/Tools/RectangleTool.cs
+++ Pinta.Tools/Tools/RectangleTool.cs
@@ -1,21 +1,21 @@
-// 
+//
 // RectangleTool.cs
-//  
+//
 // Author:
 //       Jonathan Pobst <monkey@jpobst.com>
-// 
+//
 // Copyright (c) 2010 Jonathan Pobst
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,13 +31,12 @@ namespace Pinta.Tools;
 
 public sealed class RectangleTool : ShapeTool
 {
-	private readonly IServiceProvider services;
 	public RectangleTool (IServiceProvider services) : base (services)
 	{
-		this.services = services;
 		DefaultCursor = Gdk.Cursor.NewFromTexture (Resources.GetIcon ("Cursor.Rectangle.png"), 9, 18, null);
 
 		BaseEditEngine.CorrespondingTools[ShapeType] = this;
+		EditEngine = new RectangleEditEngine (services, this);
 	}
 
 	public override string Name => Translations.GetString ("Rectangle");
@@ -45,9 +44,8 @@ public sealed class RectangleTool : ShapeTool
 	public override Gdk.Cursor DefaultCursor { get; }
 	public override int Priority => 39;
 
+	public override BaseEditEngine EditEngine { get; }
+
 	public override BaseEditEngine.ShapeTypes ShapeType
 		=> BaseEditEngine.ShapeTypes.ClosedLineCurveSeries;
-
-	protected override RectangleEditEngine CreateEditEngine ()
-		=> new (services, this);
 }
