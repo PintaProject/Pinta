72ca35b102c6853c14a3aaa39381b589e1378fc4
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2026-04-20 02:56:29 +0200

    Deleted unnecessary code

--- Pinta.Core/Classes/DocumentWorkspace.cs
+++ Pinta.Core/Classes/DocumentWorkspace.cs
@@ -110,11 +110,6 @@ public sealed class DocumentWorkspace
 		ViewSize = GetNewViewSize (document.ImageSize, value);
 
 		Invalidate ();
-
-		if (tools.CurrentTool?.CursorChangesOnZoom == true) {
-			//The current tool's cursor changes when the zoom changes.
-			tools.CurrentTool.SetCursor (tools.CurrentTool.CurrentCursor);
-		}
 	}
 
 	/// <summary>
