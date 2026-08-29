0ad0d3655db1651843901049e9af47f451fbe94a
Cameron White <cameronwhite91@gmail.com>
2026-08-29 19:49:10 -0400

    Move the menu bar option into the preferences dialog
    Unlike the other options which dynamically show rulers, etc, this option is more of a preference that changes the layout significantly.

--- Pinta/Dialogs/PreferencesDialog.cs
+++ Pinta/Dialogs/PreferencesDialog.cs
@@ -13,6 +13,9 @@ internal sealed partial class PreferencesDialog
 	[Gtk.Connect ("color_scheme_comborow")]
 	private Adw.ComboRow color_scheme_row;
 
+	[Gtk.Connect ("menubar_switchrow")]
+	private Adw.SwitchRow menubar_row;
+
 	public static PreferencesDialog New (ISettingsService settings)
 	{
 		PreferencesDialog dialog = NewWithProperties ([]);
@@ -23,6 +26,8 @@ internal sealed partial class PreferencesDialog
 	partial void Initialize ()
 	{
 		Adw.ComboRow.SelectedPropertyDefinition.Notify (color_scheme_row, OnColorSchemeChanged);
+
+		Adw.SwitchRow.ActivePropertyDefinition.Notify (menubar_row, OnMenuBarChanged);
 	}
 
 	/// <summary>
@@ -34,11 +39,35 @@ internal sealed partial class PreferencesDialog
 
 		int schemeIndex = settings.GetSetting (SettingNames.COLOR_SCHEME, 0);
 		color_scheme_row.SetSelected ((uint) schemeIndex);
+
+		bool menuBarShown = settings.GetSetting (SettingNames.MENUBAR_SHOWN, SettingDefaults.MenuBarShown ());
+		menubar_row.Active = menuBarShown;
 	}
 
 	private void OnColorSchemeChanged (Object sender, NotifySignalArgs args)
 	{
 		int schemeIndex = (int) color_scheme_row.Selected;
 		settings.PutSetting (SettingNames.COLOR_SCHEME, schemeIndex);
+	}
+
+	private void OnMenuBarChanged (Object sender, NotifySignalArgs args)
+	{
+		// Don't trigger the restart message when the setting is loaded on startup.
+		if (menubar_row.Active == settings.GetSetting (SettingNames.MENUBAR_SHOWN, SettingDefaults.MenuBarShown ()))
+			return;
+
+		settings.PutSetting (SettingNames.MENUBAR_SHOWN, menubar_row.Active);
+
+		// Changing the setting requires a restart since the application window is
+		// constructed differently (see WindowShell).
+		ShowRestartMessage ();
+	}
+
+	private void ShowRestartMessage ()
+	{
+		Adw.Toast toast = Adw.Toast.New (Translations.GetString ("Please restart Pinta for the changes to take effect."));
+		toast.Timeout = 2;
+
+		AddToast (toast);
 	}
 }
