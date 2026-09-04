376df39b5eb607297e2fced6c1cf3653aace1ec6
Cameron White <cameronwhite91@gmail.com>
2026-09-03 23:16:49 -0400

    Port the add-in info view to use a .ui file

--- Pinta.Gui.Addins/AddinInfoView.cs
+++ Pinta.Gui.Addins/AddinInfoView.cs
@@ -5,27 +5,42 @@ using Pinta.Core;
 
 namespace Pinta.Gui.Addins;
 
-[GObject.Subclass<Adw.Bin>]
+[GObject.Subclass<Adw.Bin> (qualifiedName: nameof (AddinInfoView))]
+[Gtk.Template<Gtk.AssemblyResource> ("AddinInfoView.ui")]
 internal sealed partial class AddinInfoView
 {
+	[Gtk.Connect (nameof (title_label))]
 	private Gtk.Label title_label;
+	[Gtk.Connect (nameof (version_label))]
 	private Gtk.Label version_label;
+	[Gtk.Connect (nameof (size_label))]
 	private Gtk.Label size_label;
+	[Gtk.Connect (nameof (repo_label))]
 	private Gtk.Label repo_label;
+	[Gtk.Connect (nameof (description_label))]
 	private Gtk.Label description_label;
 
+	[Gtk.Connect (nameof (info_button))]
 	private Gtk.Button info_button;
+	[Gtk.Connect (nameof (install_button))]
 	private Gtk.Button install_button;
+	[Gtk.Connect (nameof (update_button))]
 	private Gtk.Button update_button;
+	[Gtk.Connect (nameof (uninstall_button))]
 	private Gtk.Button uninstall_button;
 
+	[Gtk.Connect (nameof (enable_switch))]
 	private Gtk.Switch enable_switch;
 
+	[Gtk.Connect (nameof (content_box))]
 	private Gtk.Box content_box;
 
+	[Gtk.Connect (nameof (empty_page))]
 	private Adw.Bin empty_page;
 
+	[Gtk.Connect (nameof (view_stack))]
 	private Adw.ViewStack view_stack;
+
 	private AddinListViewItem? current_item;
 
 	/// <summary>
@@ -36,114 +51,17 @@ internal sealed partial class AddinInfoView
 	private SystemManager system = null!; // NRT - set by factory method.
 	private IChromeService chrome = null!;
 
-	[MemberNotNull (nameof (title_label))]
-	[MemberNotNull (nameof (version_label))]
-	[MemberNotNull (nameof (size_label))]
-	[MemberNotNull (nameof (repo_label))]
-	[MemberNotNull (nameof (description_label))]
-	[MemberNotNull (nameof (info_button))]
-	[MemberNotNull (nameof (install_button))]
-	[MemberNotNull (nameof (update_button))]
-	[MemberNotNull (nameof (uninstall_button))]
-	[MemberNotNull (nameof (enable_switch))]
-	[MemberNotNull (nameof (content_box))]
-	[MemberNotNull (nameof (empty_page))]
-	[MemberNotNull (nameof (view_stack))]
 	partial void Initialize ()
 	{
-		// --- Control creation
+		info_button.OnClicked += (_, _) => HandleInfoButtonClicked ();
+		install_button.OnClicked += (_, _) => HandleInstallButtonClicked ();
+		update_button.OnClicked += (_, _) => HandleUpdateButtonClicked ();
+		uninstall_button.OnClicked += (_, _) => HandleUninstallButtonClicked ();
 
-		Gtk.Label titleLabel = Gtk.Label.New (null);
-		titleLabel.Halign = Gtk.Align.Start;
-		titleLabel.AddCssClass (AdwaitaStyles.Title4);
-
-		Gtk.Label versionLabel = Gtk.Label.New (null);
-		versionLabel.Halign = Gtk.Align.Start;
-		versionLabel.AddCssClass (AdwaitaStyles.Heading);
-
-		Gtk.Label sizeLabel = Gtk.Label.New (null);
-		sizeLabel.Halign = Gtk.Align.Start;
-		sizeLabel.AddCssClass (AdwaitaStyles.Heading);
-
-		Gtk.Label repoLabel = Gtk.Label.New (null);
-		repoLabel.Halign = Gtk.Align.Start;
-		repoLabel.AddCssClass (AdwaitaStyles.Heading);
-
-		Gtk.Label descriptionLabel = CreateDescriptionLabel ();
-
-		Adw.Bin emptyPage = Adw.Bin.New ();
-
-		Gtk.Button infoButton = CreateInfoButton ();
-		Gtk.Button installButton = CreateInstallButton ();
-		Gtk.Button updateButton = CreateUpdateButton ();
-		Gtk.Button uninstallButton = CreateUninstallButton ();
-
-		Gtk.Switch enableSwitch = CreateEnableSwitch ();
-
-		BoxStyle spacedHorizontal = new (
-			orientation: Gtk.Orientation.Horizontal,
-			spacing: 6,
-			cssClass: AdwaitaStyles.Toolbar);
-		Gtk.Box hbox = GtkExtensions.Box (
-			spacedHorizontal,
-			[
-				enableSwitch,
-				installButton,
-				updateButton,
-				infoButton,
-				uninstallButton
-			]
-		);
-
-		BoxStyle spacedVertical = new (
-			orientation: Gtk.Orientation.Vertical,
-			spacing: 10);
-		Gtk.Box contentBox = GtkExtensions.Box (
-			spacedVertical,
-			[
-				titleLabel,
-				versionLabel,
-				sizeLabel,
-				repoLabel,
-				descriptionLabel,
-				hbox
-			]
-		);
-		contentBox.SetAllMargins (10);
-
-		Adw.ViewStack viewStack = Adw.ViewStack.New ();
-		viewStack.Add (emptyPage);
-		viewStack.Add (contentBox);
-		viewStack.SetVisibleChild (emptyPage);
-
-		// --- Gtk.Widget initialization
-
-		WidthRequest = 300;
-
-		// --- Adwaita.Bin initialization
-
-		Child = viewStack;
-
-		// --- References to keep
-
-		title_label = titleLabel;
-		version_label = versionLabel;
-		size_label = sizeLabel;
-		repo_label = repoLabel;
-		description_label = descriptionLabel;
-
-		info_button = infoButton;
-		install_button = installButton;
-		update_button = updateButton;
-		uninstall_button = uninstallButton;
-
-		enable_switch = enableSwitch;
-
-		content_box = contentBox;
-
-		empty_page = emptyPage;
-
-		view_stack = viewStack;
+		enable_switch.OnStateSet += (_, _) => {
+			HandleEnableSwitched ();
+			return false;
+		};
 	}
 
 	internal void Configure (SystemManager system, IChromeService chrome)
@@ -153,67 +71,6 @@ internal sealed partial class AddinInfoView
 	}
 
 	public static new AddinInfoView New () => NewWithProperties ([]);
-
-	private Gtk.Switch CreateEnableSwitch ()
-	{
-		Gtk.Switch result = Gtk.Switch.New ();
-		result.Visible = false;
-		result.OnStateSet += (_, _) => {
-			HandleEnableSwitched ();
-			return false;
-		};
-		return result;
-	}
-
-	private static Gtk.Label CreateDescriptionLabel ()
-	{
-		Gtk.Label result = Gtk.Label.New (null);
-		result.Halign = Gtk.Align.Start;
-		result.Hexpand = true;
-		result.Valign = Gtk.Align.Start;
-		result.Vexpand = true;
-		result.Xalign = 0;
-		result.Wrap = true;
-		result.AddCssClass (AdwaitaStyles.Body);
-		return result;
-	}
-
-	private Gtk.Button CreateInfoButton ()
-	{
-		Gtk.Button result = Gtk.Button.NewWithLabel (Translations.GetString ("More Information..."));
-		result.OnClicked += (_, _) => HandleInfoButtonClicked ();
-		result.Visible = false;
-		return result;
-	}
-
-	private Gtk.Button CreateInstallButton ()
-	{
-		Gtk.Button result = Gtk.Button.NewWithLabel (Translations.GetString ("Install..."));
-		result.AddCssClass (AdwaitaStyles.SuggestedAction);
-		result.OnClicked += (_, _) => HandleInstallButtonClicked ();
-		result.Visible = false;
-		return result;
-	}
-
-	private Gtk.Button CreateUpdateButton ()
-	{
-		Gtk.Button result = Gtk.Button.NewWithLabel (Translations.GetString ("Update..."));
-		result.AddCssClass (AdwaitaStyles.SuggestedAction);
-		result.OnClicked += (_, _) => HandleUpdateButtonClicked ();
-		result.Visible = false;
-		return result;
-	}
-
-	private Gtk.Button CreateUninstallButton ()
-	{
-		Gtk.Button result = Gtk.Button.NewWithLabel (Translations.GetString ("Uninstall..."));
-		result.AddCssClass (AdwaitaStyles.DestructiveAction);
-		result.OnClicked += (_, _) => HandleUninstallButtonClicked ();
-		result.Visible = false;
-		result.Hexpand = true;
-		result.Halign = Gtk.Align.End;
-		return result;
-	}
 
 	public void Update (AddinListViewItem? item)
 	{
