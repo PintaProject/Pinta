99c3df8ec0b0ba90b6ce71d5b7656ea2da3c8f6a
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2026-09-12 22:19:23 +0200

    Fix/spinbutton value commit (#2294)
    * More lax handling of invalid values

--- Pinta.Gui.Widgets/Widgets/HScaleSpinButtonWidget.cs
+++ Pinta.Gui.Widgets/Widgets/HScaleSpinButtonWidget.cs
@@ -64,13 +64,17 @@ public sealed partial class HScaleSpinButtonWidget
 		hScale.Halign = Gtk.Align.Fill;
 		hScale.OnValueChanged += HandleHscaleValueChanged;
 
+		Gtk.EventControllerKey spinButtonKeyController = Gtk.EventControllerKey.New ();
+		spinButtonKeyController.OnKeyReleased += SpinButtonKeyRelease;
+
 		Gtk.SpinButton spinButton = Gtk.SpinButton.NewWithRange (0, 100, 1);
 		spinButton.CanFocus = true;
 		spinButton.ClimbRate = 1;
 		spinButton.Numeric = true;
 		spinButton.Adjustment!.PageIncrement = 10;
 		spinButton.OnValueChanged += HandleSpinValueChanged;
-		spinButton.SetActivatesDefaultImmediate (true);
+		spinButton.SetActivatesDefault (true);
+		spinButton.AddController (spinButtonKeyController);
 
 		Gtk.Button resetButton = Gtk.Button.NewFromIconName (Resources.StandardIcons.GoPrevious);
 		resetButton.WidthRequest = 28;
@@ -96,6 +100,17 @@ public sealed partial class HScaleSpinButtonWidget
 		title_label = titleLabel;
 		h_scale = hScale;
 		spin_button = spinButton;
+	}
+
+	private void SpinButtonKeyRelease (
+		Gtk.EventControllerKey sender,
+		Gtk.EventControllerKey.KeyReleasedSignalArgs args)
+	{
+		if (!double.TryParse (spin_button.GetText (), out double parsed)) return;
+		Gtk.Adjustment adjustment = spin_button.Adjustment!;
+		if (parsed < adjustment.Lower || parsed > adjustment.Upper) return;
+		if (spin_button.Value == parsed) return;
+		spin_button.Value = parsed;
 	}
 
 	public static HScaleSpinButtonWidget New (double initialValue)
