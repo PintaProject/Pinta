2513dd14b809d6f07bb7cb58a70e23c9709cffbd
Cameron White <cameronwhite91@gmail.com>
2026-09-14 22:45:49 -0400

    Initial work toward adding a language preference
    - Add utility methods for listing the available languages and constructing a nice display name
    - Add a combobox in the preferences dialog which lists these languages

--- Pinta.Core/Classes/Translations.cs
+++ Pinta.Core/Classes/Translations.cs
@@ -25,16 +25,22 @@
 // THE SOFTWARE.
 
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace Pinta.Core;
 
 public static class Translations
 {
 	private const string PintaTextDomain = "pinta";
+	private static string locale_dir = "";
 
 	public static void Init (string localeDir)
 	{
+		locale_dir = localeDir;
+
 		// Note we need to initialize the GLib module since this is called very early in startup,
 		// before GTK is initialized.
 		GLib.Module.Initialize ();
@@ -65,5 +71,34 @@ public static class Translations
 	public static string GetString (string text, params object[] args)
 	{
 		return string.Format (GetString (text), args);
+	}
+
+	/// <summary>
+	/// Returns a list of the language codes (e.g. 'fr', 'en_CA') that Pinta has translations for.
+	/// The list is not in any particular order.
+	/// </summary>
+	public static IEnumerable<string> GetAvailableLanguages ()
+	{
+		string moFile = $"{PintaTextDomain}.mo";
+
+		// List folders in the locale dir if they contain Pinta's translation file.
+		// On Linux, the folder might e.g. be /usr/share/locale which contains additional languages.
+		DirectoryInfo dirInfo = new (locale_dir);
+		return dirInfo.EnumerateDirectories ()
+			.Where (dir => File.Exists (Path.Combine (dir.FullName, "LC_MESSAGES", moFile)))
+			.Select (dir => dir.Name);
+	}
+
+	/// <summary>
+	/// Returns a suitable display name for the specified language code.
+	/// </summary>
+	public static string GetLanguageDisplayName (string languageCode)
+	{
+		// Map back from e.g. 'en_CA' to 'en-CA' for the dotnet APIs.
+		// This ensures we get 'English (Canada)' rather than 'English (Sort Order=ca)'.
+		string dotnetLanguageCode = languageCode.Replace ('_', '-');
+
+		CultureInfo cultureInfo = CultureInfo.GetCultureInfo (dotnetLanguageCode);
+		return cultureInfo.TextInfo.ToTitleCase (cultureInfo.NativeName);
 	}
 }
